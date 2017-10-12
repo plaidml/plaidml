90e11c09e6ef81cd54aba3b732ad92e54b14de44
Brian Retford <brian@vertex.ai>
2017-10-12 15:24:51 -0700

    Add missing new files

---
+++ tile/hal/opencl/zero_kernel_svm_disabled.cc
@@ -0,0 +1,23 @@
+// Copyright 2017, Vertex.AI. CONFIDENTIAL
+
+#include "tile/hal/opencl/zero_kernel.h"
+
+namespace vertexai {
+namespace tile {
+namespace hal {
+namespace opencl {
+
+CLObj<cl_event> ZeroKernel::FillBufferImpl(const DeviceState::Queue& queue, Buffer* buf, void* pattern,
+                                           size_t pattern_size, const std::vector<cl_event>& deps) {
+  CLObj<cl_event> done;
+  auto event_wait_list = deps.size() ? deps.data() : nullptr;
+  Err err = clEnqueueFillBuffer(queue.cl_queue.get(), buf->mem(), pattern, pattern_size, 0, buf->size(), deps.size(),
+                                event_wait_list, done.LvaluePtr());
+  Err::Check(err, "unable to fill buffer");
+  return done;
+}
+
+}  // namespace opencl
+}  // namespace hal
+}  // namespace tile
+}  // namespace vertexai
