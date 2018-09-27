92eef8d706da4c9a5f1bd957af2c81eec3bd223e
Frank Laub <frank.laub@intel.com>
2018-09-27 00:04:24 -0700

    Adjust stencil matching
    * Improve costing function
    * Add alpha to allow for startup cycles modeling

--- tile/codegen/tile.h
+++ tile/codegen/tile.h
@@ -11,15 +11,20 @@ namespace vertexai {
 namespace tile {
 namespace codegen {
 
-struct StencilCriteria {
+struct StencilIndex {
   std::string name;
   int size;
   std::vector<int> out_strides;
   std::vector<int> in_strides;
 };
 
+struct StencilSpec {
+  size_t alpha;
+  std::vector<StencilIndex> idxs;
+};
+
 struct StencilMatch {
-  size_t total;
+  size_t cost;
   std::vector<std::string> names;
   TileShape tile;
   bool is_fallback;
@@ -29,14 +34,13 @@ std::ostream& operator<<(std::ostream& os, const StencilMatch& match);
 bool operator==(const StencilMatch& lhs, const StencilMatch& rhs);
 bool operator<(const StencilMatch& lhs, const StencilMatch& rhs);
 
-StencilMatch FindBestStencil(const std::vector<std::vector<StencilCriteria>>& criteria,  //
-                             stripe::Block* block);
+StencilMatch FindBestStencil(const std::vector<StencilSpec>& specs, stripe::Block* block);
 
 void ApplyTile(stripe::Block* outer, const TileShape& tile);
 
 typedef std::function<TileShape(stripe::Block* block)> TileGenerator;
 
-void TilePass(stripe::Block* block, const std::vector<std::vector<StencilCriteria>>& criteria);
+void TilePass(stripe::Block* block, const std::vector<StencilSpec>& specs);
 void TilePass(stripe::Block* block, const TileGenerator& generator);
 
 }  // namespace codegen
