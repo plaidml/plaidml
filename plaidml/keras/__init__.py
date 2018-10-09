3f59bf3ab5335896ae1afb75a1150523d2e5aa53
Brian Retford <brian.retford@intel.com>
2018-10-09 14:20:53 -0700

    remove agpl reference

--- plaidml/keras/__init__.py
+++ plaidml/keras/__init__.py
@@ -1,10 +1,4 @@
 # Copyright 2018 Intel Corporation.
-#
-# Licensed under the GNU Affero General Public License V3 (the License) ;
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    https://www.gnu.org/licenses/agpl-3.0.en.html
 """Patches in a PlaidML backend for Keras.
 
 This module hooks the system meta module path to add a backend for Keras
