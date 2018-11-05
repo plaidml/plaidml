66262eeddf074b879d3f54c514ab4d828151cd86
Rob Earhart <robert.earhart@intel.com>
2018-11-04 20:22:32 -0800

    Add memory scheduler

--- tile/codegen/placer.h
+++ tile/codegen/placer.h
@@ -13,14 +13,12 @@ namespace codegen {
 // Assigns locations to all Refinements within a Block, including all
 // nested sub-Blocks.  Note that all dependencies for the block and
 // sub-blocks should be established when this function is called.
-//
-// TODO: Compute alignment correctly.
-void PlaceRefinements(stripe::Block* outermost_block, std::size_t alignment = 16);
+void PlaceRefinements(stripe::Block* outermost_block, const proto::MemoryPlacementPass& options);
 
-inline void MemPlacementPass(stripe::Block* root, const proto::GenericPass& options) {
+inline void MemPlacementPass(stripe::Block* root, const proto::MemoryPlacementPass& options) {
   auto reqs = FromProto(options.reqs());
-  RunOnBlocks(root, reqs, [](const AliasMap& map, stripe::Block* block) {  //
-    PlaceRefinements(block, 4);
+  RunOnBlocks(root, reqs, [&](const AliasMap& map, stripe::Block* block) {  //
+    PlaceRefinements(block, options);
   });
 }
 
