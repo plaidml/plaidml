13f0080bf9a53f32ccaac91aa5b93553016f4ae4
Rob Earhart <robert.earhart@intel.com>
2018-11-22 19:09:18 -0800

    Add noreturn to throw_with_trace

--- base/util/throw.h
+++ base/util/throw.h
@@ -6,6 +6,6 @@
 using traced = boost::error_info<struct tag_stacktrace, boost::stacktrace::stacktrace>;
 
 template <typename E>
-void throw_with_trace(const E& ex) {
+[[noreturn]] void throw_with_trace(const E& ex) {
   throw boost::enable_error_info(ex) << traced(boost::stacktrace::stacktrace());
 }
