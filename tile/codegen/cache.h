0556afa5e3bf56e1714fb9c223f2287c82cd67ca
Frank Laub <github@frank.laub.io>
2018-12-18 22:18:19 -0800

    Fix CachePass

--- tile/codegen/cache.h
+++ tile/codegen/cache.h
@@ -18,23 +18,7 @@ void ApplyCache(stripe::Block* block,             //
                 const stripe::Location& mem_loc,  //
                 const stripe::Location& xfer_loc);
 
-void CacheBlock(stripe::Block* block,                  //
-                const std::set<stripe::RefDir>& dirs,  //
-                const stripe::Location& mem_loc,       //
-                const stripe::Location& xfer_loc);
-
-inline void CachePass(stripe::Block* root, const proto::CachePass& options) {
-  auto reqs = FromProto(options.reqs());
-  std::set<stripe::RefDir> dirs;
-  for (const auto& dir : options.dirs()) {
-    dirs.emplace(stripe::FromProto(static_cast<stripe::proto::Refinement::Dir>(dir)));
-  }
-  auto mem_loc = stripe::FromProto(options.mem_loc());
-  auto xfer_loc = stripe::FromProto(options.xfer_loc());
-  RunOnBlocks(root, reqs, [&](const AliasMap& map, stripe::Block* block) {  //
-    CacheBlock(block, dirs, mem_loc, xfer_loc);
-  });
-}
+void CachePass(stripe::Block* root, const proto::CachePass& options);
 
 }  // namespace codegen
 }  // namespace tile
