5ab33fa20c798d1daf0af53f1d484da7c3d16121
Tim Zerrell <tim.zerrell@intel.com>
2019-04-04 14:03:39 -0700

    Add tests based on PaddlePaddle ops

--- tile/lib/lib.cc
+++ tile/lib/lib.cc
@@ -199,6 +199,122 @@ function (I[N, Lx, Ly, CI], K[LKx, LKy, CI, CO]) -> (O) {
   return runinfo;
 }
 
+lang::RunInfo LoadLarsMomentum4d(const std::string& name,     //
+                                 const TensorShape& x_shape,  //
+                                 const TensorShape& lr_shape) {
+  // Note: X/Grad/Veloc/NewX/NewVeloc should all have the same shape for the
+  // semantics of this operation to be correct, so we only pass in 1 shape for
+  // all of them.
+  double lars_coeff = 1. / 1024.;
+  double lars_weight_decay = 1. / 2048.;
+  double momentum = 1. / 8.;
+  auto code = R"***(
+function (X[X0, X1, X2, X3], Grad[Grad0, Grad1, Grad2, Grad3], Veloc[Veloc0, Veloc1, Veloc2, Veloc3], LR[]) -> (NewX, NewVeloc) {
+    XSqr = X * X;
+    X_MS[] = +(XSqr[x0, x1, x2, x3]);
+    XNorm = sqrt(X_MS);
+    GradSqr = Grad * Grad;
+    Grad_MS[] = +(GradSqr[g0, g1, g2, g3]);
+    GradNorm = sqrt(Grad_MS);
+    LocLR = LR * %1% * XNorm / (GradNorm + %2% * XNorm);
+    NewVeloc = %3% * Veloc + LocLR * (Grad + %2% * X);
+    NewX = X - NewVeloc;
+})***";
+  lang::RunInfo runinfo;
+  runinfo.program_name = name;
+  runinfo.code = str(boost::format(code) % lars_coeff % lars_weight_decay % momentum);
+  runinfo.input_shapes.insert(std::make_pair("X", x_shape));
+  runinfo.input_shapes.insert(std::make_pair("Grad", x_shape));
+  runinfo.input_shapes.insert(std::make_pair("Veloc", x_shape));
+  runinfo.input_shapes.insert(std::make_pair("LR", lr_shape));
+  runinfo.output_shapes.insert(std::make_pair("NewX", x_shape));
+  runinfo.output_shapes.insert(std::make_pair("NewVeloc", x_shape));
+  runinfo.input_buffers = {
+      {"X", MakeBuffer(x_shape)},
+      {"Grad", MakeBuffer(x_shape)},
+      {"Veloc", MakeBuffer(x_shape)},
+      {"LR", MakeBuffer(lr_shape)},
+  };
+  return runinfo;
+}
+
+lang::RunInfo LoadPow(const std::string& name,  //
+                      const TensorShape& i1,    //
+                      const TensorShape& i2,    //
+                      const TensorShape& output) {
+  auto code = R"***(
+function (X, Y) -> (O) {
+    O = pow(X, Y);
+})***";
+  lang::RunInfo runinfo;
+  runinfo.program_name = name;
+  runinfo.code = code;
+  runinfo.input_shapes.insert(std::make_pair("X", i1));
+  runinfo.input_shapes.insert(std::make_pair("Y", i2));
+  runinfo.output_shapes.insert(std::make_pair("O", output));
+  runinfo.input_buffers = {
+      {"X", MakeBuffer(i1)},
+      {"Y", MakeBuffer(i2)},
+  };
+  return runinfo;
+}
+
+lang::RunInfo LoadLayerNorm4dAx2(const std::string& name,  //
+                                 const TensorShape& input) {
+  // Note: I/G/B/O should all have the same shape, so pass in one shape to share
+  const TensorShape epsilon_shape = SimpleShape(DataType::FLOAT32, {});
+  auto code = R"***(
+function (I[I0, I1, I2, I3], G[I0, I1, I2, I3], B[I0, I1, I2, I3], Epsilon[]) -> (O) {
+    H = I2 * I3;
+    Sum[i0, i1, 0, 0: I0, I1, 1, 1] = +(I[i0, i1, i2, i3]);
+    Mu = Sum / H;
+    Diff = I - Mu;
+    SqDiff = Diff * Diff;
+    SumSqDiff[i0, i1, 0, 0: I0, I1, 1, 1] = +(SqDiff[i0, i1, i2, i3]);
+    Stdev = sqrt(SumSqDiff + Epsilon) / H;
+    O = (G / Stdev) * (I - Mu) + B;
+})***";
+  lang::RunInfo runinfo;
+  runinfo.program_name = name;
+  runinfo.code = code;
+  runinfo.input_shapes.insert(std::make_pair("I", input));
+  runinfo.input_shapes.insert(std::make_pair("G", input));
+  runinfo.input_shapes.insert(std::make_pair("B", input));
+  runinfo.input_shapes.insert(std::make_pair("Epsilon", epsilon_shape));
+  runinfo.output_shapes.insert(std::make_pair("O", input));
+  runinfo.input_buffers = {
+      {"I", MakeBuffer(input)},
+      {"G", MakeBuffer(input)},
+      {"B", MakeBuffer(input)},
+      {"Epsilon", MakeBuffer(epsilon_shape)},
+  };
+  return runinfo;
+}
+
+lang::RunInfo LoadPolygonBoxTransform(const std::string& name,  //
+                                      const TensorShape& input) {
+  // Note: I and O have the same shape
+  auto code = R"***(
+function (I[N, C, H, W]) -> (O) {
+    Widx = index(I, 3);
+    TEpartial[2*n, c, h, w: N,C,H,W] = =(I[2*n, c, h, w]);
+    TE = 4 * Widx - TEpartial;
+    Hidx = index(I, 2);
+    TOpartial[2*n + 1, c, h, w: N,C,H,W] = =(I[2*n+1, c, h, w]);
+    TO = 4 * Hidx - TOpartial;
+    O = TE + TO;
+})***";
+  lang::RunInfo runinfo;
+  runinfo.program_name = name;
+  runinfo.code = code;
+  runinfo.input_shapes.insert(std::make_pair("I", input));
+  runinfo.output_shapes.insert(std::make_pair("O", input));
+  runinfo.input_buffers = {
+      {"I", MakeBuffer(input)},
+  };
+  return runinfo;
+}
+
 }  // namespace lib
 }  // namespace tile
 }  // namespace vertexai
