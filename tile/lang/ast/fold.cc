ba02780c6e07954169a6e5db53151e6672b33e86
Frank Laub <frank.laub@intel.com>
2019-07-18 17:10:10 -0700

    Improve constant folding to include heterogenous types

--- tile/lang/ast/fold.cc
+++ tile/lang/ast/fold.cc
@@ -9,14 +9,20 @@ namespace ast {
 
 namespace {
 
-template <typename NumType, typename ExprType>
+ExprPtr make_number(ExprPtr, int64_t value) { return std::make_shared<IntConst>(value); }
+DimExprPtr make_number(DimExprPtr, int64_t value) { return std::make_shared<DimIntExpr>(value); }
+PolyExprPtr make_number(PolyExprPtr, int64_t value) { return std::make_shared<PolyLiteral>(value); }
+
+ExprPtr make_number(ExprPtr, double value) { return std::make_shared<FloatConst>(value); }
+
+template <typename NumType1, typename NumType2, typename ExprType>
 std::shared_ptr<ExprType> fold_add(        //
     const std::shared_ptr<ExprType>& lhs,  //
     const std::shared_ptr<ExprType>& rhs) {
-  auto lhs_num = std::dynamic_pointer_cast<NumType>(lhs);
-  auto rhs_num = std::dynamic_pointer_cast<NumType>(rhs);
+  auto lhs_num = std::dynamic_pointer_cast<NumType1>(lhs);
+  auto rhs_num = std::dynamic_pointer_cast<NumType2>(rhs);
   if (lhs_num && rhs_num) {
-    return std::make_shared<NumType>(lhs_num->value + rhs_num->value);
+    return make_number(lhs, lhs_num->value + rhs_num->value);
   }
   if (lhs_num && lhs_num->value == 0) {
     return rhs;
@@ -27,15 +33,15 @@ std::shared_ptr<ExprType> fold_add(        //
   return nullptr;
 }
 
-template <typename NumType, typename OpExprType, typename NegOpType, typename ExprType>
+template <typename NumType1, typename NumType2, typename OpExprType, typename NegOpType, typename ExprType>
 std::shared_ptr<ExprType> fold_sub(        //
     const std::shared_ptr<ExprType>& lhs,  //
     const std::shared_ptr<ExprType>& rhs,  //
     NegOpType neg_op) {
-  auto lhs_num = std::dynamic_pointer_cast<NumType>(lhs);
-  auto rhs_num = std::dynamic_pointer_cast<NumType>(rhs);
+  auto lhs_num = std::dynamic_pointer_cast<NumType1>(lhs);
+  auto rhs_num = std::dynamic_pointer_cast<NumType2>(rhs);
   if (lhs_num && rhs_num) {
-    return std::make_shared<NumType>(lhs_num->value - rhs_num->value);
+    return make_number(lhs, lhs_num->value - rhs_num->value);
   }
   // TODO: deal with ComputeShape
   // if (lhs_num && lhs_num->value == 0) {
@@ -48,14 +54,14 @@ std::shared_ptr<ExprType> fold_sub(        //
   return nullptr;
 }
 
-template <typename NumType, typename ExprType>
+template <typename NumType1, typename NumType2, typename ExprType>
 std::shared_ptr<ExprType> fold_mul(        //
     const std::shared_ptr<ExprType>& lhs,  //
     const std::shared_ptr<ExprType>& rhs) {
-  auto lhs_num = std::dynamic_pointer_cast<NumType>(lhs);
-  auto rhs_num = std::dynamic_pointer_cast<NumType>(rhs);
+  auto lhs_num = std::dynamic_pointer_cast<NumType1>(lhs);
+  auto rhs_num = std::dynamic_pointer_cast<NumType2>(rhs);
   if (lhs_num && rhs_num) {
-    return std::make_shared<NumType>(lhs_num->value * rhs_num->value);
+    return make_number(lhs, lhs_num->value * rhs_num->value);
   }
   if (lhs_num && lhs_num->value == 1) {
     return rhs;
@@ -66,17 +72,17 @@ std::shared_ptr<ExprType> fold_mul(        //
   return nullptr;
 }
 
-template <typename NumType, typename ExprType>
+template <typename NumType1, typename NumType2, typename ExprType>
 std::shared_ptr<ExprType> fold_div(        //
     const std::shared_ptr<ExprType>& lhs,  //
     const std::shared_ptr<ExprType>& rhs) {
-  auto lhs_num = std::dynamic_pointer_cast<NumType>(lhs);
-  auto rhs_num = std::dynamic_pointer_cast<NumType>(rhs);
+  auto lhs_num = std::dynamic_pointer_cast<NumType1>(lhs);
+  auto rhs_num = std::dynamic_pointer_cast<NumType2>(rhs);
   if (lhs_num && rhs_num) {
-    return std::make_shared<NumType>(lhs_num->value / rhs_num->value);
+    return make_number(lhs, lhs_num->value / rhs_num->value);
   }
   if (lhs_num && lhs_num->value == 0) {
-    return std::make_shared<NumType>(0);
+    return lhs;
   }
   if (rhs_num && rhs_num->value == 1) {
     return lhs;
@@ -94,25 +100,25 @@ std::shared_ptr<ExprType> MakeOp(IntOp op, const std::vector<std::shared_ptr<Exp
       }
     } break;
     case IntOp::Add: {
-      auto ret = fold_add<IntType>(args[0], args[1]);
+      auto ret = fold_add<IntType, IntType>(args[0], args[1]);
       if (ret) {
         return ret;
       }
     } break;
     case IntOp::Sub: {
-      auto ret = fold_sub<IntType, OpType>(args[0], args[1], IntOp::Neg);
+      auto ret = fold_sub<IntType, IntType, OpType>(args[0], args[1], IntOp::Neg);
       if (ret) {
         return ret;
       }
     } break;
     case IntOp::Mul: {
-      auto ret = fold_mul<IntType>(args[0], args[1]);
+      auto ret = fold_mul<IntType, IntType>(args[0], args[1]);
       if (ret) {
         return ret;
       }
     } break;
     case IntOp::Div: {
-      auto ret = fold_div<IntType>(args[0], args[1]);
+      auto ret = fold_div<IntType, IntType>(args[0], args[1]);
       if (ret) {
         return ret;
       }
@@ -142,40 +148,56 @@ ExprPtr MakeCall(const std::string& fn, const std::vector<ExprPtr>& args) {
       return std::make_shared<FloatConst>(-float_expr->value);
     }
   } else if (fn == "add") {
-    auto int_ret = fold_add<IntConst>(args[0], args[1]);
-    if (int_ret) {
-      return int_ret;
+    if (auto ret = fold_add<IntConst, IntConst>(args[0], args[1])) {
+      return ret;
     }
-    auto float_ret = fold_add<FloatConst>(args[0], args[1]);
-    if (float_ret) {
-      return float_ret;
+    if (auto ret = fold_add<FloatConst, FloatConst>(args[0], args[1])) {
+      return ret;
+    }
+    if (auto ret = fold_add<IntConst, FloatConst>(args[0], args[1])) {
+      return ret;
+    }
+    if (auto ret = fold_add<FloatConst, IntConst>(args[0], args[1])) {
+      return ret;
     }
   } else if (fn == "sub") {
-    auto int_ret = fold_sub<IntConst, CallExpr>(args[0], args[1], "neg");
-    if (int_ret) {
-      return int_ret;
+    if (auto ret = fold_sub<IntConst, IntConst, CallExpr>(args[0], args[1], "neg")) {
+      return ret;
     }
-    auto float_ret = fold_sub<FloatConst, CallExpr>(args[0], args[1], "neg");
-    if (float_ret) {
-      return float_ret;
+    if (auto ret = fold_sub<FloatConst, FloatConst, CallExpr>(args[0], args[1], "neg")) {
+      return ret;
+    }
+    if (auto ret = fold_sub<IntConst, FloatConst, CallExpr>(args[0], args[1], "neg")) {
+      return ret;
+    }
+    if (auto ret = fold_sub<FloatConst, IntConst, CallExpr>(args[0], args[1], "neg")) {
+      return ret;
     }
   } else if (fn == "mul") {
-    auto int_ret = fold_mul<IntConst>(args[0], args[1]);
-    if (int_ret) {
-      return int_ret;
+    if (auto ret = fold_mul<IntConst, IntConst>(args[0], args[1])) {
+      return ret;
     }
-    auto float_ret = fold_mul<FloatConst>(args[0], args[1]);
-    if (float_ret) {
-      return float_ret;
+    if (auto ret = fold_mul<FloatConst, FloatConst>(args[0], args[1])) {
+      return ret;
+    }
+    if (auto ret = fold_mul<IntConst, FloatConst>(args[0], args[1])) {
+      return ret;
+    }
+    if (auto ret = fold_mul<FloatConst, IntConst>(args[0], args[1])) {
+      return ret;
     }
   } else if (fn == "div") {
-    auto int_ret = fold_div<IntConst>(args[0], args[1]);
-    if (int_ret) {
-      return int_ret;
+    if (auto ret = fold_div<IntConst, IntConst>(args[0], args[1])) {
+      return ret;
     }
-    auto float_ret = fold_div<FloatConst>(args[0], args[1]);
-    if (float_ret) {
-      return float_ret;
+    if (auto ret = fold_div<FloatConst, FloatConst>(args[0], args[1])) {
+      return ret;
+    }
+    if (auto ret = fold_div<IntConst, FloatConst>(args[0], args[1])) {
+      return ret;
+    }
+    if (auto ret = fold_div<FloatConst, IntConst>(args[0], args[1])) {
+      return ret;
     }
   }
   auto expr = std::make_shared<CallExpr>(fn, args);
