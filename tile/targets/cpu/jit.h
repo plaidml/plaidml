a142b3392fb3ee5279dfb56fbe164c34702a69ed
Mars Saxman <mars.saxman@intel.com>
2019-08-28 13:02:28 -0700

    enable disassembly printing option for stripejit (#456)

--- tile/targets/cpu/jit.h
+++ tile/targets/cpu/jit.h
@@ -23,6 +23,9 @@ typedef std::function<void*(std::vector<DataType>*, DataType*)> External;
 struct Config {
   bool profile_block_execution = false;
   bool profile_loop_body = false;
+  bool print_llvm_ir_simple = VLOG_IS_ON(3);
+  bool print_llvm_ir_optimized = VLOG_IS_ON(4);
+  bool print_assembly = VLOG_IS_ON(4);
   std::map<std::string, External> externals;
 };
 
