4aefb2fe0de1bc19d148f62b9ce372a539e47cf9
Denise G Kutnick <denise.g.kutnick@intel.com>
2019-09-12 13:21:16 -0700

    [skip ci] fix syntax

--- .bazelci/bazelci_windows.bat
+++ .bazelci/bazelci_windows.bat
@@ -1,6 +1,6 @@
 echo %PATH%
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe
 start /wait "" Miniconda3-latest-Windows-x86_64.exe /InstallationType=JustMe /AddToPath=1 /RegisterPython=0 /NoRegistry=0 /S /D=%UserProfile%\miniconda3
-%UserProfile%\miniconda3\Scripts\activate.bat
+CALL %UserProfile%\miniconda3\Scripts\activate.bat
 %UserProfile%\miniconda3\Scripts\conda.exe init cmd.exe
 %UserProfile%\miniconda3\Scripts\conda.exe install --file environment-windows.yml
