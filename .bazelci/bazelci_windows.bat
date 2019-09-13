8cb728cc89b8e3254fcbc8121225b32401a83fb4
Denise G Kutnick <denise.g.kutnick@intel.com>
2019-09-13 13:24:29 -0700

    [skip ci] try upgrading conda

--- .bazelci/bazelci_windows.bat
+++ .bazelci/bazelci_windows.bat
@@ -1,10 +1,9 @@
 echo %PATH%
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe
 start /wait "" Miniconda3-latest-Windows-x86_64.exe /InstallationType=JustMe /AddToPath=1 /RegisterPython=0 /NoRegistry=0 /S /D=%UserProfile%\miniconda3
-CALL %UserProfile%\miniconda3\Scripts\activate.bat
 %UserProfile%\miniconda3\Scripts\conda.exe init cmd.exe
-%UserProfile%\miniconda3\Scripts\conda.exe update -n base conda
 %UserProfile%\miniconda3\Scripts\conda.exe env update --file environment-windows.yml
+%UserProfile%\miniconda3\Scripts\conda.exe update -n base conda
 set PLAIDML_DEVICE_IDS=llvm_cpu.0
 set PLAIDML_TARGET=llvm_cpu
 set PLAIDML_EXPERIMENTAL=1
