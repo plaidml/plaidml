f2ed713353e5b90f7824ccc1dc1aa102a29e981a
Denise G Kutnick <denise.g.kutnick@intel.com>
2019-09-13 08:02:34 -0700

    [skip ci] fix syntax

--- .bazelci/bazelci_windows.bat
+++ .bazelci/bazelci_windows.bat
@@ -4,3 +4,5 @@ start /wait "" Miniconda3-latest-Windows-x86_64.exe /InstallationType=JustMe /Ad
 CALL %UserProfile%\miniconda3\Scripts\activate.bat
 %UserProfile%\miniconda3\Scripts\conda.exe init cmd.exe
 %UserProfile%\miniconda3\Scripts\conda.exe env update --file environment-windows.yml
+wget https://releases.bazel.build/0.28.1/release/bazel-0.28.1-windows-x86_64.exe
+bazel-0.28.1-windows-x86_64.exe test //...
