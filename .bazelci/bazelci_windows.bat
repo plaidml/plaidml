a52cbe2abe3f287741ae701142db8d1f6be0d51f
Denise G Kutnick <denise.g.kutnick@intel.com>
2019-09-13 13:53:00 -0700

    [skip ci] try building wheel

--- .bazelci/bazelci_windows.bat
+++ .bazelci/bazelci_windows.bat
@@ -4,9 +4,8 @@ start /wait "" Miniconda3-latest-Windows-x86_64.exe /InstallationType=JustMe /Ad
 CALL %UserProfile%\miniconda3\Scripts\activate.bat
 %UserProfile%\miniconda3\Scripts\conda.exe init cmd.exe
 %UserProfile%\miniconda3\Scripts\conda.exe env update --file environment-windows.yml
-%UserProfile%\miniconda3\Scripts\conda.exe update -n base conda
 set PLAIDML_DEVICE_IDS=llvm_cpu.0
 set PLAIDML_TARGET=llvm_cpu
 set PLAIDML_EXPERIMENTAL=1
 wget https://releases.bazel.build/0.28.1/release/bazel-0.28.1-windows-x86_64.exe
-bazel-0.28.1-windows-x86_64.exe test //... --config=windows_x86_64
+bazel-0.28.1-windows-x86_64.exe build //plaidml:wheel --config=windows_x86_64
