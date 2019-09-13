23eee0e526190829f935a4d29bd8efce57764fab
Denise G Kutnick <denise.g.kutnick@intel.com>
2019-09-13 13:24:05 -0700

    [skip ci] try upgrading conda

--- .bazelci/bazelci_windows.bat
+++ .bazelci/bazelci_windows.bat
@@ -1,6 +1,7 @@
 echo %PATH%
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe
 start /wait "" Miniconda3-latest-Windows-x86_64.exe /InstallationType=JustMe /AddToPath=1 /RegisterPython=0 /NoRegistry=0 /S /D=%UserProfile%\miniconda3
+CALL %UserProfile%\miniconda3\Scripts\activate.bat
 %UserProfile%\miniconda3\Scripts\conda.exe init cmd.exe
 %UserProfile%\miniconda3\Scripts\conda.exe update -n base conda
 %UserProfile%\miniconda3\Scripts\conda.exe env update --file environment-windows.yml
