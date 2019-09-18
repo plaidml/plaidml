8b861df8f3dbc34caae136d6ae50170e751249be
Matt Westervelt <matthew.westervelt@intel.com>
2019-09-17 17:43:02 -0700

    planner

--- ci/planner.py
+++ ci/planner.py
@@ -5,19 +5,21 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 import yaml
+import util
 
 
 class Planner(GridLayout):
 
     def __init__(self, **kwargs):
         super(Planner, self).__init__(**kwargs)
-
         with open('ci/plan.yml') as file_:
             plan = yaml.safe_load(file_)
-
+        self.cols = 3
         variants = []
         for variant in plan['VARIANTS'].keys():
-            self.add_widget(Label(text=variant))
+            self.add_widget(Label(text=variant, col=1))
+
+        tests = []
 
 
 class MyApp(App):
