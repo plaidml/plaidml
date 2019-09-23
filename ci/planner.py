2baf274022bd4be473b9257ee03cd28104e09da2
Matt Westervelt <matthew.westervelt@intel.com>
2019-09-23 16:20:29 -0700

    testing kivy widgets

--- ci/planner.py
+++ ci/planner.py
@@ -3,30 +3,60 @@ kivy.require('1.11.1')  # replace with your current kivy version !
 
 from kivy.app import App
 from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from functools import partial
+
+from kivy.uix.boxlayout import BoxLayout
 import yaml
 import util
 
 
-class Planner(GridLayout):
+class Planner(BoxLayout):
 
     def __init__(self, **kwargs):
         super(Planner, self).__init__(**kwargs)
+
         with open('ci/plan.yml') as file_:
             plan = yaml.safe_load(file_)
-        self.cols = 3
+
+        def callback(instance):
+            print("button pressed: " + instance.text)
+            for pkey, platform in plan['PLATFORMS'].items():
+                print(pkey, platform)
+                pinfo = plan['PLATFORMS'][pkey]
+                variant = pinfo['variant']
+                displayed = []
+                if instance.text in variant:
+                    self.platform = Button(text=pkey)
+                    self.add_widget(self.platform)
+                    displayed.append(self.platform)
+            print(*displayed)
+
+            # for platform in plan['PLATFORMS'].keys():
+            #     self.add_widget(Button(text=platform))
+
+        self.add_widget(self.label)
+
         variants = []
         for variant in plan['VARIANTS'].keys():
-            self.add_widget(Label(text=variant, col=1))
+            self.variant = Button(text=variant)
+            self.variant.bind(on_press=callback)
+            self.add_widget(self.variant)
+        # platforms = []
+        # for platform in plan['PLATFORMS'].keys():
+        #     self.add_widget(Button(text=platform))
+        #suites = []
+        #for suite in plan['SUITES'].keys():
+        #self.add_widget(Button(text=suite))
 
-        tests = []
 
-
-class MyApp(App):
+class PlannerApp(App):
 
     def build(self):
         return Planner()
 
 
 if __name__ == '__main__':
-    MyApp().run()
+    layout = BoxLayout(padding=10)
+    label = Label(text="PlaidML")
+    PlannerApp().run()
