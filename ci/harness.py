c97f13c8e4a7defa455c741808653fc336e6adc5
Frank Laub <frank.laub@intel.com>
2019-09-27 10:41:59 -0700

    Allow disabling of stripe

--- ci/harness.py
+++ ci/harness.py
@@ -102,6 +102,10 @@ def run(args, remainder):
 
     if 'stripe' in args.platform:
         env['PLAIDML_USE_STRIPE'] = '1'
+        if 'llvm' in args.platform:
+            env['STRIPE_JIT'] = '1'
+    else:
+        env['PLAIDML_USE_STRIPE'] = '0'
     if 'cuda' in args.platform:
         env['CUDA_DEVICE_ORDER'] = buildkite_metadata('CUDA_CUDA_DEVICE_ORDER', 'PCI_BUS_ID')
         env['CUDA_VISIBLE_DEVICES'] = buildkite_metadata('CUDA_VISIBLE_DEVICES', '0')
