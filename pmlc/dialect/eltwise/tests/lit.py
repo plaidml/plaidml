c898396a09fbe57a264c5672eae6324064739057
Frank Laub <frank.laub@intel.com>
2019-10-18 20:37:11 -0700

    Integrate lit testing

--- pmlc/dialect/eltwise/tests/lit.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import os
-import subprocess
-import sys
-
-lit = os.path.join('external', 'llvm', 'lit')
-sys.exit(subprocess.run([lit, os.path.dirname(__file__), '-v'] + sys.argv[1:]).returncode)
