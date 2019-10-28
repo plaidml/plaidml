8fa921596b38de6d005170ccdd5a78ee2370078c
Jeremy Bruestle <jeremy.bruestle@intel.com>
2019-10-28 11:13:42 -0700

    Renabled demo tiling pass

--- pmlc/dialect/stripe/tile.cc
+++ pmlc/dialect/stripe/tile.cc
@@ -7,7 +7,6 @@ namespace dialect {
 namespace stripe {
 
 void Tile(ParallelForOp op, llvm::ArrayRef<int64_t> tile_sizes) {
-  /*
   // Make a builder to write to just before the terminator
   Block* obody = op.getBody();
   auto builder = op.getBodyBuilder();
@@ -19,41 +18,39 @@ void Tile(ParallelForOp op, llvm::ArrayRef<int64_t> tile_sizes) {
     inner.getOperation()->setAttr("idx_names", names);
   }
   Block* ibody = inner.getBody();
-  OpBuilder ibuild = inner.getBodyBuilder();
-
-  auto aff_type = AffineType::get(ibuild.getContext());
+  Block* cbody = ibody;
+  OpBuilder cbuild = inner.getBodyBuilder();
 
   // Compute the new ranges, make the new affines, and add new constraints
   llvm::SmallVector<int64_t, 8> new_ranges;
   for (size_t i = 0; i < op.ranges().size(); i++) {
     // Compute new range and save it for later updating
     new_ranges.push_back((op.getRange(i) + tile_sizes[i] - 1) / tile_sizes[i]);
-    // Recreate original index value
-    auto mouter = ibuild.create<AffineMulOp>(op.getLoc(), ibody->getArgument(i), tile_sizes[i]);
-    auto computed = ibuild.create<AffineAddOp>(op.getLoc(), mouter, ibody->getArgument(i));
-    // new_values.push_back(computed);
-    obody->getArgument(i)->replaceAllUsesWith(computed);
-    mouter.getOperation()->setOperand(0, obody->getArgument(i));
+
+    // Make a polynomial to represent the computed index value, replace uses of
+    // old value, then update poly to be the correct computation
+    auto computed_idx = cbuild.create<AffinePolyOp>(op.getLoc(), AffinePolynomial());
+    obody->getArgument(i)->replaceAllUsesWith(computed_idx);
+    computed_idx.setAttr("coeffs", cbuild.getI64ArrayAttr({tile_sizes[i], 1}));
+    computed_idx.getOperation()->setOperands({obody->getArgument(i), ibody->getArgument(i)});
 
     // Check if it's an uneven split, and if so, make a constraint
     if (op.getRange(i) % tile_sizes[i] != 0) {
-      auto neg = ibuild.create<AffineMulOp>(op.getLoc(), computed, int64_t(-1));
-      auto orange = ibuild.create<AffineConstOp>(op.getLoc(), aff_type, ibuild.getI64IntegerAttr(op.getRange(i) - 1));
-      auto cexpr = ibuild.create<AffineAddOp>(op.getLoc(), neg, orange);
-      auto aif = ibuild.create<ConstraintOp>(op.getLoc(), cexpr);
-      ibody = new Block();
-      aif.getOperation()->getRegion(0).push_back(ibody);
-      ibuild.setInsertionPointToStart(ibody);
-      ibuild.create<TerminateOp>(op.getLoc());
-      ibuild.setInsertionPointToStart(ibody);
+      auto cpoly = AffinePolynomial(computed_idx.result()) * -1 + AffinePolynomial(op.getRange(i) - 1);
+      auto cexpr = cbuild.create<AffinePolyOp>(op.getLoc(), cpoly);
+      auto aif = cbuild.create<ConstraintOp>(op.getLoc(), cexpr);
+      cbody = new Block();
+      aif.getOperation()->getRegion(0).push_back(cbody);
+      cbuild.setInsertionPointToStart(cbody);
+      cbuild.create<TerminateOp>(op.getLoc());
+      cbuild.setInsertionPointToStart(cbody);
     }
   }
   // Update outer ranges
   op.getOperation()->setAttr("ranges", builder.getI64ArrayAttr(new_ranges));
   // Move the rest of the code into the interior
-  ibody->getOperations().splice(std::prev(ibody->end(), 1), obody->getOperations(), obody->begin(),
+  cbody->getOperations().splice(std::prev(cbody->end(), 1), obody->getOperations(), obody->begin(),
                                 std::prev(obody->end(), 2));
-  */
 }
 
 }  // namespace stripe
