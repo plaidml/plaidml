7ce4e8c2b329986bc87691c0e236e49b0980b0b7
Sana Nazir <snazir@snazir-mac01.local>
2019-11-06 20:02:15 -0800

    UW sample code

---
+++ networks/scitile/toroidal_example/min_main.py
@@ -0,0 +1,16 @@
+import numpy as np
+from min_parallel import gridintegrate
+
+N = 128  # number of grid points along each coord direction
+MINVAL, MAXVAL = -1.25, 1.25  # coordinate bounding values
+
+
+def main():
+    coordvals = np.linspace(MINVAL, MAXVAL, N, dtype=np.float64)
+    delta = (MAXVAL - MINVAL) / (N - 1)  # grid spacing
+    res = gridintegrate(coordvals, delta)
+    print('Value of the integral is ', res)
+
+
+if __name__ == '__main__':
+    main()
