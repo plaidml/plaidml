dad2ea1c39888be4825bf51501966f15f3f7e48e
Sana Nazir <snazir@snazir-mac01.local>
2019-11-06 19:07:47 -0800

    uw_tests tolerance for slightly varying answers on diff h/w

--- networks/scitile/uw_tests.py
+++ networks/scitile/uw_tests.py
@@ -1,7 +1,11 @@
 import unittest
+import numpy.testing as npt
 import os
 
 from networks.scitile.uw_toroidal_shell import toroidal_shell_integral
+
+DEFAULT_TOL = 1e-3
+DEFAULT_ATOL = 1e-8
 
 
 class UWTest(unittest.TestCase):
@@ -12,7 +16,8 @@ class UWTest(unittest.TestCase):
         minval, maxval = -1.25, 1.25
         eps = 1.0e-8
         result = toroidal_shell_integral(n, minval, maxval, eps)
-        self.assertEqual(result, 3.9926786915581705)
+        npt.assert_allclose(result, 3.9926786915581705, rtol=DEFAULT_TOL, atol=DEFAULT_ATOL)
+        #self.assertEqual(result, 3.9926786915581705)
 
 
 if __name__ == '__main__':
