6efde04df2a8b569ed5ca1b839bb8d39eb9cc286
Sana Nazir <snazir@snazir-mac01.local>
2019-12-16 17:57:15 -0800

    4D integrals

---
+++ networks/scitile/storti_integrals/hypersphere.py
@@ -0,0 +1,94 @@
+from op import *
+
+import matplotlib.pyplot as plt
+
+
+def hypersphere_volume_exact(r):
+    return (math.pi**2) * 0.5 * (r**4)
+
+
+def hypersphere_area_exact(r):
+    return 2 * (math.pi**2) * (r**3)
+
+
+def hypersphere(X, Y, Z, W, vars):
+    R = vars[0]  #radius
+    p = vars[1]  #dim
+    #return edsl.sqrt(sq(X) + sq(Y) + sq(Z) + sq(W)) - 1
+    return edsl.pow(edsl.pow(X, p) + edsl.pow(Y, p) + edsl.pow(Z, p) + edsl.pow(W, p), (1 / p)) - R
+
+
+def generate_plot_data(min_N, max_N, jump, R, p, check_integral, type):
+    error_chart = np.array([])
+    error_chart_N_by_delta = np.array([])
+
+    for N in range(min_N, max_N, jump):
+        print("evaluating for N: {}".format(N))
+        minval = -1.25 * R
+        maxval = 1.25 * R
+        delta = (maxval - minval) / (N - 1)  # grid spacing
+        eps = 1.0e-8
+        error = check_integral(type, N, minval, maxval, eps, hypersphere, [R, p])
+        error_chart = np.append(error_chart, math.log(error, 10))
+        error_chart_N_by_delta = np.append(error_chart_N_by_delta, math.log((R / delta), 10))
+
+    return [error_chart_N_by_delta, error_chart]
+
+
+def get_line_y(x, y):
+    den = x.dot(x) - x.mean() * x.sum()
+    m = (x.dot(y) - y.mean() * x.sum()) / den
+    b = (y.mean() * x.dot(x) - x.mean() * x.dot(y)) / den
+    y_line = (m * x + b)
+    res = y - y_line
+    tot = y - y.mean()
+    R_sq = 1 - res.dot(res) / tot.dot(tot)
+    label = "f(x) = " + '%.2f' % m + "x +" + '%.2f' % b + "| R^2 = " + '%.2f' % R_sq
+    return [y_line, label]
+
+
+def main(
+):  #TODO:work in progress to generate graphs will clean up after documentation is completed
+    #TODO:incorrect results produced
+    #TODO:write tests once 4Dintegral correctness proof is found
+    R = 1.0  # radius
+    p = 2
+
+    min_N = 32
+    max_N = 65
+    interval = 32
+
+    def check_integral(type, N, minval, maxval, eps, frep, vars):
+        if type == 'volume':
+            exact_value = hypersphere_volume_exact(R)
+            result = integral_volume_4D(N, minval, maxval, eps, frep, vars)
+        if type == 'surface_area':
+            exact_value = hypersphere_area_exact(R)
+            result = integral_surface_area_4D(N, minval, maxval, eps, frep, vars,
+                                              integrand_empty_4D)
+        print("Exact value: {}".format(exact_value))
+        print("computed result using integral: {}".format(result))
+        return (abs(result - exact_value) / exact_value) * 100
+
+    fig = plt.figure()
+    fig.suptitle("convergence study Genus 1 : hypersphere - 2 ", fontsize=14)
+    ax1 = fig.add_subplot(111)
+    ax1.set(xlabel='log(R/delta)', ylabel='log(error percentage)')
+
+    x1, y1 = generate_plot_data(min_N, max_N, interval, R, p, check_integral, 'volume')
+    x2, y2 = generate_plot_data(min_N, max_N, interval, R, p, check_integral, 'surface_area')
+
+    ax1.scatter(x1, y1, label='Volume')
+    ax1.scatter(x2, y2, label='SurfaceArea')
+
+    y_line1, label1 = get_line_y(x1, y1)
+    ax1.plot(x1, y_line1, label=label1)
+    y_line2, label2 = get_line_y(x2, y2)
+    ax1.plot(x2, y_line2, label=label2)
+
+    plt.legend(loc='lower left')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
--- networks/scitile/storti_integrals/op.py
+++ networks/scitile/storti_integrals/op.py
@@ -19,6 +19,13 @@ def sum(R):
     return O
 
 
+def sum_4D(R):
+    idxs = edsl.TensorIndexes(4)
+    O = edsl.TensorOutput()
+    O[()] += R[idxs]
+    return O
+
+
 def meshgrid(
         n,  # number of grid points along each coord direction
         minval,  # coordinate bounding values
@@ -32,6 +39,24 @@ def meshgrid(
     Y = edsl.Tensor(edsl.LogicalShape(plaidml.DType.FLOAT32, Y_data.shape))
     Z = edsl.Tensor(edsl.LogicalShape(plaidml.DType.FLOAT32, Z_data.shape))
     return X, Y, Z, X_data, Y_data, Z_data
+
+
+def meshgrid_4D(
+        n,  # number of grid points along each coord direction
+        minval,  # coordinate bounding values
+        maxval):  # coordinate bounding values
+    coordvals = np.linspace(minval, maxval, n, dtype=np.float32)
+    delta = (maxval - minval) / (n - 1)  # grid spacing
+    X_data = coordvals.reshape(n, 1, 1, 1)
+    Y_data = coordvals.reshape(1, n, 1, 1)
+    Z_data = coordvals.reshape(1, 1, n, 1)
+    W_data = coordvals.reshape(1, 1, 1, n)
+
+    X = edsl.Tensor(edsl.LogicalShape(plaidml.DType.FLOAT32, X_data.shape))
+    Y = edsl.Tensor(edsl.LogicalShape(plaidml.DType.FLOAT32, Y_data.shape))
+    Z = edsl.Tensor(edsl.LogicalShape(plaidml.DType.FLOAT32, Z_data.shape))
+    W = edsl.Tensor(edsl.LogicalShape(plaidml.DType.FLOAT32, W_data.shape))
+    return X, Y, Z, W, X_data, Y_data, Z_data, W_data
 
 
 def partial(
@@ -52,13 +77,44 @@ def partial(
     return O / (2.0 * delta)
 
 
+def partial_4D(
+        F,  # F: differentiable function tensor
+        wrt,  # wrt: ('x' | 'y' | 'z')
+        delta):  # delta: grid spacing
+    F_neg = -F
+    dims = edsl.TensorDims(4)
+    x, y, z, w = edsl.TensorIndexes(4)
+    F.bind_dims(*dims)
+    O = edsl.TensorOutput(*dims)
+    if wrt == 'x':
+        O[x, y, z, w] = F[x + 1, y, z, w] + F_neg[x - 1, y, z, w]
+    elif wrt == 'y':
+        O[x, y, z, w] = F[x, y + 1, z, w] + F_neg[x, y - 1, z, w]
+    elif wrt == 'z':
+        O[x, y, z, w] = F[x, y, z + 1, w] + F_neg[x, y, z - 1, w]
+    elif wrt == 'w':
+        O[x, y, z, w] = F[x, y, z, w + 1] + F_neg[x, y, z, w - 1]
+    return O / (2.0 * delta)
+
+
 def grad(
         F,  # F: differentiable function tensor
         delta):  # delta: grid spacing
     return partial(F, 'x', delta) + partial(F, 'y', delta) + partial(F, 'z', delta)
 
 
+def grad_4D(
+        F,  # F: differentiable function tensor
+        delta):  # delta: grid spacing
+    return partial_4D(F, 'x', delta) + partial_4D(F, 'y', delta) + partial_4D(
+        F, 'z', delta) + partial_4D(F, 'w', delta)
+
+
 def integrand_empty(X, Y, Z):
+    return 1
+
+
+def integrand_empty_4D(X, Y, Z, W):
     return 1
 
 
@@ -76,8 +132,8 @@ def integral_surface_area(
 
     X, Y, Z, X_data, Y_data, Z_data = meshgrid(n, minval, maxval)
     F = frep(X, Y, Z, frep_vars)
-    # F = frep_torus(X, Y, Z, R, r)
     G = integrand(X, Y, Z)
+
     DFDX = partial(F, 'x', delta)
     DFDY = partial(F, 'y', delta)
     DFDZ = partial(F, 'z', delta)
@@ -94,6 +150,43 @@ def integral_surface_area(
     O = sum(-H * G)
 
     result = run_program(X, Y, Z, X_data, Y_data, Z_data, O, benchmark)
+    return result * (delta**3)
+
+
+def integral_surface_area_4D(
+        n,  # number of grid points along each coord direction
+        minval,  # coordinate bounding values
+        maxval,  # coordinate bounding values
+        eps,  # Threshold for trivial gradient
+        frep,  # function 
+        frep_vars,  #functno rep variables
+        integrand,  # integrand 
+        benchmark=False):  # benchmark: get timing information
+
+    delta = (maxval - minval) / (n - 1)  # grid spacing
+
+    X, Y, Z, W, X_data, Y_data, Z_data, W_data = meshgrid_4D(n, minval, maxval)
+    F = frep(X, Y, Z, W, frep_vars)
+    G = integrand(X, Y, Z, W)
+
+    DFDX = partial_4D(F, 'x', delta)
+    DFDY = partial_4D(F, 'y', delta)
+    DFDZ = partial_4D(F, 'z', delta)
+    DFDW = partial_4D(F, 'w', delta)
+
+    # chi: occupancy function: 1 inside the region (f<0), 0 outside the region (f>0)
+    CHI = edsl.select(F > 0, 0, 1)
+    DCHIDX = partial_4D(CHI, 'x', delta)
+    DCHIDY = partial_4D(CHI, 'y', delta)
+    DCHIDZ = partial_4D(CHI, 'z', delta)
+    DCHIDW = partial_4D(CHI, 'w', delta)
+
+    NUMER = DFDX * DCHIDX + DFDY * DCHIDY + DFDZ * DCHIDZ + DFDW * DCHIDW
+    DENOM = edsl.sqrt(sq(DFDX) + sq(DFDY) + sq(DFDZ) + sq(DFDW))
+    H = edsl.select(DENOM < eps, 0, NUMER / DENOM)
+    O = sum_4D(-H * G)
+
+    result = run_program_4D(X, Y, Z, W, X_data, Y_data, Z_data, W_data, O, benchmark)
     return result * (delta**3)
 
 
@@ -123,8 +216,34 @@ def integral_volume(
     return result * (delta**3)
 
 
+def integral_volume_4D(
+        n,  # number of grid points along each coord direction
+        minval,  # coordinate bounding values
+        maxval,  # coordinate bounding values
+        eps,  # Threshold for trivial gradient
+        frep,  # function rep
+        frep_vars,  # function rep variables
+        benchmark=False):  # benchmark: get timing information
+
+    delta = (maxval - minval) / (n - 1)  # grid spacing
+
+    X, Y, Z, W, X_data, Y_data, Z_data, W_data = meshgrid_4D(n, minval, maxval)
+    F = frep(X, Y, Z, W, frep_vars)
+
+    PHI = (X + Y + Z + W) / 4.0
+    # chi: occupancy function: 1 inside the region (f<0), 0 outside the region (f>0)
+    CHI = edsl.select(F > 0, 0, 1)
+
+    DelCHI = grad_4D(CHI, delta)
+    O = sum_4D(-PHI * DelCHI)
+
+    result = run_program_4D(X, Y, Z, W, X_data, Y_data, Z_data, W_data, O, benchmark)
+
+    return result * (delta**3)
+
+
 def run_program(X, Y, Z, X_data, Y_data, Z_data, O, benchmark=False):
-    program = edsl.Program('torus_volume', [O])
+    program = edsl.Program('integral_program', [O])
     binder = plaidml_exec.Binder(program)
     executable = binder.compile()
 
@@ -132,6 +251,34 @@ def run_program(X, Y, Z, X_data, Y_data, Z_data, O, benchmark=False):
         binder.input(X).copy_from_ndarray(X_data)
         binder.input(Y).copy_from_ndarray(Y_data)
         binder.input(Z).copy_from_ndarray(Z_data)
+        executable.run()
+        return binder.output(O).as_ndarray()
+
+    if benchmark:
+        # the first run will compile and run
+        print('compiling...')
+        result = run()
+
+        # subsequent runs should not include compile time
+        print('running...')
+        ITERATIONS = 100
+        elapsed = timeit.timeit(run, number=ITERATIONS)
+        print('runtime:', elapsed / ITERATIONS)
+    else:
+        result = run()
+    return result
+
+
+def run_program_4D(X, Y, Z, W, X_data, Y_data, Z_data, W_data, O, benchmark=False):
+    program = edsl.Program('4D_integral_program', [O])
+    binder = plaidml_exec.Binder(program)
+    executable = binder.compile()
+
+    def run():
+        binder.input(X).copy_from_ndarray(X_data)
+        binder.input(Y).copy_from_ndarray(Y_data)
+        binder.input(Z).copy_from_ndarray(Z_data)
+        binder.input(W).copy_from_ndarray(W_data)
         executable.run()
         return binder.output(O).as_ndarray()
 
