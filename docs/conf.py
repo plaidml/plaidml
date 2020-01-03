21ca83ab64569b7264792f3524dc3182727a749e
Denise G Kutnick <denise.g.kutnick@intel.com>
2020-01-03 15:07:39 -0800

    add watch to bazel and various sphinx upgrades

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
@@ -47,6 +47,9 @@ extensions = [
     'breathe',
     'sphinx_rtd_theme',
     'sphinxcontrib.katex',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.autodoc',
+    'sphinx_tabs.tabs',
 ]
 
 # Setup the breathe extension
