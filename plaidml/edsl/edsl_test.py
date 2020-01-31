39f9225e99fb447fb884090e253d1405f630fc01
Denise Kutnick <denise.g.kutnick@intel.com>
2020-01-30 18:51:07 -0800

    Add Placeholder API for Python (#891)
    * add placeholder API for python
    
    * fixes
    
    * fixes
    
    * better tests
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- plaidml/edsl/edsl_test.py
+++ plaidml/edsl/edsl_test.py
@@ -158,7 +158,7 @@ def arg_max(I):
     I.bind_dims(X0, X1, X2)
     Max = TensorOutput(X0, X2)
     Max[x0, x2] >= I[x0, x1, x2]
-    One = Tensor(LogicalShape(plaidml.DType.FLOAT32))
+    One = Placeholder(plaidml.DType.FLOAT32)
     T = TensorOutput(X1)
     T[x1] = One[()]
     IX = index(T, 0)
@@ -297,7 +297,7 @@ module {
         self.assertEqual(outputs[0].tolist(), [[4, 5, 6], [7, 8, 9], [10, 11, 12]])
 
     def test_sum_over_axis(self):
-        I = Tensor(LogicalShape(plaidml.DType.FLOAT32, [1, 784]))
+        I = Placeholder(plaidml.DType.FLOAT32, [1, 784])
         O = sum_over_axis(I)
         program = Program('sum_over_axis', [O])
         expected = '''
@@ -317,7 +317,7 @@ module {
         self.compare_results(program, expected)
 
     def test_max_over_axis(self):
-        I = Tensor(LogicalShape(plaidml.DType.FLOAT32, [1, 784]))
+        I = Placeholder(plaidml.DType.FLOAT32, [1, 784])
         O = max_over_axis(I)
         program = Program('max_over_axis', [O])
         expected = '''
@@ -337,8 +337,8 @@ module {
         self.compare_results(program, expected)
 
     def test_matmul(self):
-        A = Tensor(LogicalShape(plaidml.DType.FLOAT32, [1, 784]))
-        B = Tensor(LogicalShape(plaidml.DType.FLOAT32, [784, 784]))
+        A = Placeholder(plaidml.DType.FLOAT32, [1, 784])
+        B = Placeholder(plaidml.DType.FLOAT32, [784, 784])
         O = matmul(A, B)
         program = Program('matmul', [O])
         expected = '''
@@ -359,7 +359,7 @@ module {
         self.compare_results(program, expected)
 
     def test_avg(self):
-        I = Tensor(LogicalShape(plaidml.DType.FLOAT32, [1, 784]))
+        I = Placeholder(plaidml.DType.FLOAT32, [1, 784])
         O = avg(I)
         program = Program('avg', [O])
         expected = '''
@@ -379,7 +379,7 @@ module {
         self.compare_results(program, expected)
 
     def test_avg_stages(self):
-        I = Tensor(LogicalShape(plaidml.DType.FLOAT32, [1, 784]))
+        I = Placeholder(plaidml.DType.FLOAT32, [1, 784])
         O = avg_stages(I)
         program = Program('avg_stages', [O])
         expected = '''
@@ -401,7 +401,7 @@ module {
         self.compare_results(program, expected)
 
     def test_avg_merge(self):
-        I = Tensor(LogicalShape(plaidml.DType.FLOAT32, [1, 784]))
+        I = Placeholder(plaidml.DType.FLOAT32, [1, 784])
         O = avg_merge(I)
         program = Program('avg_merge', [O])
         expected = '''
@@ -423,7 +423,7 @@ module {
         self.compare_results(program, expected)
 
     def test_max_pool_1d(self):
-        I = Tensor(LogicalShape(plaidml.DType.FLOAT32, [10]), name='I')
+        I = Placeholder(plaidml.DType.FLOAT32, [10], name='I')
         O = max_pool_1d(I)
         program = Program('max_pool_1d', [O])
         expected = '''
@@ -444,7 +444,7 @@ module {
         self.compare_results(program, expected)
 
     def test_skip(self):
-        I = Tensor(LogicalShape(plaidml.DType.FLOAT32, [1, 784]))
+        I = Placeholder(plaidml.DType.FLOAT32, [1, 784])
         O = skip(I)
         program = Program('skip', [O])
         expected = '''
@@ -464,8 +464,8 @@ module {
         self.compare_results(program, expected)
 
     def test_conv_1d(self):
-        I = Tensor(LogicalShape(plaidml.DType.FLOAT32, [1, 224, 3]))
-        K = Tensor(LogicalShape(plaidml.DType.FLOAT32, [3, 3, 1]))
+        I = Placeholder(plaidml.DType.FLOAT32, [1, 224, 3])
+        K = Placeholder(plaidml.DType.FLOAT32, [3, 3, 1])
         O = conv_1d(I, K)
         program = Program('conv_1d', [O])
         expected = '''
@@ -486,8 +486,8 @@ module {
         self.compare_results(program, expected)
 
     def test_conv_2d_dilated(self):
-        I = Tensor(LogicalShape(plaidml.DType.FLOAT32, [1, 224, 224, 1]))
-        K = Tensor(LogicalShape(plaidml.DType.FLOAT32, [3, 3, 1, 32]))
+        I = Placeholder(plaidml.DType.FLOAT32, [1, 224, 224, 1])
+        K = Placeholder(plaidml.DType.FLOAT32, [3, 3, 1, 32])
         O = conv_2d_dilated(I, K)
         program = Program('conv_2d_dilated', [O])
         expected = '''
@@ -508,8 +508,8 @@ module {
         self.compare_results(program, expected)
 
     def test_complex_conv_2d(self):
-        I = Tensor(LogicalShape(plaidml.DType.FLOAT32, [1, 224, 224, 3, 3]))
-        K = Tensor(LogicalShape(plaidml.DType.FLOAT32, [3, 3, 3, 3, 32]))
+        I = Placeholder(plaidml.DType.FLOAT32, [1, 224, 224, 3, 3])
+        K = Placeholder(plaidml.DType.FLOAT32, [3, 3, 3, 3, 32])
         O = complex_conv_2d(I, K, 1, 2, 1, 2)
         program = Program('complex_conv_2d', [O])
         expected = '''
@@ -532,8 +532,8 @@ module {
     @unittest.skip(
         'TODO: currently segfaults mismatched dimensions error needs to be printed correctly')
     def test_complex_conv_2d_dim_mismatch(self):
-        I = Tensor(LogicalShape(plaidml.DType.FLOAT32, [1, 1, 1, 1, 1]))
-        K = Tensor(LogicalShape(plaidml.DType.FLOAT32, [1, 1, 1, 1, 1]))
+        I = Placeholder(plaidml.DType.FLOAT32, [1, 1, 1, 1, 1])
+        K = Placeholder(plaidml.DType.FLOAT32, [1, 1, 1, 1, 1])
         O = complex_conv_2d(I, K, 1, 2, 1, 2)
         program = Program('complex_conv_2d', [O])
         # expected = '''?'''
@@ -541,17 +541,17 @@ module {
 
     def test_mnist_mlp(self):
         # model.add(Dense(512, activation='relu', input_shape=(784,)))
-        I = Tensor(LogicalShape(plaidml.DType.FLOAT32, [1, 784]))
-        K1 = Tensor(LogicalShape(plaidml.DType.FLOAT32, [784, 512]))
-        B1 = Tensor(LogicalShape(plaidml.DType.FLOAT32, [512]))
+        I = Placeholder(plaidml.DType.FLOAT32, [1, 784])
+        K1 = Placeholder(plaidml.DType.FLOAT32, [784, 512])
+        B1 = Placeholder(plaidml.DType.FLOAT32, [512])
         D1 = relu(dot(I, K1) + B1)
         # model.add(Dense(512, activation='relu'))
-        K2 = Tensor(LogicalShape(plaidml.DType.FLOAT32, [512, 512]))
-        B2 = Tensor(LogicalShape(plaidml.DType.FLOAT32, [512]))
+        K2 = Placeholder(plaidml.DType.FLOAT32, [512, 512])
+        B2 = Placeholder(plaidml.DType.FLOAT32, [512])
         D2 = relu(dot(D1, K2) + B2)
         # model.add(Dense(10, activation='softmax'))
-        K3 = Tensor(LogicalShape(plaidml.DType.FLOAT32, [512, 10]))
-        B3 = Tensor(LogicalShape(plaidml.DType.FLOAT32, [10]))
+        K3 = Placeholder(plaidml.DType.FLOAT32, [512, 10])
+        B3 = Placeholder(plaidml.DType.FLOAT32, [10])
         D3 = softmax(dot(D2, K3) + B3)
         program = Program('mnist_mlp', [D3])
         expected = '''
@@ -589,25 +589,25 @@ module {
 
     def test_mnist_cnn(self):
         # model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
-        I = Tensor(LogicalShape(plaidml.DType.FLOAT32, [1, 224, 224, 1]))
-        K1 = Tensor(LogicalShape(plaidml.DType.FLOAT32, [3, 3, 1, 32]))
-        B1 = Tensor(LogicalShape(plaidml.DType.FLOAT32, [32]))
+        I = Placeholder(plaidml.DType.FLOAT32, [1, 224, 224, 1])
+        K1 = Placeholder(plaidml.DType.FLOAT32, [3, 3, 1, 32])
+        B1 = Placeholder(plaidml.DType.FLOAT32, [32])
         C1 = relu(conv_2d(I, K1) + B1)
         # model.add(Conv2D(64, (3, 3), activation='relu'))
-        K2 = Tensor(LogicalShape(plaidml.DType.FLOAT32, [3, 3, 32, 64]))
-        B2 = Tensor(LogicalShape(plaidml.DType.FLOAT32, [64]))
+        K2 = Placeholder(plaidml.DType.FLOAT32, [3, 3, 32, 64])
+        B2 = Placeholder(plaidml.DType.FLOAT32, [64])
         C2 = relu(conv_2d(C1, K2) + B2)
         # model.add(MaxPooling2D(pool_size=(2, 2)))
         P1 = max_pool_2d(C2)
         # model.add(Flatten())
         F = flatten(P1)
         self.assertEqual(str(F.shape), 'tensor<1x12100x!eltwise.f32>')
-        K3 = Tensor(LogicalShape(plaidml.DType.FLOAT32, [12100, 128]))
-        B3 = Tensor(LogicalShape(plaidml.DType.FLOAT32, [128]))
+        K3 = Placeholder(plaidml.DType.FLOAT32, [12100, 128])
+        B3 = Placeholder(plaidml.DType.FLOAT32, [128])
         D1 = relu(dot(F, K3) + B3)
         # model.add(Dense(num_classes, activation='softmax'))
-        K4 = Tensor(LogicalShape(plaidml.DType.FLOAT32, [128, 100]))
-        B4 = Tensor(LogicalShape(plaidml.DType.FLOAT32, [100]))
+        K4 = Placeholder(plaidml.DType.FLOAT32, [128, 100])
+        B4 = Placeholder(plaidml.DType.FLOAT32, [100])
         D2 = softmax(dot(D1, K4) + B4)
         program = Program('mnist_cnn', [D2])
         expected = '''
@@ -659,7 +659,7 @@ module {
         self.compare_results(program, expected)
 
     def test_arg_max(self):
-        I = Tensor(LogicalShape(plaidml.DType.FLOAT32, [1, 10, 10]))
+        I = Placeholder(plaidml.DType.FLOAT32, [1, 10, 10])
         O = arg_max(I)
         program = Program('arg_max', [O])
         self.assertEqual(str(O.shape), 'tensor<1x10x!eltwise.u32>')
@@ -687,7 +687,7 @@ module {
         self.compare_results(program, expected)
 
     def test_global_min(self):
-        I = Tensor(LogicalShape(plaidml.DType.FLOAT32, [10, 10, 10]), name='I')
+        I = Placeholder(plaidml.DType.FLOAT32, [10, 10, 10], name='I')
         O = global_min(I)
         program = Program('global_min', [O])
         expected = '''
@@ -709,7 +709,7 @@ module {
         self.compare_results(program, expected)
 
     def test_cum_sum(self):
-        I = Tensor(LogicalShape(plaidml.DType.FLOAT32, [10]), name='I')
+        I = Placeholder(plaidml.DType.FLOAT32, [10], name='I')
         O = csum(I)
         program = Program('cum_sum', [O])
         expected = '''
@@ -736,10 +736,10 @@ module {
         self.assertTrue('Cannot compute shape' in str(err.exception))
 
     def test_unique_names(self):
-        A = Tensor(LogicalShape(plaidml.DType.FLOAT32), name='A')
-        B = Tensor(LogicalShape(plaidml.DType.FLOAT32), name='B')
-        C0 = Tensor(LogicalShape(plaidml.DType.FLOAT32), name='C')
-        C1 = Tensor(LogicalShape(plaidml.DType.FLOAT32), name='C')
+        A = Placeholder(plaidml.DType.FLOAT32, name='A')
+        B = Placeholder(plaidml.DType.FLOAT32, name='B')
+        C0 = Placeholder(plaidml.DType.FLOAT32, name='C')
+        C1 = Placeholder(plaidml.DType.FLOAT32, name='C')
         program = Program('unique_names', [A + B + C0 + C1])
         expected = '''
 
@@ -800,7 +800,7 @@ module {
         self.compare_results(program, expected)
 
     def test_repeat_elts(self):
-        I = Tensor(LogicalShape(plaidml.DType.FLOAT32, [10, 10, 10]))
+        I = Placeholder(plaidml.DType.FLOAT32, [10, 10, 10])
         N0, N1, N2 = TensorDims(3)
         n0, n1, n2, k = TensorIndexes(4)
         I.bind_dims(N0, N1, N2)
@@ -827,8 +827,8 @@ module {
         self.compare_results(program, expected)
 
     def test_use_default(self):
-        P = Tensor(LogicalShape(plaidml.DType.FLOAT32, [1, 7, 10, 10]))
-        I = Tensor(LogicalShape(plaidml.DType.FLOAT32, [1, 10, 10]))
+        P = Placeholder(plaidml.DType.FLOAT32, [1, 7, 10, 10])
+        I = Placeholder(plaidml.DType.FLOAT32, [1, 10, 10])
         B, N1, N2 = TensorDims(3)
         b, i1, i2 = TensorIndexes(3)
         I.bind_dims(B, N1, N2)
@@ -852,8 +852,8 @@ module {
 
     @unittest.skip('TODO: Finish bounds pass')
     def test_defract(self):
-        I = Tensor(LogicalShape(plaidml.DType.FLOAT32, [3]), name='I')
-        K = Tensor(LogicalShape(plaidml.DType.FLOAT32, [3]), name='K')
+        I = Placeholder(plaidml.DType.FLOAT32, [3], name='I')
+        K = Placeholder(plaidml.DType.FLOAT32, [3], name='K')
         i, j = TensorIndexes(2)
         O = TensorOutput(5)
         O[i] += (I[(i - j + 1) // 2] * K[j])
@@ -880,7 +880,7 @@ module {
 
     @unittest.skip('TODO: Finish bounds pass')
     def test_defract_short(self):
-        I = Tensor(LogicalShape(plaidml.DType.FLOAT32, [3]), name='I')
+        I = Placeholder(plaidml.DType.FLOAT32, [3], name='I')
         i, j = TensorIndexes(2)
         O = TensorOutput(6)
         O[i] += (I[(i - 1) // 2])
@@ -906,8 +906,8 @@ module {
     @unittest.skip('TODO: Finish bounds pass')
     def test_defract_long(self):
         shape = [1, 3, 3, 1]
-        I = Tensor(LogicalShape(plaidml.DType.FLOAT32, shape), name='I')
-        K = Tensor(LogicalShape(plaidml.DType.FLOAT32, shape), name='K')
+        I = Placeholder(plaidml.DType.FLOAT32, shape, name='I')
+        K = Placeholder(plaidml.DType.FLOAT32, shape, name='K')
         n, x0, x1, c0, c1, co, ci, k0, k1 = TensorIndexes(9)
         O = TensorOutput(1, 5, 5, 1)
         O[n, x0, x1, co] += (I[n, (x0 + k0 - 1) // 2,
@@ -960,8 +960,8 @@ module {
         reformat those names as valid tile identifiers. If we are doing that,
         this test will pass, otherwise we'll get a syntax error.'''
 
-        I = Tensor(LogicalShape(plaidml.DType.FLOAT32, [3]), name='I')
-        K = Tensor(LogicalShape(plaidml.DType.FLOAT32, [3]), name='K')
+        I = Placeholder(plaidml.DType.FLOAT32, [3], name='I')
+        K = Placeholder(plaidml.DType.FLOAT32, [3], name='K')
         i, j = TensorIndexes(2)
         O = TensorOutput(5)
         O[i] += (I[(i - j + 1) // 2] * K[j])
@@ -986,7 +986,7 @@ module {
         self.assertEqual(outputs[0].tolist(), [2, 5, 4, 9, 6])
 
     def test_identity(self):
-        I = Tensor(LogicalShape(plaidml.DType.FLOAT32, [3]), name='I')
+        I = Placeholder(plaidml.DType.FLOAT32, [3], name='I')
         program = Program('identity', [I])
         expected = '''
 module {
@@ -1002,8 +1002,8 @@ module {
 
     @unittest.skip('TODO: exception needs to be thrown')
     def test_assignment_exceptions(self):
-        A = Tensor(LogicalShape(plaidml.DType.FLOAT32, [5, 1]), name='A')
-        B = Tensor(LogicalShape(plaidml.DType.FLOAT32, [1, 5]), name='B')
+        A = Placeholder(plaidml.DType.FLOAT32, [5, 1], name='A')
+        B = Placeholder(plaidml.DType.FLOAT32, [1, 5], name='B')
         L, M, N = TensorDims(3)
         i, j, k = TensorIndexes(3)
         A.bind_dims(L, M)
@@ -1046,7 +1046,7 @@ module {
         self.assertTrue("illegal assignment aggregation" in str(cm.exception))
 
     def test_two_outputs(self):
-        I = Tensor(LogicalShape(plaidml.DType.FLOAT32, [3]), name='I')
+        I = Placeholder(plaidml.DType.FLOAT32, [3], name='I')
         program1 = Program('two_outputs', [I, I])
         expected = '''
 module {
@@ -1071,6 +1071,33 @@ module {
         outputs = plaidml.exec.run(program2, [(I, np.array([(1, 2, 3)]))])
         self.assertEqual(outputs[0].tolist(), [1, 2, 3])
         self.assertEqual(outputs[1].tolist(), [1, 2, 3])
+
+    def test_placeholder_noshape(self):
+        I1 = Placeholder(plaidml.DType.INT32)
+        program1 = Program('placeholder_noshape', [I1])
+        I2 = Placeholder(LogicalShape(plaidml.DType.INT32))
+        program2 = Program('placeholder_noshape', [I2])
+        self.assertEqual(str(I1.shape), "tensor<!eltwise.i32>")
+        self.assertEqual(str(I2.shape), "tensor<!eltwise.i32>")
+        self.compare_results(program1, str(program2))
+
+    def test_placeholder_noname(self):
+        I1 = Placeholder(plaidml.DType.INT32, [1, 1])
+        program1 = Program('placeholder_noname', [I1])
+        I2 = Placeholder(LogicalShape(plaidml.DType.INT32, [1, 1]))
+        program2 = Program('placeholder_noname', [I2])
+        self.assertEqual(str(I1.shape), "tensor<1x1x!eltwise.i32>")
+        self.assertEqual(str(I2.shape), "tensor<1x1x!eltwise.i32>")
+        self.compare_results(program1, str(program2))
+
+    def test_placeholder_with_name(self):
+        I1 = Placeholder(plaidml.DType.INT32, [1, 1], name='I')
+        program1 = Program('placeholder_with_name', [I1])
+        I2 = Placeholder(LogicalShape(plaidml.DType.INT32, [1, 1]), name='I')
+        program2 = Program('placeholder_with_name', [I2])
+        self.assertEqual(str(I1.shape), "tensor<1x1x!eltwise.i32>")
+        self.assertEqual(str(I2.shape), "tensor<1x1x!eltwise.i32>")
+        self.compare_results(program1, str(program2))
 
 
 if __name__ == '__main__':
