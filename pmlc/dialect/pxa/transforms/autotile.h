644384764eabb7a46706b1605a3ba49955d6b668
Jeremy Bruestle <jeremy.bruestle@intel.com>
2020-02-03 15:46:51 -0800

    Fixed linux compiler warnings (one of which was actually a bug, thanks Linux), also apparently triggered reformating

--- pmlc/dialect/pxa/transforms/autotile.h
+++ pmlc/dialect/pxa/transforms/autotile.h
@@ -20,43 +20,48 @@ std::vector<int64_t> PowerOfTwoGenerator(int64_t range);
 std::vector<int64_t> EvenTilingGenerator(int64_t range);
 
 // Produces only 'range' itself
-inline std::vector<int64_t> ExactRangeGenerator(int64_t range) { return {range}; }
+inline std::vector<int64_t> ExactRangeGenerator(int64_t range) {
+  return {range};
+}
 
 // Produces only a specific value
 class FixedTileSizeGenerator {
- public:
+public:
   explicit FixedTileSizeGenerator(int64_t val) : val(val) {}
   std::vector<int64_t> operator()(int64_t range) const { return {val}; }
 
- private:
+private:
   int64_t val;
 };
 
 // Producs the union of multiple generators
 template <typename Head, typename... Rest>
 class UnionGenerator {
- public:
-  UnionGenerator(const Head& head, const Rest&... rest) : head(head), rest(rest...) {}
+public:
+  UnionGenerator(const Head &head, const Rest &... rest)
+      : head(head), rest(rest...) {}
   std::vector<int64_t> operator()(int64_t range) const {
     auto v1 = head(range);
     auto v2 = rest(range);
     std::vector<int64_t> vout;
-    std::set_union(v1.begin(), v1.end(), v2.begin(), v2.end(), std::back_inserter(vout));
+    std::set_union(v1.begin(), v1.end(), v2.begin(), v2.end(),
+                   std::back_inserter(vout));
+    return vout;
   }
 
- private:
-  const Head& head;
+private:
+  const Head &head;
   UnionGenerator<Rest...> rest;
 };
 
 template <typename Single>
 class UnionGenerator<Single> {
- public:
-  explicit UnionGenerator(const Single& single) : single(single) {}
+public:
+  explicit UnionGenerator(const Single &single) : single(single) {}
   std::vector<int64_t> operator()(int64_t range) const { return single(range); }
 
- private:
-  const Single& single;
+private:
+  const Single &single;
 };
 
 // A tile cost model is a functor from an array of tile sizes (i.e.
@@ -68,7 +73,8 @@ inline double DummyCostModel(ArrayRef<int64_t> tile) { return 1.0; }
 // Given a generator and cost model, find the best tile size, return empty
 // tiling when all tiles are infeasible
 template <typename Generator, typename CostModel>
-llvm::SmallVector<int64_t, 8> FindBestTileSize(const Generator& generator, const CostModel& costModel,
+llvm::SmallVector<int64_t, 8> FindBestTileSize(const Generator &generator,
+                                               const CostModel &costModel,
                                                ArrayRef<int64_t> ranges) {
   // Build a list of potential tile sizes for each dimension.
   // Basically, we are caching the output of the generator in case it is
@@ -82,7 +88,7 @@ llvm::SmallVector<int64_t, 8> FindBestTileSize(const Generator& generator, const
   llvm::SmallVector<int64_t, 8> bestTileSize;
   llvm::SmallVector<int64_t, 8> curTileSize(ranges.size());
   // Build a recursive lambda to walk over the options (thanks c++14!)
-  auto recurse = [&](auto& self, int idx) -> void {
+  auto recurse = [&](auto &self, size_t idx) -> void {
     if (idx == allowedTileSizes.size()) {
       double newCost = costModel(curTileSize);
       if (newCost < bestCost) {
@@ -102,4 +108,4 @@ llvm::SmallVector<int64_t, 8> FindBestTileSize(const Generator& generator, const
   return bestTileSize;
 }
 
-}  // namespace pmlc::dialect::pxa
+} // namespace pmlc::dialect::pxa
