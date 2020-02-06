0e65f12f2902d42051ea2fa37679d737d4f93dc5
Denise Kutnick <denise.g.kutnick@intel.com>
2020-02-06 13:01:52 -0800

    MLSys Demo: Interactive Logic (2/2) (#906)
    * interactive demo 2/2
    
    * cleanup
    
    * fix visuals
    
    * fix display
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- demos/mlsys_2020/demo_source.py
+++ demos/mlsys_2020/demo_source.py
@@ -1,7 +1,7 @@
 # Begin with some imports
 import numpy as np
 import drawSvg as draw
-from drawSvg.widgets import DrawingWidget
+from drawSvg.widgets import DrawingWidget, AsyncAnimation
 import ipywidgets as widgets
 
 import plaidml
@@ -11,17 +11,35 @@ from plaidml.edsl import *
 
 class Demo:
 
-    edsl_snippets = {
-        "Matrix Multiplication": "R[i, j] += X[i, k] * Y[k, j]",
-        "Sum Over Axis": "R[i] += X[i, j]",
-        "Elementwise Sum": "R = X + Y",
-        "Elementwise Multiply": "R = X * Y",
-        "Elementwise Square Root": "R = sqrt(X)",
-    }
+    def __init__(self):
+        self.edsl_snippets = {
+            "Contraction": {
+                "Matrix Multiplication": "R[i, j] += X[i, k] * Y[k, j]",
+                "Sum Over Axis": "R[i] += X[i, j]"
+            },
+            "Elementwise": {
+                "Sum": "R = X + Y",
+                "Multiply": "R = X * Y",
+                "Square Root": "R = sqrt(X)"
+            }
+        }
+        # Placeholders
+        self.input_x_vals = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+        self.input_y_vals = np.array([[9, 8, 7], [6, 5, 4], [3, 2, 1]])
+        self.output_vals = np.full([3, 3], np.nan)
+        self.input_x_anim = self.draw_grid(self.input_x_vals.shape,
+                                           self.input_x_vals,
+                                           color='#ffddaa')
+        self.input_y_anim = self.draw_grid(self.input_y_vals.shape,
+                                           self.input_y_vals,
+                                           color='#ffddff')
+        self.output_anim = self.draw_grid(self.output_vals.shape,
+                                          self.output_vals,
+                                          color='#ffffff')
 
     # Drawing helper: this can be used for inputs, outputs, etc.
     # Eventually, this will include animation logic too.
-    def draw_grid(dims, vals, grid_size=10, color='#ffffff'):
+    def draw_grid(self, dims, vals, grid_size=10, color='#ffffff'):
         anim = draw.Drawing(330, 330, origin=(0, 0))
         for x in range(dims[0]):
             for y in range(dims[1]):
@@ -35,7 +53,7 @@ class Demo:
                         stroke_width=grid_size,  # outline size
                         stroke='black',  # outline color
                         fill=color))  # fill color
-                string_output = str(vals[x][y])
+                string_output = str(vals[dims[1] - 1 - y][x])
                 font_size = 50 / len(string_output) + 25
                 group.draw(
                     draw.Text(
@@ -48,43 +66,35 @@ class Demo:
                 anim.append(group)
         return anim
 
-    # The program that will be executed during the demo.
-    # Eventually, this will have callbacks enabled for custom operations.
-    def edsl_program(X, Y):
+    def runtime_handler(self, op_type, textbox_value):
+        X = Placeholder(plaidml.DType.INT32, [3, 3])
+        Y = Placeholder(plaidml.DType.INT32, [3, 3])
+        edsl_context = globals()
+        exec(
+            """
+def edsl_program(X, Y):
         I, J, K = TensorDims(3)
         i, j, k = TensorIndexes(3)
         X.bind_dims(I, K)
         Y.bind_dims(K, J)
         R = TensorOutput(I, J)
-        R[i, j] += X[i, k] * Y[k, j]
+        {}
         return R
+""".format(textbox_value), edsl_context)
+        edsl_program = edsl_context['edsl_program']
+        R = edsl_program(X, Y)
+        program = Program('edsl_program', [R])
 
-    # Hardcoded placeholders for now
-    # Eventually, this will be a callback to edsl_program that is triggered by the Run button.
-    A = Placeholder(plaidml.DType.INT32, [3, 3])
-    B = Placeholder(plaidml.DType.INT32, [3, 3])
-    O = edsl_program(A, B)
-    program = Program('edsl_program', [O])
+        # Create the binder and the executable so that the program can run.
+        binder = plaidml.exec.Binder(program)
+        executable = binder.compile()
+        binder.input(X).copy_from_ndarray(self.input_x_vals)
+        binder.input(Y).copy_from_ndarray(self.input_y_vals)
+        executable.run()
 
-    # The dimensions of each tensor are bound at this point now that the Program is created.
-    # The values of each tensor are still placeholders at this point.
-    input_a_dims = program.inputs[0].shape.int_dims
-    input_b_dims = program.inputs[1].shape.int_dims
-    output_dims = program.outputs[0].shape.int_dims
+        # The code has been run, so the values of each tensor have been bound at this point.
+        self.output_vals = binder.output(R).as_ndarray()
 
-    # Create the binder and the executable so that the program can run.
-    binder = plaidml.exec.Binder(program)
-    executable = binder.compile()
-    binder.input(A).copy_from_ndarray(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
-    binder.input(B).copy_from_ndarray(np.array([[9, 8, 7], [6, 5, 4], [3, 2, 1]]))
-    executable.run()
-    # The code has been run, so the values of each tensor have been bound at this point.
-    input_a_vals = binder.input(A).as_ndarray()
-    input_b_vals = binder.input(B).as_ndarray()
-    output_vals = binder.output(O).as_ndarray()
-
-    # Animations: create them in one place for now.
-    # Output animations are not available until the executable is complete.
-    input_a_anim = draw_grid(input_a_dims, input_a_vals, color='#ffddaa')
-    input_b_anim = draw_grid(input_b_dims, input_b_vals, color='#ffddff')
-    output_anim = draw_grid(output_dims, output_vals, color='#aaddff')
+        self.output_anim = self.draw_grid(self.output_vals.shape,
+                                          self.output_vals,
+                                          color='#aaddff')
