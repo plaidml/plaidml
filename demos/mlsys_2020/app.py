6d966df83d73721ea346c5fed8beaadff256f0e5
Denise Kutnick <denise.g.kutnick@intel.com>
2020-02-28 11:26:40 -0800

    EDSL Demo Improvements (#944)
    * WIP
    
    * styling
    
    * output widget
    
    * boilerplate for contractions
    
    * change square root to identity
    
    * add legend
    
    * delete unnecessary ipynb cells
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ demos/mlsys_2020/app.py
@@ -0,0 +1,152 @@
+import drawSvg.widgets as draw_widgets
+import ipywidgets as widgets
+from IPython.display import display
+
+import demos.mlsys_2020.core as demo
+import plaidml
+import plaidml.exec
+
+
+def run():
+
+    my_demo = demo.Demo()
+
+    # Functional widget: Text Box
+    op_textbox = lambda x: widgets.Text(value=list(my_demo.edsl_snippets[x].values())[0],
+                                        placeholder=list(my_demo.edsl_snippets[x].values())[0],
+                                        disabled=False)
+
+    # Functional widget: Dropdown
+    op_dropdown = lambda x: widgets.Dropdown(options=list(my_demo.edsl_snippets[x].keys()),
+                                             value=list(my_demo.edsl_snippets[x].keys())[0],
+                                             disabled=False)
+
+    # Functional widget: Contraction op
+
+    contraction_textbox = op_textbox("Contraction")
+
+    contraction_dropdown = op_dropdown("Contraction")
+
+    contraction_boilerplate = widgets.HTML(
+        value=my_demo.boilerplate_html[contraction_dropdown.value])
+
+    contraction_out = widgets.Output()
+    with contraction_out:
+        display(contraction_boilerplate)
+
+    def contraction_dropdown_handler(change):
+        contraction_textbox.value = my_demo.edsl_snippets["Contraction"][change["new"]]
+        contraction_textbox.placeholder = contraction_textbox.value
+        contraction_boilerplate = widgets.HTML(value=my_demo.boilerplate_html[change["new"]])
+        contraction_out.clear_output()
+        with contraction_out:
+            display(contraction_boilerplate)
+
+    contraction_dropdown.observe(contraction_dropdown_handler, names='value')
+
+    contraction_op_widget = widgets.VBox([
+        widgets.HBox([widgets.Label(value="Pick an operation:"), contraction_dropdown]),
+        contraction_out,
+        widgets.VBox([contraction_textbox,
+                      widgets.HTML(value=my_demo.boilerplate_html_footer)],
+                     layout=widgets.Layout(margin='0 0 0 50px'))
+    ])
+    # Functional widget: Elementwise op
+
+    elementwise_textbox = op_textbox("Elementwise")
+
+    elementwise_dropdown = op_dropdown("Elementwise")
+
+    elementwise_boilerplate = widgets.HTML(
+        value=my_demo.boilerplate_html[elementwise_dropdown.value])
+
+    elementwise_out = widgets.Output()
+    with elementwise_out:
+        display(elementwise_boilerplate)
+
+    def elementwise_dropdown_handler(change):
+        elementwise_textbox.value = my_demo.edsl_snippets["Elementwise"][change["new"]]
+        elementwise_textbox.placeholder = elementwise_textbox.value
+        elementwise_boilerplate = widgets.HTML(value=my_demo.boilerplate_html[change["new"]])
+        elementwise_out.clear_output()
+        with elementwise_out:
+            display(elementwise_boilerplate)
+
+    elementwise_dropdown.observe(elementwise_dropdown_handler, names='value')
+
+    elementwise_op_widget = widgets.VBox([
+        widgets.HBox([widgets.Label(value="Pick an operation:"), elementwise_dropdown]),
+        elementwise_out,
+        widgets.VBox([elementwise_textbox,
+                      widgets.HTML(value=my_demo.boilerplate_html_footer)],
+                     layout=widgets.Layout(margin='0 0 0 50px'))
+    ])
+
+    textboxes = [contraction_textbox, elementwise_textbox]
+    dropdowns = {'Contraction': contraction_dropdown, 'Elementwise': elementwise_dropdown}
+
+    op_tab_titles = list(my_demo.edsl_snippets.keys())
+    op_widgets = [contraction_op_widget, elementwise_op_widget]
+    op_tabs = widgets.Tab()
+    op_tabs.children = op_widgets
+    for i in range(len(op_tabs.children)):
+        op_tabs.set_title(i, op_tab_titles[i])
+
+    op_run = widgets.Button(description='Run',
+                            disabled=False,
+                            button_style='success',
+                            tooltip='Compiles and executes your EDSL program',
+                            icon='check')
+
+    def on_run_click(cb):
+        op_type = op_tab_titles[op_tabs.selected_index]
+        textbox_value = textboxes[op_tabs.selected_index].value
+        dropdown_value = dropdowns[op_type].value
+        my_demo.runtime_handler(op_type, dropdown_value, textbox_value)
+
+    op_run.on_click(on_run_click)
+
+    # Interactive user interface (visible on the left side of the display)
+    left = widgets.VBox([
+        widgets.HTML(value="<h2>Code</h2>"),
+        widgets.HTML(
+            value=
+            "First, select a pre-written EDSL snippet from the list below, or write your own custom operation."
+        ),
+        widgets.HTML(value="Then, press Run to see the EDSL code in action!"), op_tabs, op_run
+    ])
+
+    # Live action animation (visible on the right side of the display)
+    def output_anim(arg):
+        return my_demo.output_anim
+
+    right = widgets.VBox([
+        widgets.HBox([
+            widgets.VBox([
+                widgets.HTML(value="<h2>Input Array: X</h2>"),
+                draw_widgets.DrawingWidget(my_demo.input_x_anim)
+            ]),
+            widgets.VBox([
+                widgets.HTML(value="<h2>Input Array: Y</h2>"),
+                draw_widgets.DrawingWidget(my_demo.input_y_anim)
+            ])
+        ]),
+        widgets.HTML(value="<h2>Output Array: R</h2>"),
+        draw_widgets.AsyncAnimation(1, output_anim, click_pause=False),
+        widgets.Label(value="Note: all matrices are shown in row-major order")
+    ])
+
+    # Full-screen layout
+
+    hbox_layout = widgets.Layout()
+    hbox_layout.width = '100%'
+    hbox_layout.justify_content = 'space-around'
+
+    # do this in a flexbox-friendlier way
+    edsl_title = widgets.HTML(value='<div style="text-align:center"><h1>EDSL Demo</h1></div>')
+
+    edsl_subdemo = widgets.HBox([left, right])
+    edsl_subdemo.layout = hbox_layout
+
+    edsl_demo = widgets.VBox([edsl_title, edsl_subdemo])
+    display(edsl_demo)
---
+++ demos/mlsys_2020/core.py
@@ -0,0 +1,245 @@
+import re
+
+import drawSvg as draw
+import ipywidgets as widgets
+import numpy as np
+import plaidml
+import plaidml.exec
+
+from IPython.display import display
+from plaidml.edsl import *
+import demos.mlsys_2020.style.custom_html as html
+
+
+class Demo:
+
+    def __init__(self):
+        self.edsl_snippets = {
+            "Contraction": {
+                "Matrix Multiplication": "R[i, j] += X[i, k] * Y[k, j]",
+                "Sum Over Axis": "R[i] += X[i, j]"
+            },
+            "Elementwise": {
+                "Sum": "R = X + Y",
+                "Multiply": "R = X * Y",
+                "Identity": "R = ident(X)"
+            }
+        }
+        self.boilerplate_inputs = {
+            "Matrix Multiplication": ['X', 'Y'],
+            "Sum Over Axis": ['X'],
+            "Sum": ['X', 'Y'],
+            "Multiply": ['X', 'Y'],
+            "Identity": ['X']
+        }
+        self.boilerplate_edsl = {
+            "Matrix Multiplication":
+                """
+def edsl_program(X, Y):
+        I, J, K = TensorDims(3)
+        i, j, k = TensorIndexes(3)
+        X.bind_dims(I, K)
+        Y.bind_dims(K, J)
+        R = TensorOutput(I, J)
+        {}
+        return R
+""",
+            "Sum Over Axis":
+                """
+def edsl_program(X):
+        I, J = TensorDims(2)
+        i, j = TensorIndexes(2)
+        X.bind_dims(I, J)
+        R = TensorOutput(I)
+        {}
+        return R
+""",
+            "Generic_Two_Input_Eltwise":
+                """
+def edsl_program(X, Y):
+        {}
+        return R
+""",
+            "Generic_One_Input_Eltwise":
+                """
+def edsl_program(X):
+        {}
+        return R
+"""
+        }
+        self.boilerplate_edsl["Identity"] = self.boilerplate_edsl["Generic_One_Input_Eltwise"]
+        self.boilerplate_edsl['Sum'] = self.boilerplate_edsl["Generic_Two_Input_Eltwise"]
+        self.boilerplate_edsl['Multiply'] = self.boilerplate_edsl["Generic_Two_Input_Eltwise"]
+        self.boilerplate_html = {
+            "Generic_One_Input_Eltwise":
+                f"""
+{html.code_open}
+{html.code_directive}def{html.font_close} {html.code_function_name}edsl_program{html.font_close}
+({html.code_input_param}X{html.font_close}):{html.br}
+{html.code_close}
+""",
+            "Generic_Two_Input_Eltwise":
+                f"""
+{html.code_open}
+{html.code_directive}def{html.font_close} {html.code_function_name}edsl_program{html.font_close}
+({html.code_input_param}X{html.font_close}, {html.code_input_param}Y{html.font_close}):{html.br}
+{html.code_close}
+""",
+            "Matrix Multiplication":
+                f"""
+{html.code_open}
+{html.code_directive}def{html.font_close} {html.code_function_name}edsl_program{html.font_close}
+({html.code_input_param}X{html.font_close}, {html.code_input_param}Y{html.font_close}):{html.br}
+{html.sp}{html.code_tensor_dim}I{html.font_close}, {html.code_tensor_dim}J{html.font_close}, {html.code_tensor_dim}K{html.font_close}
+ = {html.code_directive}TensorDims{html.font_close}({html.code_numeric}3{html.font_close}){html.br}
+{html.sp}{html.code_tensor_index}i{html.font_close}, {html.code_tensor_index}j{html.font_close}, {html.code_tensor_index}k{html.font_close}
+ = {html.code_directive}TensorIndexes{html.font_close}({html.code_numeric}3{html.font_close}){html.br}
+{html.sp}{html.code_input_param}X{html.font_close}.{html.code_directive}bind_dims{html.font_close}
+({html.code_tensor_dim}I{html.font_close},{html.code_tensor_dim}K{html.font_close}){html.br}
+{html.sp}{html.code_input_param}Y{html.font_close}.{html.code_directive}bind_dims{html.font_close}
+({html.code_tensor_dim}K{html.font_close},{html.code_tensor_dim}J{html.font_close}){html.br}
+{html.sp}{html.code_output_param}R{html.font_close} = {html.code_directive}TensorOutput{html.font_close}
+({html.code_tensor_dim}I{html.font_close},{html.code_tensor_dim}J{html.font_close}){html.br}
+{html.code_close}
+""",
+            "Sum Over Axis":
+                f"""
+{html.code_open}
+{html.code_directive}def{html.font_close} {html.code_function_name}edsl_program{html.font_close}
+({html.code_input_param}X{html.font_close}):{html.br}
+{html.sp}{html.code_tensor_dim}I{html.font_close}, {html.code_tensor_dim}J{html.font_close}
+ = {html.code_directive}TensorDims{html.font_close}({html.code_numeric}2{html.font_close}){html.br}
+{html.sp}{html.code_tensor_index}i{html.font_close}, {html.code_tensor_index}j{html.font_close}
+ = {html.code_directive}TensorIndexes{html.font_close}({html.code_numeric}2{html.font_close}){html.br}
+{html.sp}{html.code_input_param}X{html.font_close}.{html.code_directive}bind_dims{html.font_close}
+({html.code_tensor_dim}I{html.font_close},{html.code_tensor_dim}J{html.font_close}){html.br}
+{html.sp}{html.code_output_param}R{html.font_close} = {html.code_directive}TensorOutput{html.font_close}
+({html.code_tensor_dim}I{html.font_close}){html.br}
+{html.code_close}
+""",
+        }
+        self.boilerplate_html_footer = f"""
+{html.code_open}
+{html.code_directive}return{html.font_close} {html.code_output_param}R{html.font_close}
+{html.code_close}
+"""
+        self.boilerplate_html["Identity"] = self.boilerplate_html["Generic_One_Input_Eltwise"]
+        self.boilerplate_html['Sum'] = self.boilerplate_html["Generic_Two_Input_Eltwise"]
+        self.boilerplate_html['Multiply'] = self.boilerplate_html["Generic_Two_Input_Eltwise"]
+        # Placeholders
+        self.input_x_vals = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+        self.input_y_vals = np.array([[9, 8, 7], [6, 5, 4], [3, 2, 1]])
+        self.output_vals = np.full([3, 3], np.nan)
+        self.input_x_anim = self.draw_grid(self.input_x_vals.shape,
+                                           self.input_x_vals,
+                                           color='#ffddaa')
+        self.input_y_anim = self.draw_grid(self.input_y_vals.shape,
+                                           self.input_y_vals,
+                                           color='#ffddff')
+        self.output_anim = self.draw_grid(self.output_vals.shape,
+                                          self.output_vals,
+                                          color='#ffffff')
+
+    # Drawing helper: this can be used for inputs, outputs, etc.
+    # Eventually, this will include animation logic too.
+    def draw_grid(self, dims, vals, grid_size=10, color='#ffffff'):
+        anim = draw.Drawing(330, 330, origin=(0, 0))
+        for x in range(dims[0]):
+            for y in range(dims[1]):
+                group = draw.Group()
+                group.draw(
+                    draw.Rectangle(
+                        100 * x + grid_size,  # origin x coords
+                        100 * y + grid_size,  # origin y coords
+                        100,  # grid width
+                        100,  # grid height
+                        stroke_width=grid_size,  # outline size
+                        stroke='black',  # outline color
+                        fill=color))  # fill color
+                string_output = str(vals[dims[1] - 1 - y][x])
+                font_size = 50 / len(string_output) + 25
+                group.draw(
+                    draw.Text(
+                        string_output,
+                        font_size,
+                        100 * x + grid_size + font_size / 3 +
+                        2 * len(string_output),  # origin x coords
+                        100 * y + grid_size + 2250 / (font_size + 20),  # origin y coords
+                        center=0))
+                anim.append(group)
+        return anim
+
+    def draw_line(self, dims, vals, grid_size=10, color='#ffffff'):
+        anim = draw.Drawing(330, 120, origin=(0, 0))
+        for x in range(dims[0]):
+            group = draw.Group()
+            group.draw(
+                draw.Rectangle(
+                    100 * x + grid_size,  # origin x coords
+                    grid_size,  # origin y coords
+                    100,  # grid width
+                    100,  # grid height
+                    stroke_width=grid_size,  # outline size
+                    stroke='black',  # outline color
+                    fill=color))  # fill color
+            string_output = str(vals[x])
+            font_size = 50 / len(string_output) + 25
+            group.draw(
+                draw.Text(
+                    string_output,
+                    font_size,
+                    100 * x + grid_size + font_size / 3 +
+                    2 * len(string_output),  # origin x coords
+                    grid_size + 2250 / (font_size + 20),  # origin y coords
+                    center=0))
+            anim.append(group)
+        return anim
+
+    def runtime_handler(self, op_type, op_name, textbox_value):
+        X = Placeholder(plaidml.DType.INT32, [3, 3])
+        Y = Placeholder(plaidml.DType.INT32, [3, 3])
+        edsl_context = globals()
+        exec(self.boilerplate_edsl[op_name].format(textbox_value), edsl_context)
+        edsl_program = edsl_context['edsl_program']
+        if len(self.boilerplate_inputs[op_name]) == 1:
+            if 'X' in self.boilerplate_inputs[op_name]:
+                R = edsl_program(X)
+            if 'Y' in self.boilerplate_inputs[op_name]:
+                R = edsl_program(Y)
+        else:
+            R = edsl_program(X, Y)
+        program = Program('edsl_program', [R], debug=True)
+        for p in program.passes:
+            name = p[0]
+            print('\n')
+            print(name)
+            text = widgets.Textarea(value=''.join(p),
+                                    placeholder='Passes',
+                                    disabled=False,
+                                    layout=widgets.Layout(
+                                        height='100%',
+                                        width='auto',
+                                    ))
+            box = widgets.VBox([text], layout={'height': '350px'})
+            #display(box)
+
+        # Create the binder and the executable so that the program can run.
+        binder = plaidml.exec.Binder(program)
+        executable = binder.compile()
+        if 'X' in self.boilerplate_inputs[op_name]:
+            binder.input(X).copy_from_ndarray(self.input_x_vals)
+        if 'Y' in self.boilerplate_inputs[op_name]:
+            binder.input(Y).copy_from_ndarray(self.input_y_vals)
+        executable.run()
+
+        # The code has been run, so the values of each tensor have been bound at this point.
+        self.output_vals = binder.output(R).as_ndarray()
+
+        if self.output_vals.ndim == 1:
+            self.output_anim = self.draw_line(self.output_vals.shape,
+                                              self.output_vals,
+                                              color='#aaddff')
+        else:
+            self.output_anim = self.draw_grid(self.output_vals.shape,
+                                              self.output_vals,
+                                              color='#aaddff')
