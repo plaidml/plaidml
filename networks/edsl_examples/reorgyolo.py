f054c0c455e0639176e5ee9f75b793535f0bcaeb
Sana Nazir <snazir@jsboswel-mobl.amr.corp.intel.com>
2020-04-20 20:32:40 -0700

    notes added reversible test added

--- networks/edsl_examples/reorgyolo.py
+++ networks/edsl_examples/reorgyolo.py
@@ -1,3 +1,4 @@
+#---------------------------------------------------------------------------------
 #   This example illustrates how to use eDSL to write the reorg layer employed in YOLO-v2
 #   --------------------------Op description-----------------------------------
 #   The reorg layer is employed in YOLO-v2 or Darknet to combine midlevel and
@@ -21,12 +22,38 @@
 #   https://gist.github.com/leimao/ece7217b5d07fe4e685c47af5e76744a
 #   is currently being used for local testing
 # TODO: handle cases where the spatial dims aren't evenly divided by stride
-# TODO: neaten up the reorgyolo c_o>c_in issue in tester function
-# TODO: either figure out how to reference leimao blog code or get rid of it
-# TODO: write backend_test style test against pytorch implementation
-# TODO: remove test functions and python test code
-# TODO: write a note on handling the modulus operator in eDSL
 # TODO: write a note on how floor division works in eDSL
+
+# reorgyolo_comparison demonstrates the reorg layer in python. In order to convert
+# this into eDSL we will first need to get rid of the modulus operators inside the
+# loops.
+# reorgyolo_comparison_nodivmod demonstrates reorgyolo implemented without employing
+# modulus operations. For clarity we have implemented reorgyolo without linearized
+# arrays. It might be easier to take a look at reorgyolo_comparison_non_linear and
+# reorgyolo_comparison_nodivmod to understand the required changes.
+# In general to replace a modulus operation with a multiplicatio operation, if the
+# modulus operation is applied to an index, the following generic technique can be
+# employed.
+# The simple for loop shown below can be replaced by two for loops to achieve exactly
+# the same index traversal. To convince yourself that this is true try out both the
+# loops in python shell.
+
+# R = 10 # some constant
+# n = 5 #  some constant
+# for i in range(R):
+#   j = i % n
+#   print("index i : " + str(i) + "  index j:" + str(j))
+
+# for q in range (R//n):
+#     for j in range(n):
+#         i = n*q + j
+#         print("index i : " + str(i) + "  index j:" + str(j))
+
+# Once we have gotten rid of the modulus operations we can convert the loops to eDSL
+# The eDSL implementation comforms more closely to the math involved in reorgyolo
+# and helps demystify this convoluted operation.
+# For more eDSL magic see: https://plaidml.readthedocs.io/en/latest/usage/edsl.html
+#---------------------------------------------------------------------------------
 
 import numpy as np
 
@@ -35,10 +62,23 @@ import plaidml2.edsl as edsl
 import plaidml2.exec as plaidml_exec
 import plaidml2.op as plaidml_op
 
+#---------------------------------------------------------------------------------
+#    Python Implementation of reorgyolo: Our starting point.
 
+
+#    Title: reorg_simulation
+#    Author: Mao, Lei
+#    Date: 2020
+#    Code version: sample
+#    Availability: https://gist.github.com/leimao/ece7217b5d07fe4e685c47af5e76744a
+#---------------------------------------------------------------------------------
 def reorgyolo_comparison(arrayIn, batch, C, H, W, stride, forward=False):
-    arrayLen = len(arrayIn)
+    arrayLen = np.prod(arrayIn.shape)
     arrayOut = np.zeros(arrayLen)
+    if forward == False:
+        C = C * (stride * stride)
+        H = H // stride
+        W = W // stride
     out_c = C // (stride * stride)
     for b in range(batch):
         for k in range(C):
@@ -57,9 +97,44 @@ def reorgyolo_comparison(arrayIn, batch, C, H, W, stride, forward=False):
     return arrayOut
 
 
+#---------------------------------------------------------------------------------
+#       Python implementation of reorgyolo without using linearized arrays
+#---------------------------------------------------------------------------------
+def reorgyolo_comparison_non_linear(arrayIn, batch, C, H, W, stride, forward=False):
+    if forward == False:
+        C = C * (stride * stride)
+        H = H // stride
+        W = W // stride
+        arrayOut = np.zeros((batch, C, H, W))
+    else:
+        arrayOut = np.zeros((batch, int(C // (stride * stride)), H * stride, W * stride))
+    out_c = C // (stride * stride)
+    for b in range(batch):
+        for k in range(C):
+            for j in range(H):
+                for i in range(W):
+                    c2 = k % out_c
+                    offset = k // out_c
+                    w2 = i * stride + offset % stride
+                    h2 = j * stride + offset // stride
+                    if forward:
+                        arrayOut[b, c2, h2, w2] = arrayIn[b, k, j, i]
+                    else:
+                        arrayOut[b, k, j, i] = arrayIn[b, c2, h2, w2]
+    return arrayOut
+
+
+#---------------------------------------------------------------------------------
+#       Python implementation of reorgyolo without employing modulus operations
+#---------------------------------------------------------------------------------
 def reorgyolo_comparison_nodivmod(arrayIn, batch, C, H, W, stride, forward=False):
-    arrayLen = len(arrayIn)
-    arrayOut = np.zeros(arrayLen)
+    if forward == False:
+        C = C * (stride * stride)
+        H = H // stride
+        W = W // stride
+        arrayOut = np.zeros((batch, C, H, W))
+    else:
+        arrayOut = np.zeros((batch, int(C // (stride * stride)), H * stride, W * stride))
     out_c = C // (stride * stride)
     for n1 in range(batch):
         for w1 in range(W):
@@ -69,18 +144,18 @@ def reorgyolo_comparison_nodivmod(arrayIn, batch, C, H, W, stride, forward=False
                         for w_jump in range(stride):
                             _c1 = w_jump + h_jump * stride
                             c1 = c2 + (_c1 * out_c)
-                            in_index = w1 + W * (h1 + H * (c1 + C * n1))
                             w2 = w1 * stride + w_jump
                             h2 = h1 * stride + h_jump
-                            out_index = int(w2 + W * stride * (h2 + H * stride *
-                                                               (c2 + out_c * n1)))
                             if forward:
-                                arrayOut[out_index] = arrayIn[in_index]
+                                arrayOut[n1, c2, h2, w2] = arrayIn[n1, c1, h1, w1]
                             else:
-                                arrayOut[in_index] = arrayIn[out_index]
+                                arrayOut[n1, c1, h1, w1] = arrayIn[n1, c2, h2, w2]
     return arrayOut
 
 
+#---------------------------------------------------------------------------------
+#       eDSL implementation of reorgyolo
+#---------------------------------------------------------------------------------
 def reorgyolo(I, stride, decrease_C):
     N, C, H, W = edsl.TensorDims(4)
     n, w1, h1, c2, h_jump, w_jump = edsl.TensorIndexes(6)
@@ -101,12 +176,12 @@ def reorgyolo(I, stride, decrease_C):
 
 
 def main():
-    n_i = 1
+    n_i = 2
     c_i = 4
     h_i = 6
     w_i = 6
     stride = 2
-    decrease = False
+    decrease = True
 
     I_data_linear = np.array(list(range(n_i * c_i * h_i * w_i))).astype(np.int)
     I_data = np.reshape(I_data_linear, (n_i, c_i, h_i, w_i))
@@ -117,8 +192,6 @@ def main():
     print(I_data)
     print("_______________________________________________")
 
-    I = edsl.Tensor(edsl.LogicalShape(plaidml.DType.FLOAT32, I_data.shape))
-    O = reorgyolo(I, stride, decrease)
     if decrease:
         c_o = c_i // (stride * stride)
         h_o = h_i * stride
@@ -127,6 +200,45 @@ def main():
         c_o = c_i * (stride * stride)
         h_o = h_i // stride
         w_o = w_i // stride
+
+    expected_result_l = reorgyolo_comparison(I_data_linear,
+                                             batch=n_i,
+                                             C=c_i,
+                                             H=h_i,
+                                             W=w_i,
+                                             stride=stride,
+                                             forward=decrease)
+    O_exp = np.reshape(expected_result_l, (n_i, c_o, h_o, w_o))
+    print("_______________________________________________")
+    print("expected result: \n{}".format(O_exp))
+    print("_______________________________________________")
+
+    expected_result = reorgyolo_comparison_non_linear(I_data,
+                                                      batch=n_i,
+                                                      C=c_i,
+                                                      H=h_i,
+                                                      W=w_i,
+                                                      stride=stride,
+                                                      forward=decrease)
+
+    print("_______________________________________________")
+    print("reorgyolo_comparison_non_linear result new: \n{}".format(expected_result))
+
+    print("_______________________________________________")
+    expected_result = reorgyolo_comparison_nodivmod(I_data,
+                                                    batch=n_i,
+                                                    C=c_i,
+                                                    H=h_i,
+                                                    W=w_i,
+                                                    stride=stride,
+                                                    forward=decrease)
+
+    print("_______________________________________________")
+    print("reorgyolo_comparison_nodivmod result: \n{}".format(expected_result))
+    print("_______________________________________________")
+
+    I = edsl.Tensor(edsl.LogicalShape(plaidml.DType.FLOAT32, I_data.shape))
+    O = reorgyolo(I, stride, decrease)
 
     program = edsl.Program('reorgyolo', [O])
     binder = plaidml_exec.Binder(program)
@@ -137,49 +249,6 @@ def main():
 
     print("_______________________________________________")
     print("eDSL computed result: \n{}".format(result))
-    print("_______________________________________________")
-
-    if (c_o > c_i):  # leimao edit
-        expected_result_l = reorgyolo_comparison_nodivmod(I_data_linear,
-                                                          batch=n_i,
-                                                          C=c_o,
-                                                          H=h_o,
-                                                          W=w_o,
-                                                          stride=stride,
-                                                          forward=decrease)
-    else:
-        expected_result_l = reorgyolo_comparison_nodivmod(I_data_linear,
-                                                          batch=n_i,
-                                                          C=c_i,
-                                                          H=h_i,
-                                                          W=w_i,
-                                                          stride=stride,
-                                                          forward=decrease)
-    O_new = np.reshape(expected_result_l, (n_i, c_o, h_o, w_o))
-
-    print("_______________________________________________")
-    print("new result: \n{}".format(O_new))
-    print("_______________________________________________")
-
-    if (c_o > c_i):  # leimao edit
-        expected_result_l = reorgyolo_comparison(I_data_linear,
-                                                 batch=n_i,
-                                                 C=c_o,
-                                                 H=h_o,
-                                                 W=w_o,
-                                                 stride=stride,
-                                                 forward=decrease)
-    else:
-        expected_result_l = reorgyolo_comparison(I_data_linear,
-                                                 batch=n_i,
-                                                 C=c_i,
-                                                 H=h_i,
-                                                 W=w_i,
-                                                 stride=stride,
-                                                 forward=decrease)
-    O_exp = np.reshape(expected_result_l, (n_i, c_o, h_o, w_o))
-    print("_______________________________________________")
-    print("expected result: \n{}".format(O_exp))
     print("_______________________________________________")
 
 
--- networks/edsl_examples/tests.py
+++ networks/edsl_examples/tests.py
@@ -4,65 +4,78 @@ import numpy.testing as npt
 from networks.edsl_examples.reorgyolo import *
 
 
-def test_reorgyolo(n_i, c_i, h_i, w_i, stride, decrease):
-
+def create_reorgyolo_data(n_i, c_i, h_i, w_i):
     I_data_linear = np.array(list(range(n_i * c_i * h_i * w_i))).astype(np.int)
     I_data = np.reshape(I_data_linear, (n_i, c_i, h_i, w_i))
+    return I_data
 
+
+def create_and_run_reorgyolo_program(I_data, stride, decrease):
     I = edsl.Tensor(edsl.LogicalShape(plaidml.DType.FLOAT32, I_data.shape))
     O = reorgyolo(I, stride, decrease)
-
-    #create eDSL program, compile and run
     program = edsl.Program('reorgyolo', [O])
     binder = plaidml_exec.Binder(program)
     executable = binder.compile()
     binder.input(I).copy_from_ndarray(I_data)
     executable.run()
     result = binder.output(O).as_ndarray()
-
-    #compute expected results without eDSL
-    if decrease:
-        c_o = c_i // (stride * stride)
-        h_o = h_i * stride
-        w_o = w_i * stride
-    else:
-        c_o = c_i * (stride * stride)
-        h_o = h_i // stride
-        w_o = w_i // stride
-
-    if (c_o > c_i):
-        expected_result_l = reorgyolo_comparison(I_data_linear,
-                                                 batch=n_i,
-                                                 C=c_o,
-                                                 H=h_o,
-                                                 W=w_o,
-                                                 stride=stride,
-                                                 forward=decrease)
-    else:
-        expected_result_l = reorgyolo_comparison(I_data_linear,
-                                                 batch=n_i,
-                                                 C=c_i,
-                                                 H=h_i,
-                                                 W=w_i,
-                                                 stride=stride,
-                                                 forward=decrease)
-
-    expected_result = np.reshape(expected_result_l, (n_i, c_o, h_o, w_o))
-
-    #check results
-    npt.assert_array_equal(result, expected_result)
+    return result
 
 
 class EDSLExampleTests(unittest.TestCase):
 
-    #@unittest.skip("Fails: incorrect output produced")  #TODO: fix
     def tests_reorgyolo_backward(self):
-        test_reorgyolo(1, 4, 6, 6, 2, False)
+        n_i = 2  # number of batches
+        c_i = 4  # number of channels
+        h_i = 6  # height of each channel
+        w_i = 6  # width of each channel
+        stride = 2  # stride
+        decrease = False
+        I_data = create_reorgyolo_data(n_i, c_i, h_i, w_i)
+        result = create_and_run_reorgyolo_program(I_data, stride, decrease)
+        expected_result = reorgyolo_comparison_non_linear(I_data,
+                                                          batch=n_i,
+                                                          C=c_i,
+                                                          H=h_i,
+                                                          W=w_i,
+                                                          stride=stride,
+                                                          forward=decrease)
+
+        #check results
+        npt.assert_array_equal(result, expected_result)
 
     def tests_reorgyolo_forward(self):
-        test_reorgyolo(2, 4, 6, 6, 2, True)
+        n_i = 2  # number of batches
+        c_i = 4  # number of channels
+        h_i = 6  # height of each channel
+        w_i = 6  # width of each channel
+        stride = 2  # stride
+        decrease = True
+        I_data = create_reorgyolo_data(n_i, c_i, h_i, w_i)
+        result = create_and_run_reorgyolo_program(I_data, stride, decrease)
+        expected_result = reorgyolo_comparison_non_linear(I_data,
+                                                          batch=n_i,
+                                                          C=c_i,
+                                                          H=h_i,
+                                                          W=w_i,
+                                                          stride=stride,
+                                                          forward=decrease)
 
-    #def test reorgyolo_reversible:
+        #check results
+        npt.assert_array_equal(result, expected_result)
+
+    def test_reorgyolo_reversible(self):
+        n_i = 2  # number of batches
+        c_i = 4  # number of channels
+        h_i = 6  # height of each channel
+        w_i = 6  # width of each channel
+        stride = 2  # stride
+        decrease = True
+
+        I_data = create_reorgyolo_data(n_i, c_i, h_i, w_i)
+        result = create_and_run_reorgyolo_program(I_data, stride, decrease)
+        result_I = create_and_run_reorgyolo_program(result, stride, not decrease)
+        npt.assert_array_equal(result_I, I_data)
 
 
 if __name__ == '__main__':
