523a327b99e4c6bf963011e828787d85ecec662e
yaoshi <yao.shi@intel.com>
2020-05-17 14:48:44 -0700

    Fix bug in tile transform

--- pmlc/dialect/pxa/transforms/tile.cc
+++ pmlc/dialect/pxa/transforms/tile.cc
@@ -2,6 +2,7 @@
 
 #include <iostream>
 
+#include "mlir/include/mlir/IR/BlockAndValueMapping.h"
 #include "pmlc/dialect/pxa/transforms/tile.h"
 
 namespace pmlc::dialect::pxa {
@@ -31,11 +32,23 @@ void performTiling(AffineParallelOp op, llvm::ArrayRef<int64_t> tileSizes) {
   // Make the inner parallel for (abve all other code);
   auto inner = builder.create<AffineParallelOp>(
       op.getLoc(), op.getResultTypes(), lbMap, outerIdxs, ubMap, outerIdxs);
-  // Splice instructions into the interior
+  // Build a map from old indices to new indices
+  mlir::BlockAndValueMapping mapper;
+  auto innerIdxs = inner.getBody()->getArguments();
+  for (unsigned i = 0; i < outerIdxs.size(); ++i) {
+    mapper.map(outerIdxs[i], innerIdxs[i]);
+  }
+  // Clone new instructions into the interior
   auto &innerLoopOps = inner.getBody()->getOperations();
   auto &outerLoopOps = outerBody->getOperations();
-  innerLoopOps.splice(std::prev(innerLoopOps.end()), outerLoopOps,
-                      std::next(outerLoopOps.begin(), 1), outerLoopOps.end());
+  for (auto& origOp : outerLoopOps) {
+    if (&origOp == inner.getOperation()) {
+      continue;
+    }
+    Operation* newOp = origOp.clone(mapper);
+    innerLoopOps.push_back(newOp);
+  }
+  outerLoopOps.erase(std::next(outerLoopOps.begin(), 1), outerLoopOps.end());
   // Add a return of the values of the inner to the outer
   builder.setInsertionPointToEnd(op.getBody());
   builder.create<AffineYieldOp>(op.getLoc(), inner.getResults());
