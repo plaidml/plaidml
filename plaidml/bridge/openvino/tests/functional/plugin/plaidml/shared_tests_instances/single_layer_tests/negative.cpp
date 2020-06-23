5bfaece285139114861257078b23c75e15fcea6c
jd-bartlett96 <jd.bartlett96@gmail.com>
2020-06-23 12:14:26 -0700

    Data type correction

--- plaidml/bridge/openvino/tests/functional/plugin/plaidml/shared_tests_instances/single_layer_tests/negative.cpp
+++ plaidml/bridge/openvino/tests/functional/plugin/plaidml/shared_tests_instances/single_layer_tests/negative.cpp
@@ -10,13 +10,13 @@ using LayerTestsDefinitions::NegativeLayerTest;
 
 namespace {
 
-std::vector<std::vector<std::vector<size_t>>> inShapes = {{2},                    //
-                                                          {1, 1, 1, 3},           //
-                                                          {1, 2, 4},              //
-                                                          {1, 4, 4},              //
-                                                          {1, 4, 4, 1},           //
-                                                          {1, 1, 1, 1, 1, 1, 3},  //
-                                                          {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
+std::vector<std::vector<size_t>> inShapes = {{2},                    //
+                                             {1, 1, 1, 3},           //
+                                             {1, 2, 4},              //
+                                             {1, 4, 4},              //
+                                             {1, 4, 4, 1},           //
+                                             {1, 1, 1, 1, 1, 1, 3},  //
+                                             {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
 
 std::vector<InferenceEngine::Precision> netPrecisions = {
     InferenceEngine::Precision::FP32,
