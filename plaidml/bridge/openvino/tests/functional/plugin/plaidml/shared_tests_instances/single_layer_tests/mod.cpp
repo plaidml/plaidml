feedbdf30ccdd00aa0b4e3f28b0ca2e7b300d401
jd-bartlett96 <jd.bartlett96@gmail.com>
2020-06-24 12:47:41 -0700

    Adding mod op

---
+++ plaidml/bridge/openvino/tests/functional/plugin/plaidml/shared_tests_instances/single_layer_tests/mod.cpp
@@ -0,0 +1,27 @@
+// Copyright (C) 2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <cstddef>
+#include <map>
+#include <vector>
+
+#include "common_test_utils/test_constants.hpp"
+#include "single_layer_tests/mod.hpp"
+
+using LayerTestsDefinitions::ModLayerTest;
+
+namespace {
+const std::vector<InferenceEngine::Precision> netPrecisions = {
+    InferenceEngine::Precision::FP32,
+};
+
+const std::vector<std::vector<std::size_t>> inputShapes = {
+    {std::vector<std::size_t>({1, 30}), std::vector<std::size_t>({1, 30})}};
+
+INSTANTIATE_TEST_CASE_P(CompareWithRefs, ModLayerTest,
+                        ::testing::Combine(::testing::ValuesIn(netPrecisions), ::testing::Values(inputShapes),
+                                           ::testing::Values(CommonTestUtils::DEVICE_PLAIDML)),
+                        ModLayerTest::getTestCaseName);
+
+}  // namespace
