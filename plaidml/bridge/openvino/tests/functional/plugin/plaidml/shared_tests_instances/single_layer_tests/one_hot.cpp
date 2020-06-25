676b65234419c51b9aca1fcd2716695693b2e306
1sanz <sana.nazir@intel.com>
2020-06-25 14:35:43 -0700

    minor fix

--- plaidml/bridge/openvino/tests/functional/plugin/plaidml/shared_tests_instances/single_layer_tests/one_hot.cpp
+++ plaidml/bridge/openvino/tests/functional/plugin/plaidml/shared_tests_instances/single_layer_tests/one_hot.cpp
@@ -12,6 +12,7 @@ using LayerTestsDefinitions::OneHotLayerTest;
 namespace {
 
 const std::vector<InferenceEngine::Precision> netPrecisions = {
+    InferenceEngine::Precision::I32,  //
     InferenceEngine::Precision::I64,  //
 };
 
@@ -21,12 +22,12 @@ const float on_value = 3;
 const float off_value = 1;
 
 INSTANTIATE_TEST_CASE_P(OneHotCheck, OneHotLayerTest,
-                        ::testing::Combine(::testing::Values(axis),                              //
-                                           ::testing::Values(depth),                             //
-                                           ::testing::Values(on_value),                          //
-                                           ::testing::Values(off_value),                         //
-                                           ::testing::ValuesIn(netPrecisions),                   //
-                                           ::testing::Values(std::vector<size_t>({2, 2})),       //
-                                           ::testing::Values(CommonTestUtils::DEVICE_PLAIDML)),  //
+                        ::testing::Combine(::testing::Values(axis),                               //
+                                           ::testing::Values(depth),                              //
+                                           ::testing::Values(on_value),                           //
+                                           ::testing::Values(off_value),                          //
+                                           ::testing::ValuesIn(netPrecisions),                    //
+                                           ::testing::Values(std::vector<size_t>({1, 9, 2, 4})),  //
+                                           ::testing::Values(CommonTestUtils::DEVICE_PLAIDML)),   //
                         OneHotLayerTest::getTestCaseName);
 }  // namespace
