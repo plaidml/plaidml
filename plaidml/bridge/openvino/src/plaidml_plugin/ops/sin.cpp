549c38456e744970ee9a975145f9d852c7fe396e
Namrata Choudhury <namrata.choudhury@intel.com>
2020-06-29 08:44:54 -0700

    add comment for sin workaround

--- plaidml/bridge/openvino/src/plaidml_plugin/ops/sin.cpp
+++ plaidml/bridge/openvino/src/plaidml_plugin/ops/sin.cpp
@@ -19,7 +19,7 @@ namespace PlaidMLPlugin {
 static OpRegistration reg("sin", [](const Context& ctx) {
   IE_ASSERT(ctx.operands.size() == 1);
   auto I = ctx.operands.at(0);
-  return edsl::make_tuple(cos(I - M_PI / 2));
+  return edsl::make_tuple(cos(I - M_PI / 2));  // workaround till lowering for sin is available
 });
 
 }  // namespace PlaidMLPlugin
