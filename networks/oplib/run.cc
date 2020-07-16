0c4cbc5c751b5581790d943f883ec4461b22a0ef
Frank Laub <frank.laub@intel.com>
2020-07-16 00:48:49 +0000

    cleanups

--- networks/oplib/run.cc
+++ networks/oplib/run.cc
@@ -5,6 +5,7 @@
 #include "networks/oplib/oplib.h"
 #include "plaidml/exec/exec.h"
 #include "plaidml/op/op.h"
+#include "pmlc/util/logging.h"
 
 // TODO: command line parsing to dispatch on network to run
 int main(int argc, char** argv) {
@@ -14,10 +15,11 @@ int main(int argc, char** argv) {
     plaidml::op::init();
     plaidml::exec::init();
     auto program = networks::oplib::buildResnet50();
-    // std::cout << program.str() << std::endl;
+    IVLOG(1, program.str());
     auto executable = plaidml::exec::Binder(program).compile();
     std::cout << "Running..." << std::endl;
     executable->run();
+    std::cout << "EXIT_SUCCESS" << std::endl;
     return EXIT_SUCCESS;
   } catch (const std::exception& ex) {
     std::cerr << "Caught unhandled exception: " << ex.what() << std::endl;
