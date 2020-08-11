dc55260c337e6f3f9ce029c0896310e0e9abf17e
abialas <artur.bialas@intel.com>
2020-08-11 10:00:13 +0200

    i1StorageToi32 changes after code review

--- pmlc/dialect/stdx/transforms/i1_to_i32.cc
+++ pmlc/dialect/stdx/transforms/i1_to_i32.cc
@@ -12,7 +12,7 @@ namespace pmlc::dialect::stdx {
 
 namespace {
 
-/// Changes loadOp from i1 memref to loadOp i32 followed by creting constant
+/// Changes loadOp from i1 memref to loadOp i32 followed by creating constant
 /// value 0 of i32 type and doing cmpi afterwards, next this bool-like type will
 /// be produced
 class LoadOpI1ToI32 final : public OpRewritePattern<LoadOp> {
@@ -34,7 +34,7 @@ public:
 };
 } // namespace
 
-/// Changes loadOp from i1 memref to loadOp i32 followed by creting constant
+/// Changes loadOp from i1 memref to loadOp i32 followed by creating constant
 /// value 0 of i32 type and doing cmpi afterwards, next this bool-like type will
 /// be produced
 LogicalResult LoadOpI1ToI32::matchAndRewrite(LoadOp loadOp,
@@ -53,8 +53,7 @@ LogicalResult LoadOpI1ToI32::matchAndRewrite(LoadOp loadOp,
   auto newLoadOp =
       rewriter.create<LoadOp>(loc, loadOp.memref(), loadOp.indices());
 
-  auto const0 =
-      rewriter.create<ConstantOp>(loc, rewriter.getIntegerAttr(destType, 0));
+  auto const0 = rewriter.create<ConstantIntOp>(loc, 0, destType);
   auto cmpOp = rewriter.create<CmpIOp>(
       loc, CmpIPredicate::ne, newLoadOp.getResult(), const0.getResult());
 
@@ -74,10 +73,8 @@ LogicalResult StoreOpI1ToI32::matchAndRewrite(StoreOp storeOp,
   auto destType = rewriter.getIntegerType(32);
   auto loc = storeOp.getLoc();
 
-  auto const0 =
-      rewriter.create<ConstantOp>(loc, rewriter.getIntegerAttr(destType, 0));
-  auto const1 =
-      rewriter.create<ConstantOp>(loc, rewriter.getIntegerAttr(destType, 1));
+  auto const0 = rewriter.create<ConstantIntOp>(loc, 0, destType);
+  auto const1 = rewriter.create<ConstantIntOp>(loc, 1, destType);
   auto selOp = rewriter.create<SelectOp>(
       loc, storeOp.value(), const1.getResult(), const0.getResult());
 
