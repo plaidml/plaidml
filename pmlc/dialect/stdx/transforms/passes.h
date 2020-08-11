37af646575d9f2fe7c336335e20338be801e0f51
abialas1 <artur.bialas@intel.com>
2020-08-11 19:15:30 +0200

    i1 Storage To i32 pass added (#1074)
    * i1StorageToi8 pass added
    
    * changes after CR
    
    * simplified mlir test for i1_to_i8
    
    * updated mlir test for i1_to_i8
    
    * changed implementation and moved to i32 due to spirv issues
    
    * i1StorageToi32 changes after code review

--- pmlc/dialect/stdx/transforms/passes.h
+++ pmlc/dialect/stdx/transforms/passes.h
@@ -12,5 +12,6 @@ class Pass;
 namespace pmlc::dialect::stdx {
 
 std::unique_ptr<mlir::Pass> createBoundsCheckPass();
+std::unique_ptr<mlir::Pass> createI1StorageToI32Pass();
 
 } // namespace pmlc::dialect::stdx
