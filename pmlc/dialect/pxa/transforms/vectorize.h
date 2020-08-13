cdc30bcd396be55b62bd35d6fb347594b72be0e1
Jeremy Bruestle <jeremy.bruestle@intel.com>
2020-08-13 12:10:08 -0700

    Part 1

--- pmlc/dialect/pxa/transforms/vectorize.h
+++ pmlc/dialect/pxa/transforms/vectorize.h
@@ -6,8 +6,12 @@
 
 namespace pmlc::dialect::pxa {
 
+// Explicitly attempt to vectorize on a specific loop/index/size
 LogicalResult performVectorization(mlir::AffineParallelOp op,
                                    mlir::BlockArgument index,
                                    unsigned vectorSize);
+
+// Attempt to vectorize a loop on the stride 1 index of it's output
+LogicalResult simpleVectorize(mlir::AffineParallelOp op, unsigned vecSize);
 
 } // namespace pmlc::dialect::pxa
