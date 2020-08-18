75560779a09ca989ecd8d0cf8e9170f82b062c80
Adam Straw <adam.d.straw@intel.com>
2020-08-18 12:19:21 -0700

    cleanup; addressing feedback

--- pmlc/dialect/pxa/transforms/buffer_placement.cc
+++ pmlc/dialect/pxa/transforms/buffer_placement.cc
@@ -12,58 +12,42 @@ using namespace mlir; // NOLINT[build/namespaces]
 
 namespace pmlc::dialect::pxa {
 
-template <typename Iterator, typename Range>
-static Iterator getLast(Range range) {
-  auto it = range.begin();
-  auto itEnd = range.end();
-  auto itNext = it;
-  while (itNext != itEnd) {
-    it = itNext;
-    ++itNext;
-  }
-  return it;
-}
-
 struct BufferPlacementPass : public BufferPlacementBase<BufferPlacementPass> {
   void runOnFunction() final {
     auto fn = getFunction();
     fn.walk([&](AllocOp alloc) {
-      IVLOG(1, "alloc: " << debugString(*alloc));
+      IVLOG(3, "alloc: " << debugString(*alloc));
 
       Operation *lastOp = alloc;
-      auto uses = getIndirectUses(alloc);
-      auto itUse = uses.begin();
-      while (itUse != uses.end()) {
-        Operation *use = itUse->getOwner();
-        IVLOG(1, "  use: " << debugString(*use));
+      Block *allocBlock = alloc.getOperation()->getBlock();
+      for (auto &itUse : getIndirectUses(alloc)) {
+        Operation *use = itUse.getOwner();
+        IVLOG(3, "  use: " << debugString(*use));
 
-        Block *allocBlock = alloc.getOperation()->getBlock();
         while (use->getBlock() != allocBlock) {
           use = use->getParentOp();
           assert(use && "use does not have a common ancestor");
-          IVLOG(1, "  up, use: " << debugString(*use));
+          IVLOG(3, "  parent: " << debugString(*use));
+        }
+
+        if (isa<ReturnOp>(use)) {
+          IVLOG(3, "  return");
+          return;
         }
 
         if (!use->isBeforeInBlock(lastOp)) {
           lastOp = use;
         }
-
-        ++itUse;
       }
-      IVLOG(1, "  last: " << debugString(*lastOp));
-
-      if (isa<ReturnOp>(lastOp)) {
-        IVLOG(1, "  return");
-        return;
-      }
+      IVLOG(3, "  last: " << debugString(*lastOp));
 
       Operation *nextOp = lastOp->getNextNode();
       if (!nextOp) {
-        IVLOG(1, "  terminator");
+        IVLOG(3, "  terminator");
         return;
       }
 
-      IVLOG(1, "  nextOp: " << debugString(*nextOp));
+      IVLOG(3, "  nextOp: " << debugString(*nextOp));
       OpBuilder builder(nextOp);
       builder.create<DeallocOp>(alloc.getLoc(), alloc);
     });
