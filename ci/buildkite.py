00a24e75a900e1b2e60d39a4bb326b5781d55682
Frank Laub <frank.laub@intel.com>
2020-08-19 04:55:10 +0000

    Try fix

--- ci/buildkite.py
+++ ci/buildkite.py
@@ -225,8 +225,16 @@ def make_all_wheels(workdir):
 
 
 def download_test_artifacts(pattern):
-    util.buildkite_download(pattern, '.')
-    util.buildkite_download(pattern.replace('/', '\\'), '.')
+    try:
+        util.buildkite_download(pattern, '.')
+    except:
+        # This happens if no artifacts were produced at all (for *nix)
+        pass
+    try:
+        util.buildkite_download(pattern.replace('/', '\\'), '.')
+    except:
+        # This happens if no artifacts were produced at all (for windows)
+        pass
     for path in glob.glob(pattern):
         src = Path(path)
         tgt = Path(path.replace('\\', '/'))
