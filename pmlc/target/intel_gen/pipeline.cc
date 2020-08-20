b710a6579733741946b5c83181e8ca9951804ad4
xin1.wang <xin1.wang@intel.com>
2020-08-20 16:38:00 -0400

    Add LowerStdxAndStdToLLVMPass to fix stdx.reshape legalization. Passed testBatchFlatten.
    Signed-off-by: xin1.wang <xin1.wang@intel.com>

--- pmlc/target/intel_gen/pipeline.cc
+++ pmlc/target/intel_gen/pipeline.cc
@@ -4,9 +4,11 @@
 #include "mlir/Conversion/GPUToVulkan/ConvertGPUToVulkanPass.h"
 #include "mlir/Conversion/SCFToGPU/SCFToGPUPass.h"
 #include "mlir/Conversion/SCFToStandard/SCFToStandard.h"
+#include "mlir/Conversion/StandardToLLVM/ConvertStandardToLLVM.h"
 #include "mlir/Conversion/StandardToLLVM/ConvertStandardToLLVMPass.h"
 #include "mlir/Conversion/StandardToSPIRV/ConvertStandardToSPIRVPass.h"
 #include "mlir/Dialect/GPU/Passes.h"
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/Dialect/SPIRV/Passes.h"
 #include "mlir/Dialect/SPIRV/SPIRVDialect.h"
 #include "mlir/Dialect/SPIRV/SPIRVOps.h"
@@ -19,6 +21,7 @@
 #include "pmlc/conversion/SCFToGPU/SCFToGPUPass.h"
 #include "pmlc/conversion/gpu/lowering.h"
 #include "pmlc/conversion/pxa_to_affine/passes.h"
+#include "pmlc/conversion/stdx_to_llvm/passes.h"
 #include "pmlc/conversion/tile_to_pxa/passes.h"
 #include "pmlc/dialect/pxa//transforms/passes.h"
 #include "pmlc/dialect/stdx/transforms/passes.h"
@@ -29,6 +32,36 @@ using namespace mlir; // NOLINT[build/namespaces]
 namespace pmlc::target::intel_gen {
 
 namespace {
+// Lower stdx and std dialect to llvm dialect
+struct LowerStdxAndStdToLLVMPass
+    : public PassWrapper<LowerStdxAndStdToLLVMPass, OperationPass<ModuleOp>> {
+  void runOnOperation() override {
+    auto module = getOperation();
+    auto *context = module.getContext();
+
+    LowerToLLVMOptions options = {
+        /*useBarePtrCallConv=*/false,
+        /*emitCWrappers=*/true,
+        /*indexBitwidth=*/kDeriveIndexBitwidthFromDataLayout,
+        /*useAlignedAlloc=*/false,
+    };
+    LLVMTypeConverter typeConverter(context, options);
+
+    OwningRewritePatternList patterns;
+    populateStdToLLVMConversionPatterns(typeConverter, patterns);
+    conversion::stdx_to_llvm::populateStdXToLLVMConversionPatterns(
+        typeConverter, patterns);
+
+    LLVMConversionTarget target(*context);
+    if (failed(applyPartialConversion(module, target, patterns))) {
+      signalPassFailure();
+    }
+  }
+};
+
+std::unique_ptr<Pass> createLowerStdxAndStdToLLVMPass() {
+  return std::make_unique<LowerStdxAndStdToLLVMPass>();
+}
 
 void pipelineBuilder(OpPassManager &pm) {
   pm.getContext()->getOrLoadDialect<spirv::SPIRVDialect>();
@@ -64,12 +97,7 @@ void pipelineBuilder(OpPassManager &pm) {
 
   // GPU to Vulkan.
   pm.addPass(conversion::gpu::createConvertGpuLaunchFuncToVulkanCallsPass());
-  pm.addPass(createLowerToLLVMPass(LowerToLLVMOptions{
-      /*useBarePtrCallConv=*/false,
-      /*emitCWrappers=*/true,
-      /*indexBitwidth=*/kDeriveIndexBitwidthFromDataLayout,
-      /*useAlignedAlloc=*/false,
-  }));
+  pm.addPass(createLowerStdxAndStdToLLVMPass());
 }
 
 } // namespace
