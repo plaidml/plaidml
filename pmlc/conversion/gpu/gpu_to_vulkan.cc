9dd932d6d883e93e63f4cdc011cc899d0355cafb
abialas <artur.bialas@intel.com>
2020-08-21 13:33:38 +0200

    added missing header

--- pmlc/conversion/gpu/gpu_to_vulkan.cc
+++ pmlc/conversion/gpu/gpu_to_vulkan.cc
@@ -1,3 +1,5 @@
+// Copyright 2020, Intel Corporation
+
 #include "mlir/Conversion/GPUToVulkan/ConvertGPUToVulkanPass.h"
 #include "mlir/Dialect/GPU/GPUDialect.h"
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
