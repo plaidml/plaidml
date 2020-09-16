4b4a39cc0c9c687ff6d0b4a450d68f3c3382a22d
Denise G Kutnick <denise.g.kutnick@intel.com>
2020-09-16 13:47:15 -0700

    fixes

--- plaidml/bridge/tensorflow/tests/i3d_control.py
+++ plaidml/bridge/tensorflow/tests/i3d_control.py
@@ -13,11 +13,6 @@
 # limitations under the License.
 # ============================================================================
 """Loads a sample video and classifies using a trained Kinetics checkpoint."""
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import numpy as np
 import tensorflow as tf
 
