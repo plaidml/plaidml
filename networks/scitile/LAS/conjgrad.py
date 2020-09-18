a91ad00e99a54c74abcfe186328d6681d4dabf9c
John Bartlett <jd.bartlett96@gmail.com>
2020-09-18 14:59:34 -0700

    Added test case data & autostopping on conjgrad

--- networks/scitile/LAS/conjgrad.py
+++ networks/scitile/LAS/conjgrad.py
@@ -7,23 +7,18 @@ import plaidml.op as op
 import plaidml.tile as tile
 
 
-
-N = int(sys.argv[1])
-
-# Generate a reasonable test system
-np_A = np.empty((N,N))
-for i in range(N):
-    for j in range(N):
-        np_A[i,j] = N - abs(i-j)
-# b = np.random.rand(N)
-b = np.zeros(N)
-b[-1] = 1
+# Load test case
+print("Loading test case data")
+testname = sys.argv[1]
+testdat = np.load('test_data/'+testname+'.npz')
+np_A, b = testdat['A'], testdat['b']
+N = len(b)
+print("Test case data loaded")
 
 np_x = np.zeros(N)
 np_r = b - np.matmul(np_A, np_x)
 np_rsq = np.matmul(np_r, np_r)
 np_p = np_r.copy()
-
 dtype = plaidml.DType.FLOAT32
 ctx = plaidml.Context()
 with plaidml.open_first_device(ctx) as dev:
@@ -33,15 +28,21 @@ with plaidml.open_first_device(ctx) as dev:
     X = tile.Value.from_ndims(1)
     R = tile.Value.from_ndims(1)
 
-    Ap = op.matmul(A, P)
-    alpha = RSQ/op.matmul(P, Ap)
+    Ap = tile.Operation("""function (A[I,J], P[J]) -> (Ap) {
+                            Ap[i: I] = +(A[i,j] * P[j]);
+                        }
+                        """, [('A', A), ('P', P)], [('Ap', tile.Shape(dtype, (N,)))]).outputs['Ap']
+    pAp = tile.Operation("""function(Ap[I], P[I]) -> (C) {
+                        C[:] = +(Ap[i] * P[i]);
+                    }""", [('Ap', Ap), ('P', P)], [('C', tile.Shape(dtype, ()))]).outputs['C']
+    alpha = RSQ/pAp
     OX = X + alpha*P
     OR = R - alpha*Ap
     RSQN = op.matmul(OR, OR)
     OP = OR + RSQN/RSQ * P
 
-    func = tile.compose(ctx, dev, inputs=[('A', A), ('P', P), ('RSQ', RSQ), ('X', X), ('R', R)], 
-                                  outputs=[('OP', OP), ('RSQN', RSQN), ('OX', OX), ('OR', OR)])
+    func = tile.compose(ctx, dev, inputs=[('A', A), ('P', P), ('RSQ', RSQ), ('X', X), ('R', R)],
+                                  outputs=[('OX', OX), ('OR', OR), ('RSQN', RSQN), ('OP', OP)])
     invoker = plaidml.Invoker(ctx, func)
 
     pld_A = plaidml.Tensor(dev, plaidml.Shape(ctx, dtype, *np_A.shape))
@@ -72,20 +73,28 @@ with plaidml.open_first_device(ctx) as dev:
     invoker.set_input('X', pld_X)
     invoker.set_input('R', pld_R)
 
-    invoker.set_output('OP', pld_P)
-    invoker.set_output('RSQN', pld_RSQ)
     invoker.set_output('OX', pld_X)
     invoker.set_output('OR', pld_R)
+    invoker.set_output('RSQN', pld_RSQ)
+    invoker.set_output('OP', pld_P)
     
     t0 = time.time()
     for tt in range(N):
         invoker.invoke()
+        with pld_RSQ.mmap_current() as view:
+            if view[0] < 1e-7:
+                print("Terminated at timestep ", tt)
+                break
 
     with pld_X.mmap_current() as view:
         view.copy_to_ndarray(np_x)
 
-    print("Time: ",time.time()-t0)
+    print("PlaidML Time: ",time.time()-t0)
 
+    t0 = time.time()
     true_sol = np.linalg.solve(np_A, b)
-    err = 100*np.mean(np.abs((np_x[abs(true_sol)>1e-2]-true_sol[abs(true_sol)>1e-2])/true_sol[abs(true_sol)>1e-2]))
-    print("Error: ",err)
+    print("Numpy Time: ", time.time()-t0)
+
+    thrs = 0.01*np.mean(true_sol)
+    err = 100*np.mean(np.abs((np_x[abs(true_sol)>thrs]-true_sol[abs(true_sol)>thrs])/true_sol[abs(true_sol)>thrs]))
+    print("Avg. % Error: ",err)
--- networks/scitile/LAS/numpy_conjgrad.py
+++ networks/scitile/LAS/numpy_conjgrad.py
@@ -10,7 +10,6 @@ def step(A, b, x, r, p, rsold):
     r -= alpha*Ap
     rsnew = r.T @ r
     p = r + p * (rsnew / rsold)
-    print(Ap, x, r, p, alpha, rsnew)
     return x, r, p, rsnew
 
 # Computes solution to Ax = b using conjugate gradient method
@@ -18,20 +17,30 @@ def conjgrad(A, b, x):
     r = b - A @ x
     p = r.copy()
     rsold = r.T @ r
-    for i in range(1):
+    for i in range(50):
         x, r, p, rsold = step(A, b, x, r, p, rsold)
+        if rsold<1e-10:
+            print("Terminated at timestep ", i)
+            break
     return x
 
-N = int(sys.argv[1])
+# N = int(sys.argv[1])
+testname = sys.argv[1]
 
 # Generate a reasonable test system
-A = np.empty((N,N))
-for i in range(N):
-    for j in range(N):
-        A[i,j] = N - abs(i-j)
-# b = np.random.rand(N,1)
-b = np.zeros((N,1))
-b[-1] = 1
+# A = np.empty((N,N))
+# for i in range(N):
+#     for j in range(N):
+#         A[i,j] = N - abs(i-j)
+# # b = np.random.rand(N,1)
+# b = np.zeros((N,1))
+# b[-1] = 1
+
+# Load input
+A = np.load('test_data/'+testname+'_A.npy')
+b = np.load('test_data/'+testname+'_b.npy')
+N = len(b)
+b = np.reshape(b, (N,1))
 x0 = np.zeros((N,1))
 
 # Numpy black-box solver
@@ -47,4 +56,4 @@ step_tm = time.time()-t0
 print("N: ", N)
 print("Numpy Linalg Solution Time: ", np_tm)
 print("Conjugate Gradient Solution Time: ", step_tm)
-print("Avg. Error: ", np.mean(np.abs((cg_x[abs(np_x)>1e-4]-np_x[abs(np_x)>1e-4])/np_x[abs(np_x)>1e-4])))
+print("Avg. % Error: ", 100*np.mean(np.abs((cg_x[abs(np_x)>1e-4]-np_x[abs(np_x)>1e-4])/np_x[abs(np_x)>1e-4])))
