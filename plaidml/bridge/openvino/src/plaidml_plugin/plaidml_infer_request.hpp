5de2e132fecda91e040e721084d28e0fee7b7efa
Tim Zerrell <tim.zerrell@intel.com>
2020-09-28 16:22:55 -0700

    WIP Fix OV-PlaidML tensor mapping

--- plaidml/bridge/openvino/src/plaidml_plugin/plaidml_infer_request.hpp
+++ plaidml/bridge/openvino/src/plaidml_plugin/plaidml_infer_request.hpp
@@ -7,9 +7,10 @@
 #include <map>
 #include <memory>
 #include <string>
-#include <unordered_map>
 
 #include "cpp_interfaces/impl/ie_infer_request_internal.hpp"
+
+#include "ngraph/descriptor/tensor.hpp"
 
 #include "plaidml/edsl/edsl.h"
 #include "plaidml/exec/exec.h"
@@ -23,7 +24,7 @@ class PlaidMLInferRequest : public InferenceEngine::InferRequestInternal {
   explicit PlaidMLInferRequest(const InferenceEngine::InputsDataMap& networkInputs,
                                const InferenceEngine::OutputsDataMap& networkOutputs,
                                const plaidml::edsl::Program& program,
-                               const std::unordered_map<std::string, plaidml::edsl::Tensor>& tensorMap);
+                               const std::map<std::string, plaidml::edsl::Tensor>& tensorIONameMap);
 
   void InferImpl() override;
 
@@ -36,7 +37,7 @@ class PlaidMLInferRequest : public InferenceEngine::InferRequestInternal {
   void SyncOutput();
 
  private:
-  std::unordered_map<std::string, plaidml::edsl::Tensor> tensorIOMap_;
+  std::map<std::string, plaidml::edsl::Tensor> tensorIONameMap_;
   plaidml::exec::Binder binder_;
   std::shared_ptr<plaidml::exec::Executable> exec_;
 };
