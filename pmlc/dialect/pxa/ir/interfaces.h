c222c96780af22bbe23cc57bf245a5f49a6d63d4
Artur Bialas <artur.bialas@intel.com>
2020-10-12 13:53:37 +0200

    added mlir namespace to cast

--- pmlc/dialect/pxa/ir/interfaces.h
+++ pmlc/dialect/pxa/ir/interfaces.h
@@ -1,15 +1,9 @@
 // Copyright 2020, Intel Corporation
 #pragma once
 
-#include "mlir/Analysis/AffineStructures.h"
-#include "mlir/Dialect/Affine/IR/AffineOps.h"
 #include "mlir/Dialect/StandardOps/IR/Ops.h"
-#include "mlir/IR/AffineExpr.h"
 #include "mlir/IR/AffineMap.h"
-#include "mlir/IR/Dialect.h"
 #include "mlir/IR/OpDefinition.h"
-#include "mlir/IR/StandardTypes.h"
-#include "mlir/Interfaces/SideEffectInterfaces.h"
 
 namespace pmlc::dialect::pxa {
 
