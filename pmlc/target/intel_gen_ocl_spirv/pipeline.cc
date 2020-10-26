c7ddf959634489c6643e5ec77d7a075ab979fed9
Artur Bialas <artur.bialas@intel.com>
2020-10-26 16:19:43 +0100

    remove memref flow opt

--- pmlc/target/intel_gen_ocl_spirv/pipeline.cc
+++ pmlc/target/intel_gen_ocl_spirv/pipeline.cc
@@ -99,10 +99,6 @@ void pipelineBuilder(OpPassManager &pm) {
   pm.addPass(createCanonicalizerPass());
   pm.addPass(createCSEPass());
 
-  pm.addPass(createMemRefDataFlowOptPass());
-  pm.addPass(createCanonicalizerPass());
-  pm.addPass(createCSEPass());
-
   // Pack dims
   pm.addPass(pmlc::target::intel_gen::createAffineIndexPackPass());
   pm.addPass(createCanonicalizerPass());
