1fe429995c8f1133be062be1e8e19c023f0273bf
Rob Earhart <robert.earhart@intel.com>
2020-11-05 20:35:40 -0800

    Minor cleanups

--- pmlc/dialect/abi/ir/dialect.h
+++ pmlc/dialect/abi/ir/dialect.h
@@ -1,17 +1,6 @@
-// Copyright 2019, Intel Corporation
+// Copyright 2020, Intel Corporation
 
 #pragma once
-
-/*
-#include "mlir/Analysis/AffineStructures.h"
-#include "mlir/Dialect/Affine/IR/AffineMemoryOpInterfaces.h"
-#include "mlir/Dialect/Affine/IR/AffineOps.h"
-#include "mlir/Dialect/StandardOps/IR/Ops.h"
-#include "mlir/IR/AffineExpr.h"
-#include "mlir/Interfaces/SideEffectInterfaces.h"
-
-#include "pmlc/util/enums.h"
- */
 
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/IR/Dialect.h"
@@ -24,50 +13,6 @@
 
 namespace pmlc::dialect::abi {
 
-/*
-using llvm::SmallVectorImpl;
-using mlir::AffineExpr;
-using mlir::AffineMap;
-using mlir::AffineMapAttr;
-using mlir::AffineValueMap;
-using mlir::AffineYieldOp;
-using mlir::ArrayAttr;
-using mlir::AtomicRMWKind;
-using mlir::Attribute;
-using mlir::BoolAttr;
-using mlir::DictionaryAttr;
-using mlir::FloatType;
-using mlir::IndexType;
-using mlir::IntegerAttr;
-using mlir::IntegerType;
-using mlir::Location;
-using mlir::LoopLikeOpInterface;
-using mlir::MemoryEffectOpInterface;
-using mlir::MemRefType;
-using mlir::MLIRContext;
-using mlir::NamedAttribute;
-using mlir::Op;
-using mlir::OpAsmParser;
-using mlir::OpAsmPrinter;
-using mlir::OpAsmSetValueNameFn;
-using mlir::OpBuilder;
-using mlir::OperationState;
-using mlir::OpFoldResult;
-using mlir::OpInterface;
-using mlir::OwningRewritePatternList;
-using mlir::ParseResult;
-using mlir::RankedTensorType;
-using mlir::ShapedType;
-using mlir::StringAttr;
-using mlir::StringRef;
-using mlir::Type;
-using mlir::TypeAttr;
-using mlir::ValueRange;
-using mlir::VectorType;
-
-namespace MemoryEffects = mlir::MemoryEffects;
-namespace SideEffects = mlir::SideEffects;
-*/
 using mlir::ArrayRef;
 using mlir::LogicalResult;
 using mlir::ModuleOp;
