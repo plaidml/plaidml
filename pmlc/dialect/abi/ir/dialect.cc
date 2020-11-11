bf4cc5ec2ea559e2599c7d4c756342cb512c6020
Rob Earhart <robert.earhart@intel.com>
2020-11-11 13:12:48 -0800

    Add ABI dialect

---
+++ pmlc/dialect/abi/ir/dialect.cc
@@ -0,0 +1,19 @@
+// Copyright 2020, Intel Corporation
+
+#include "pmlc/dialect/abi/ir/dialect.h"
+
+#define GET_OP_CLASSES
+#include "pmlc/dialect/abi/ir/ops.cc.inc"
+
+namespace pmlc::dialect::abi {
+
+using namespace mlir; // NOLINT
+
+void ABIDialect::initialize() {
+  addOperations<
+#define GET_OP_LIST
+#include "pmlc/dialect/abi/ir/ops.cc.inc" // NOLINT
+      >();
+}
+
+} // namespace pmlc::dialect::abi
