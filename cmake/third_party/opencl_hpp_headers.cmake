21cbb479e197a575ac5346f4c52a1d12d81397f4
Artur Bialas <artur.bialas@intel.com>
2020-11-19 17:29:51 +0100

    removed cmake changes

--- cmake/third_party/opencl_hpp_headers.cmake
+++ cmake/third_party/opencl_hpp_headers.cmake
@@ -4,9 +4,6 @@ FetchContent_Declare(
   URL_HASH SHA256=127936b3a5ef147f23b85fb043599d1480e9e57acabe2d2a67c5dac05aa4ad70
 )
 FetchContent_MakeAvailable(opencl_hpp_headers)
-set(BUILD_DOCS OFF CACHE BOOL "Build Documentation" FORCE)
-set(BUILD_EXAMPLES OFF CACHE BOOL "Build Examples" FORCE)
-set(BUILD_TESTS OFF CACHE BOOL "Build Unit Tests" FORCE)
 
 add_library(opencl_hpp_headers INTERFACE)
 target_include_directories(opencl_hpp_headers INTERFACE ${opencl_hpp_headers_SOURCE_DIR}/include)
