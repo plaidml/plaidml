cc63234c4ffd6503a9726a6f68e0464bde47ced2
Ling, Liyang <liyang.ling@intel.com>
2020-11-25 11:16:58 +0800

    Adjust format for openvino op

--- plaidml/bridge/openvino/ops/roi_align.cpp
+++ plaidml/bridge/openvino/ops/roi_align.cpp
@@ -8,6 +8,7 @@
 #include "plaidml_util.hpp"
 
 #include <algorithm>
+#include <cmath>
 
 using namespace plaidml;          // NOLINT[build/namespaces]
 using namespace InferenceEngine;  // NOLINT[build/namespaces]
@@ -37,47 +38,58 @@ edsl::Tensor crop_and_resize(edsl::Tensor image, std::vector<std::vector<float>>
 
 namespace PlaidMLPlugin {
 
-static OpRegistration reg("roialign", [](const Context& ctx) {
-  IE_ASSERT(ctx.operands.size() == 3);
-  auto X = ctx.operands.at(0);
-  auto* layer = ngraph::as_type<ngraph::opset3::ROIAlign>(ctx.layer);
-  auto boxes = cast_constant_operand<int32_t>(1, layer);
-  auto batch_indices = cast_constant_operand<int32_t>(2, layer);
+void registerROIAlign() {
+  registerOp("roialign", [](const Context& ctx) {
+    IE_ASSERT(ctx.operands.size() == 3);
+    auto X = ctx.operands.at(0);
+    auto* layer = ngraph::as_type<ngraph::opset3::ROIAlign>(ctx.layer);
+    auto boxes = cast_constant_operand<int32_t>(1, layer);
+    auto batch_indices = cast_constant_operand<int32_t>(2, layer);
 
-  auto pooled_h = layer->get_pooled_h();
-  auto pooled_w = layer->get_pooled_w();
-  auto sampling_ratio = layer->get_sampling_ratio();
-  auto spatial_scale = layer->get_spatial_scale();
-  auto mode = layer->get_mode();
-  auto num_rois = static_cast<int32_t>(batch_indices.size());
+    auto pooled_h = layer->get_pooled_h();
+    auto pooled_w = layer->get_pooled_w();
+    auto sampling_ratio = layer->get_sampling_ratio();
+    auto spatial_scale = layer->get_spatial_scale();
+    auto mode = layer->get_mode();
+    auto num_rois = static_cast<int32_t>(batch_indices.size());
 
-  auto input_size = X.compute_shape().sizes();
+    auto input_size = X.compute_shape().sizes();
 
-  op::PoolMode pool_mode;
-  switch (mode) {
-    case ngraph::op::v3::ROIAlign::PoolingMode::AVG:
-      pool_mode = op::PoolMode::AVG;
-      break;
-    case ngraph::op::v3::ROIAlign::PoolingMode::MAX:
-      pool_mode = op::PoolMode::MAX;
-      break;
-    default:
-      std::runtime_error("Unsupported Pooling Mode");
-  }
+    op::PoolMode pool_mode;
+    switch (mode) {
+      case ngraph::op::v3::ROIAlign::PoolingMode::AVG:
+        pool_mode = op::PoolMode::AVG;
+        break;
+      case ngraph::op::v3::ROIAlign::PoolingMode::MAX:
+        pool_mode = op::PoolMode::MAX;
+        break;
+      default:
+        std::runtime_error("Unsupported Pooling Mode");
+    }
 
-  // Crop and Resize
-  edsl::Tensor resized_T = edsl::Placeholder(X.dtype(), {num_rois, input_size[1], pooled_h, pooled_w});
-  for (int i = 0; i < num_rois; i++) {
-    auto roi_width = std::max(spatial_scale * (boxes[4 * i + 2] - boxes[4 * i]), 1.0f);
-    auto roi_height = std::max(spatial_scale * (boxes[4 * i + 3] - boxes[4 * i + 1]), 1.0f);
-    auto x_1 = spatial_scale * boxes[4 * i];
-    auto y_1 = spatial_scale * boxes[4 * i + 1];
-    auto x_2 = spatial_scale * boxes[4 * i + 2];
-    auto y_2 = spatial_scale * boxes[4 * i + 3];
-  }
+    // Crop and Resize
+    edsl::Tensor resized_T = edsl::Placeholder(X.dtype(), {num_rois, input_size[1], pooled_h, pooled_w});
+    for (int i = 0; i < num_rois; i++) {
+      auto roi_width = std::max(spatial_scale * (boxes[4 * i + 2] - boxes[4 * i]), 1.0f);
+      auto roi_height = std::max(spatial_scale * (boxes[4 * i + 3] - boxes[4 * i + 1]), 1.0f);
+      auto x_1 = spatial_scale * boxes[4 * i];
+      auto y_1 = spatial_scale * boxes[4 * i + 1];
+      auto x_2 = spatial_scale * boxes[4 * i + 2];
+      auto y_2 = spatial_scale * boxes[4 * i + 3];
+
+      int sampling_h, sampling_w;
+      if (sampling_ratio == 0) {
+        sampling_h = std::ceil(roi_height / pooled_h);
+        sampling_w = std::ceil(roi_width / pooled_w);
+      } else {
+        sampling_h = sampling_w = sampling_ratio;
+      }
+    }
+
+    auto result = op::pool(resized_T, pool_mode, );
+
+    return edsl::make_tuple();
+  });
 }
 
-  auto result = op::pool(resized_T, pool_mode, )
-
-  return edsl::make_tuple();
-}
+}  // namespace PlaidMLPlugin
