925852ba0653f3659e7f963ff77faf7142497246
XingHongChenIntel <xinghong.chen@intel.com>
2020-11-26 19:17:22 +0800

    add more test case.

--- plaidml/bridge/openvino/ops/reverse_sequence.cpp
+++ plaidml/bridge/openvino/ops/reverse_sequence.cpp
@@ -15,27 +15,27 @@ namespace {
 
 template <typename T>
 std::vector<T> cast_constant_operand(size_t operand_idx, ngraph::Node* layer) {
-    auto* ngraph_const = ngraph::as_type<ngraph::op::Constant>(layer->get_input_node_ptr(operand_idx));
-    if (ngraph_const) {
-        return ngraph_const->cast_vector<T>();
-    } else {
-        THROW_IE_EXCEPTION << " input [1] is Unsupported inputType; ";
-    }
+  auto* ngraph_const = ngraph::as_type<ngraph::op::Constant>(layer->get_input_node_ptr(operand_idx));
+  if (ngraph_const) {
+    return ngraph_const->cast_vector<T>();
+  } else {
+    THROW_IE_EXCEPTION << " input [1] is Unsupported inputType; ";
+  }
 }
 
 edsl::Tensor reverse_tensor(edsl::Tensor reverse_crop, int64_t seq_axis) {
-    std::vector<edsl::TensorDim> dims(reverse_crop.rank());
-    reverse_crop.bind_dims(dims);
-    std::vector<edsl::TensorIndex> I_idxs(reverse_crop.rank());
-    std::vector<edsl::TensorIndex> O_idxs;
-    for (int64_t axis = 0; axis < reverse_crop.rank(); axis++) {
-        if (axis == seq_axis) {
-            O_idxs.push_back(dims[axis] - 1 - I_idxs[axis]);
-        } else {
-            O_idxs.push_back(I_idxs[axis]);
-        }
+  std::vector<edsl::TensorDim> dims(reverse_crop.rank());
+  reverse_crop.bind_dims(dims);
+  std::vector<edsl::TensorIndex> I_idxs(reverse_crop.rank());
+  std::vector<edsl::TensorIndex> O_idxs;
+  for (int64_t axis = 0; axis < reverse_crop.rank(); axis++) {
+    if (axis == seq_axis) {
+      O_idxs.push_back(dims[axis] - 1 - I_idxs[axis]);
+    } else {
+      O_idxs.push_back(I_idxs[axis]);
     }
-    return edsl::Contraction().outShape(dims).outAccess(O_idxs).assign(reverse_crop(I_idxs));
+  }
+  return edsl::Contraction().outShape(dims).outAccess(O_idxs).assign(reverse_crop(I_idxs));
 }
 
 }  // namespace
@@ -47,16 +47,11 @@ void registerReverseSequence() {
     auto* layer = ngraph::as_type<ReverseSequence>(ctx.layer);
     IE_ASSERT(ctx.operands.size() == 2);
     auto I = ctx.operands.at(0);
+    IE_ASSERT(I.rank() > 2);
 
     auto batch_axis = layer->get_origin_batch_axis();
     auto seq_axis = layer->get_origin_sequence_axis();
-
     auto length = cast_constant_operand<int64_t>(1, layer);
-    for (auto len : length) {
-      if (len < 1) {
-        THROW_IE_EXCEPTION << " input seq len at least be one ";
-      }
-    }
 
     auto shapes = I.compute_shape().sizes();
     std::vector<edsl::Tensor> slice_pools;
@@ -69,6 +64,9 @@ void registerReverseSequence() {
           reverse_piece.add_dim(i);
           constant_piece.add_dim(i);
         } else if (j == seq_axis) {
+          if (length[i] < 1 || length[i] >= shapes[seq_axis]) {
+            THROW_IE_EXCEPTION << " unreasonable sequence length; ";
+          }
           reverse_piece.add_dim(0, length[i], 1);
           constant_piece.add_dim(length[i], shapes[seq_axis], 1);
         } else {
@@ -78,25 +76,8 @@ void registerReverseSequence() {
       }
 
       // after slice, one-dim will be cast, then unsqueeze them for concatenate
-      edsl::Tensor reverse, constant;
-      if (length[i] == 1) {
-        if (batch_axis < seq_axis) {
-          reverse = op::unsqueeze(op::unsqueeze(reverse_piece, {batch_axis}), {seq_axis});
-        } else {
-          reverse = op::unsqueeze(op::unsqueeze(reverse_piece, {seq_axis}), {batch_axis});
-        }
-      } else {
-        reverse = op::unsqueeze(reverse_piece, {batch_axis});
-      }
-      if (length[i] == shapes[seq_axis] - 1) {
-        if (batch_axis < seq_axis) {
-          constant = op::unsqueeze(op::unsqueeze(constant_piece, {batch_axis}), {seq_axis});
-        } else {
-          constant = op::unsqueeze(op::unsqueeze(constant_piece, {seq_axis}), {batch_axis});
-        }
-      } else {
-        constant = op::unsqueeze(constant_piece, {batch_axis});
-      }
+      edsl::Tensor reverse = op::unsqueeze(reverse_piece, {batch_axis});
+      edsl::Tensor constant = op::unsqueeze(constant_piece, {batch_axis});
 
       // reverse and concatenate.
       auto reverse_crop = reverse_tensor(reverse, seq_axis);
--- plaidml/bridge/openvino/tests/functional/shared_tests_instances/single_layer_tests/reverse_sequence.cpp
+++ plaidml/bridge/openvino/tests/functional/shared_tests_instances/single_layer_tests/reverse_sequence.cpp
@@ -15,23 +15,29 @@ const std::vector<InferenceEngine::Precision> netPrecisions = {
     InferenceEngine::Precision::FP32,
 };
 
-const std::vector<std::vector<size_t>> kernels = {{3, 3}, {5, 5}};
-const std::vector<std::vector<size_t>> strides = {{5, 5}, {1, 1}};
-const std::vector<std::vector<size_t>> rates = {{1, 1}, {2, 2}};
-const std::vector<ngraph::op::PadType> padTypes = {
-    ngraph::op::PadType::VALID,       //
-    ngraph::op::PadType::SAME_UPPER,  //
-    ngraph::op::PadType::SAME_LOWER,  //
-};
-
 INSTANTIATE_TEST_CASE_P(ReverseSequenceLayerTest_smoke, ReverseSequenceLayerTest,
                         ::testing::Combine(::testing::Values(0),                                   //
                                            ::testing::Values(1),                                   //
                                            ::testing::Values(std::vector<size_t>{2, 10, 10, 10}),  //
-                                           ::testing::Values(std::vector<size_t>{4, 5}),           //
+                                           ::testing::Values(std::vector<size_t>{2, 1}),           //
                                            ::testing::Values(0),                                   //
                                            ::testing::ValuesIn(netPrecisions),                     //
                                            ::testing::Values(CommonTestUtils::DEVICE_PLAIDML)),    //
+                        ReverseSequenceLayerTest::getTestCaseName);
+
+const std::vector<std::vector<size_t>> seq_length = {
+    {1, 2, 3, 1},  //
+    {2, 2, 2, 2}   //
+};
+
+INSTANTIATE_TEST_CASE_P(ReverseSequenceLayer_test, ReverseSequenceLayerTest,
+                        ::testing::Combine(::testing::ValuesIn(std::vector<int64_t>{0, 1}),      //
+                                           ::testing::ValuesIn(std::vector<int64_t>{2, 3}),      //
+                                           ::testing::Values(std::vector<size_t>{4, 4, 4, 4}),   //
+                                           ::testing::ValuesIn(seq_length),                      //
+                                           ::testing::Values(0),                                 //
+                                           ::testing::ValuesIn(netPrecisions),                   //
+                                           ::testing::Values(CommonTestUtils::DEVICE_PLAIDML)),  //
                         ReverseSequenceLayerTest::getTestCaseName);
 
 }  // namespace
