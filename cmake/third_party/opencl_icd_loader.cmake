2b4abb9b0b410fd5f57de6996459a3e53cc7fe18
Artur Bialas <artur.bialas@intel.com>
2020-12-02 12:00:11 +0100

    fixes to Windows cmake build

--- cmake/third_party/opencl_icd_loader.cmake
+++ cmake/third_party/opencl_icd_loader.cmake
@@ -4,8 +4,3 @@ FetchContent_Declare(
   URL_HASH SHA256=e4c27a5adcef4dbc0fee98864af203dc78dfc967ca7287c9bad9add030e7516e
 )
 FetchContent_MakeAvailable(opencl_icd_loader)
-
-file(COPY ${opencl_headers_SOURCE_DIR}/CL DESTINATION ${opencl_icd_loader_SOURCE_DIR}/inc)
-add_library(opencl_icd_loader STATIC ${opencl_icd_loader_SOURCE_DIR})
-target_link_libraries(opencl_icd_loader OpenCL.lib)
-set_target_properties(opencl_icd_loader PROPERTIES LINKER_LANGUAGE CXX)
