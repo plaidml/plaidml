e100159f63193a8132875cac26629089707a56fb
Jeremy Bruestle <jeremy.bruestle@intel.com>
2020-12-02 14:31:22 -0800

    Generalized hoisting pass (#1584)
    Co-authored-by: Rob Earhart <robert.earhart@intel.com>
    
    Co-authored-by: Rob Earhart <robert.earhart@intel.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: Frank Laub <frank.laub@intel.com>

---
+++ pmlc/util/loop_with_epilog.h
@@ -0,0 +1,11 @@
+// Copyright 2020 Intel Corporation
+
+#pragma once
+
+#include "mlir/IR/OpDefinition.h"
+
+using mlir::ArrayRef;
+using mlir::LogicalResult;
+using mlir::Operation;
+
+#include "pmlc/util/loop_with_epilog.h.inc"
