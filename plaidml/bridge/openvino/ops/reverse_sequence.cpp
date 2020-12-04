c6f6cbf0f250fc1f9b29ff47c286082a7a4698a0
XingHongChenIntel <xinghong.chen@intel.com>
2020-12-04 17:50:50 +0800

    replaced slice op by gather op. and clean code.

--- plaidml/bridge/openvino/ops/reverse_sequence.cpp
+++ plaidml/bridge/openvino/ops/reverse_sequence.cpp
@@ -27,14 +27,8 @@ edsl::Tensor reverse_tensor(edsl::Tensor reverse_crop, int64_t seq_axis) {
   std::vector<edsl::TensorDim> dims(reverse_crop.rank());
   reverse_crop.bind_dims(dims);
   std::vector<edsl::TensorIndex> I_idxs(reverse_crop.rank());
-  std::vector<edsl::TensorIndex> O_idxs;
-  for (int64_t axis = 0; axis < reverse_crop.rank(); axis++) {
-    if (axis == seq_axis) {
-      O_idxs.push_back(dims[axis] - 1 - I_idxs[axis]);
-    } else {
-      O_idxs.push_back(I_idxs[axis]);
-    }
-  }
+  std::vector<edsl::TensorIndex> O_idxs = I_idxs;
+  O_idxs[seq_axis] = dims[seq_axis] - 1 - I_idxs[seq_axis];
   return edsl::Contraction().outShape(dims).outAccess(O_idxs).assign(reverse_crop(I_idxs));
 }
 
@@ -47,7 +41,6 @@ void registerReverseSequence() {
     auto* layer = ngraph::as_type<ReverseSequence>(ctx.layer);
     IE_ASSERT(ctx.operands.size() == 2);
     auto I = ctx.operands.at(0);
-    IE_ASSERT(I.rank() > 2);
 
     auto batch_axis = layer->get_origin_batch_axis();
     auto seq_axis = layer->get_origin_sequence_axis();
@@ -56,32 +49,15 @@ void registerReverseSequence() {
     auto shapes = I.compute_shape().sizes();
     std::vector<edsl::Tensor> slice_pools;
     for (int64_t i = 0; i < shapes[batch_axis]; i++) {
-      // split reversed tensor.
-      auto reverse_piece = op::slice(I);
-      auto constant_piece = op::slice(I);
-      for (int64_t j = 0; j < shapes.size(); j++) {
-        if (j == batch_axis) {
-          reverse_piece.add_dim(i);
-          constant_piece.add_dim(i);
-        } else if (j == seq_axis) {
-          if (length[i] < 1 || length[i] >= shapes[seq_axis]) {
-            THROW_IE_EXCEPTION << " unreasonable sequence length; ";
-          }
-          reverse_piece.add_dim(0, length[i], 1);
-          constant_piece.add_dim(length[i], shapes[seq_axis], 1);
-        } else {
-          reverse_piece.add_dim(0, shapes[j], 1);
-          constant_piece.add_dim(0, shapes[j], 1);
-        }
-      }
-
-      // after slice, one-dim will be cast, then unsqueeze them for concatenate
-      edsl::Tensor reverse = op::unsqueeze(reverse_piece, {batch_axis});
-      edsl::Tensor constant = op::unsqueeze(constant_piece, {batch_axis});
-
+      auto batch_indices = edsl::index({edsl::TensorDim(1)}, 0) + i;
+      auto I_slice = edsl::gather(I, batch_indices).axis(batch_axis);
+      auto indices_reverse = edsl::index({edsl::TensorDim(length[i])}, 0);
+      auto I_reverse = edsl::gather(I_slice, indices_reverse).axis(seq_axis);
+      auto indices_constant = edsl::index({edsl::TensorDim(shapes[seq_axis] - length[i])}, 0) + length[i];
+      auto I_constant = edsl::gather(I_slice, indices_constant).axis(seq_axis);
       // reverse and concatenate.
-      auto reverse_crop = reverse_tensor(reverse, seq_axis);
-      slice_pools.push_back(op::concatenate({reverse_crop, constant}, seq_axis));
+      auto reverse_crop = reverse_tensor(I_reverse, seq_axis);
+      slice_pools.push_back(op::concatenate({reverse_crop, I_constant}, seq_axis));
     }
 
     auto O = op::concatenate(slice_pools, batch_axis);
