214658fba608fff3bcfe06bf0a6812320de7ffdf
Frank Laub <frank.laub@intel.com>
2020-12-09 14:25:26 -0800

    Add support for conditional runtimes (#1608)
    * Add support for conditional runtimes
    
    * Add missing defs
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- pmlc/rt/all_runtimes.cc
+++ pmlc/rt/all_runtimes.cc
@@ -1,18 +1,27 @@
 // Copyright 2020 Intel Corporation
 
 #include "pmlc/rt/llvm/register.h"
-#include "pmlc/rt/opencl/register.h"
 #include "pmlc/rt/register.h"
 #include "pmlc/rt/runtime_registry.h"
+
+#ifdef PML_ENABLE_OPENCL
+#include "pmlc/rt/opencl/register.h"
+#endif
+#ifdef PML_ENABLE_VULKAN
 #include "pmlc/rt/vulkan/register.h"
+#endif
 
 namespace pmlc::rt {
 
 void registerRuntimes() {
   registerRuntime();
   llvm::registerRuntime();
+#ifdef PML_ENABLE_OPENCL
   opencl::registerRuntime();
+#endif
+#ifdef PML_ENABLE_VULKAN
   vulkan::registerRuntime();
+#endif
 }
 
 } // namespace pmlc::rt
