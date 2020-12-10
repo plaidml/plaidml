ef12e6936b0686589ca13813ae15a287fa6f072e
Tim Zerrell <tim.zerrell@intel.com>
2020-12-09 16:23:35 -0800

    Add ShapeOf with skipped test (#1571)
    * Add ShapeOf with skipped test
    
    * Minor changes to ShapeOf
    
    Co-authored-by: nchoudhu <namrata.choudhury@intel.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ plaidml/bridge/openvino/tests/functional/shared_tests_instances/single_layer_tests/shape_of.cpp
@@ -0,0 +1,22 @@
+// Copyright (C) 2019 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <vector>
+
+#include "common_test_utils/test_constants.hpp"
+#include "single_layer_tests/shape_of.hpp"
+
+using namespace LayerTestsDefinitions;
+
+namespace {
+const std::vector<InferenceEngine::Precision> netPrecisions = {
+    InferenceEngine::Precision::I32,  //
+};
+
+INSTANTIATE_TEST_CASE_P(smoke, ShapeOfLayerTest,
+                        ::testing::Combine(::testing::ValuesIn(netPrecisions),
+                                           ::testing::Values(std::vector<size_t>({10, 10, 10})),
+                                           ::testing::Values(CommonTestUtils::DEVICE_PLAIDML)),
+                        ShapeOfLayerTest::getTestCaseName);
+}  // namespace
