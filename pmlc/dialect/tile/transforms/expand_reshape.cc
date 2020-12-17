e3efc925d3f9af3d72dff372827b6c5312b87f16
Yao Shi <yao.shi@intel.com>
2020-12-17 15:24:05 -0800

    Fix and clean up

--- pmlc/dialect/tile/transforms/expand_reshape.cc
+++ pmlc/dialect/tile/transforms/expand_reshape.cc
@@ -19,20 +19,74 @@ namespace {
 struct ExpandReshapePass : public ExpandReshapeBase<ExpandReshapePass> {
   void runOnFunction() final;
 
+  // Compute the number of indices needed
+  int computeNumIdxs(ArrayRef<int64_t> srcShape, ArrayRef<int64_t> dstShape);
+  // Expand in-order reshape operation
   void expandReshape(ReshapeOp reshapeOp);
+  // Expand out-of-order reshape operation
   void expandOutOfOrderReshape(ReshapeOp reshapeOp);
 };
 
+// Compute the number of indices needed
+int ExpandReshapePass::computeNumIdxs(ArrayRef<int64_t> srcShape, ArrayRef<int64_t> dstShape) {
+  SmallVector<int64_t, 4> srcDims(srcShape.begin(), srcShape.end());
+  SmallVector<int64_t, 4> dstDims(dstShape.begin(), dstShape.end());
+  int pSrc = srcDims.size() - 1;
+  int pDst = dstDims.size() - 1;
+  int numIdxs = 0;
+  while (pSrc >= 0 && pDst >=0) {
+    while (pSrc >= 0 && srcDims[pSrc] == 1) {
+      --pSrc;
+    }
+    if (pSrc < 0) {
+      break;
+    }
+    while (pDst >= 0 && dstDims[pDst] == 1) {
+      --pDst;
+    }
+    if (pDst < 0) {
+      break;
+    }
+    int64_t gcd = std::gcd(srcDims[pSrc], dstDims[pDst]);
+    if (gcd == 1) {
+      return -1;
+    }
+    ++numIdxs;
+    srcDims[pSrc] /= gcd;
+    dstDims[pDst] /= gcd;
+  }
+  while (pSrc >= 0) {
+    if (srcDims[pSrc] > 1) {
+      ++numIdxs;
+    }
+    --pSrc;
+  }
+  while (pDst >= 0) {
+    if (dstDims[pDst] > 1) {
+      ++numIdxs;
+    }
+    --pDst;
+  }
+  return numIdxs;
+}
+
+// Expand in-order reshape operation
 void ExpandReshapePass::expandReshape(ReshapeOp reshapeOp) {
   auto src = reshapeOp.tensor();
   auto srcShape = src.getType().cast<RankedTensorType>().getShape();
-  SmallVector<int64_t, 4> srcDims(srcShape.begin(), srcShape.end());
   auto dst = reshapeOp.getResult();
   auto dstType = dst.getType().cast<RankedTensorType>();
   auto dstShape = dstType.getShape();
-  SmallVector<int64_t, 4> dstDims(dstShape.begin(), dstShape.end());
+
+  int numIdxs = computeNumIdxs(srcShape, dstShape);
+  if (numIdxs < 0) {
+    // The indices of src and dst are out-of-order
+    expandOutOfOrderReshape(reshapeOp);
+  }
 
   auto context = reshapeOp.getContext();
+  SmallVector<int64_t, 4> srcDims(srcShape.begin(), srcShape.end());
+  SmallVector<int64_t, 4> dstDims(dstShape.begin(), dstShape.end());
   SmallVector<int64_t, 4> idxs;
   auto zeroExpr = getAffineConstantExpr(0, context);
   SmallVector<AffineExpr, 4> srcExprs(srcDims.size(), zeroExpr);
@@ -58,12 +112,9 @@ void ExpandReshapePass::expandReshape(ReshapeOp reshapeOp) {
       break;
     }
     int64_t gcd = std::gcd(srcDims[pSrc], dstDims[pDst]);
-    if (gcd == 1) {
-      // The indices of src and dst are out-of-order
-      expandOutOfOrderReshape(reshapeOp);
-      return;
-    }
-    unsigned idxId = idxs.size();
+    assert(gcd > 1);
+    int idxId = numIdxs - idxs.size() - 1;
+    assert(idxId >= 0);
     idxs.emplace_back(gcd);
     srcExprs[pSrc] = srcExprs[pSrc] + getAffineConstantExpr(mulSrc, context) * getAffineDimExpr(idxId, context);
     dstExprs[pDst] = dstExprs[pDst] + getAffineConstantExpr(mulDst, context) * getAffineDimExpr(idxId, context);
@@ -75,7 +126,8 @@ void ExpandReshapePass::expandReshape(ReshapeOp reshapeOp) {
 
   while (pSrc >= 0) {
     if (srcDims[pSrc] > 1) {
-      unsigned idxId = idxs.size();
+      unsigned idxId = numIdxs - idxs.size() - 1;
+      assert(idxId >= 0);
       idxs.emplace_back(srcDims[pSrc]);
       srcExprs[pSrc] = srcExprs[pSrc] + getAffineConstantExpr(mulSrc, context) * getAffineDimExpr(idxId, context);
     }
@@ -84,7 +136,8 @@ void ExpandReshapePass::expandReshape(ReshapeOp reshapeOp) {
   }
   while (pDst >= 0) {
     if (dstDims[pDst] > 1) {
-      unsigned idxId = idxs.size();
+      unsigned idxId = numIdxs - idxs.size() - 1;
+      assert(idxId >= 0);
       idxs.emplace_back(dstDims[pDst]);
       dstExprs[pDst] = dstExprs[pDst] + getAffineConstantExpr(mulDst, context) * getAffineDimExpr(idxId, context);
     }
@@ -92,25 +145,10 @@ void ExpandReshapePass::expandReshape(ReshapeOp reshapeOp) {
     mulDst = 1;
   }
 
-  // Reverse the dim indices
-  DenseMap<AffineExpr, AffineExpr> map;
-  unsigned numIdxs = idxs.size();
-  for (unsigned i = 0; i < numIdxs / 2; ++i) {
-    auto expr0 = getAffineDimExpr(i, context);
-    auto expr1 = getAffineDimExpr(numIdxs - i - 1, context);
-    map[expr0] = expr1;
-    map[expr1] = expr0;
-  }
-  for (unsigned i = 0; i < srcExprs.size(); ++i) {
-    srcExprs[i] = srcExprs[i].replace(map);
-  }
-  for (unsigned i = 0; i < dstExprs.size(); ++i) {
-    dstExprs[i] = dstExprs[i].replace(map);
-  }
-
   AffineMap srcMap = AffineMap::get(idxs.size(), 0, srcExprs, context);
   AffineMap sinkMap = AffineMap::get(idxs.size(), 0, dstExprs, context);
 
+  // Replace the original reshape with constraction
   OpBuilder builder(reshapeOp);
   auto elementType = dst.getType().cast<RankedTensorType>().getElementType();
   auto ident = tile::createIdentity(builder, reshapeOp.getLoc(), elementType, AggregationKind::assign);
@@ -128,8 +166,8 @@ void ExpandReshapePass::expandReshape(ReshapeOp reshapeOp) {
   reshapeOp.erase();
 }
 
+// Expand out-of-order reshape operation
 void ExpandReshapePass::expandOutOfOrderReshape(ReshapeOp reshapeOp) {
-  // TODO: expand reshape with out-of-order indices
   return;
 }
 
