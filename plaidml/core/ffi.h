96d25abc452c04f880e342b4fb18d04477df1500
John Bartlett <jd.bartlett96@gmail.com>
2020-12-17 10:21:37 -0800

    Resolving python build issues

--- plaidml/core/ffi.h
+++ plaidml/core/ffi.h
@@ -14,22 +14,6 @@
 extern "C" {
 #endif  // __cplusplus
 
-#if __cplusplus >= 202002L
-#include <source_location>
-using edsl_source_location = std::source_location;
-#elif __cplusplus >= 201703L
-#include <experimental/source_location>
-using edsl_source_location = std::experimental::source_location;
-#else
-struct edsl_source_location {
-  static edsl_source_location current() { return edsl_source_location{}; }
-  constexpr uint_least32_t line() const noexcept { return 0; }
-  constexpr uint_least32_t column() const noexcept { return 0; }
-  constexpr const char* file_name() const noexcept { return nullptr; }
-  constexpr const char* function_name() const noexcept { return nullptr; }
-};
-#endif
-
 //
 // Core API
 //
