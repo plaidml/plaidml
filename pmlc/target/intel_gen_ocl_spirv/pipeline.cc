7170e948047286e35416d73e8b076049fe799fdd
Adam Straw <26910851+adstraw@users.noreply.github.com>
2020-12-17 17:14:41 -0800

    add Affinex loop unroll pass (#1628)
    * add Affinex loop unroll pass
    
    * add operation limit
    
    * add loopsToUnroll
    
    * hook up to opencl and vulkan pipelines
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- pmlc/target/intel_gen_ocl_spirv/pipeline.cc
+++ pmlc/target/intel_gen_ocl_spirv/pipeline.cc
@@ -135,9 +135,8 @@ void pipelineBuilder(OpPassManager &pm,
   pm.addPass(createCSEPass());
 
   // Unroll affine.for loops.
-  pm.addPass(createLoopUnrollPass(
-      /*unrollFactor=*/256,
-      /*unrollUpToFactor=*/true));
+  pm.addPass(pmlc::dialect::affinex::createAffinexLoopUnroll(
+      /*operationLimit =*/2048));
   pm.addPass(createCanonicalizerPass());
   pm.addPass(createCSEPass());
 
