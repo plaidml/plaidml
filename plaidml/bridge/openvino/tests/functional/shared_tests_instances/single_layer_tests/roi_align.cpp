8cc441004d9de46099d7ace56787c978e03595d7
Ling, Liyang <liyang.ling@intel.com>
2020-12-23 16:00:13 +0800

    Update test case

--- plaidml/bridge/openvino/tests/functional/shared_tests_instances/single_layer_tests/roi_align.cpp
+++ plaidml/bridge/openvino/tests/functional/shared_tests_instances/single_layer_tests/roi_align.cpp
@@ -15,19 +15,21 @@ std::vector<InferenceEngine::Precision> netPrecisions = {
     InferenceEngine::Precision::FP32,
 };
 
-const std::vector<std::vector<size_t>> inputShapes = {{6, 3, 128, 128}, {32, 256, 200, 200}};
-const std::vector<std::vector<std::vector<float>>> rois = {{{0.1, 0.2, 0.44, 0.34}, {0.33, 0.41, 0.11, 0.32}}};
-const std::vector<std::vector<size_t>> batchIndices = {{2, 5}};
-const std::vector<size_t> numROIs = {2};
+const std::vector<std::vector<size_t>> inputShapes = {{6, 3, 128, 128}, {7, 256, 200, 200}};
+const std::vector<std::vector<float>> rois = {{{0.1, 0.2, 0.4, 0.3},  //
+                                               {0.3, 0.4, 0.2, 0.7},  //
+                                               {0.6, 0.5, 0.8, 0.8}}};
+const std::vector<size_t> batchIndices = {2, 3, 5};
+const std::vector<size_t> numROIs = {3};
 const std::vector<size_t> pooledHs = {6};
 const std::vector<size_t> pooledWs = {6};
 const std::vector<size_t> samplingRatios = {0};
-const std::vector<float> spatialScale = {20.0};
+const std::vector<float> spatialScale = {16.0, 20.0};
 const std::vector<std::string> modes = {"avg", "max"};
 
 const auto roiAlignParams = ::testing::Combine(::testing::ValuesIn(inputShapes),     //
-                                               ::testing::ValuesIn(rois),            //
-                                               ::testing::ValuesIn(batchIndices),    //
+                                               ::testing::Values(rois),              //
+                                               ::testing::Values(batchIndices),      //
                                                ::testing::ValuesIn(numROIs),         //
                                                ::testing::ValuesIn(pooledHs),        //
                                                ::testing::ValuesIn(pooledWs),        //
@@ -42,15 +44,15 @@ INSTANTIATE_TEST_CASE_P(ROIAlign, ROIAlignLayerTest,
                                            ::testing::Values(CommonTestUtils::DEVICE_PLAIDML)),  //
                         ROIAlignLayerTest::getTestCaseName);
 
-const auto smokeRoiAlignParams = ::testing::Combine(::testing::Values(inputShapes[0]),   //
-                                                    ::testing::Values(rois[0]),          //
-                                                    ::testing::Values(batchIndices[0]),  //
-                                                    ::testing::Values(2),                //
-                                                    ::testing::Values(3),                //
-                                                    ::testing::Values(3),                //
-                                                    ::testing::Values(0),                //
-                                                    ::testing::Values(20.0f),            //
-                                                    ::testing::Values("max")             //
+const auto smokeRoiAlignParams = ::testing::Combine(::testing::Values(inputShapes[0]),  //
+                                                    ::testing::Values(rois),            //
+                                                    ::testing::Values(batchIndices),    //
+                                                    ::testing::Values(2),               //
+                                                    ::testing::Values(3),               //
+                                                    ::testing::Values(3),               //
+                                                    ::testing::Values(0),               //
+                                                    ::testing::Values(20.0f),           //
+                                                    ::testing::Values("max")            //
 );
 
 INSTANTIATE_TEST_CASE_P(smoke, ROIAlignLayerTest,
