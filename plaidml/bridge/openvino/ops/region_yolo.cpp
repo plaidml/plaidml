56c8e6044360658daa400d269c5614da97eed23a
YangleiZouIntel <yanglei.zou@intel.com>
2020-12-23 18:05:26 +0800

    Checkpoint

--- plaidml/bridge/openvino/ops/region_yolo.cpp
+++ plaidml/bridge/openvino/ops/region_yolo.cpp
@@ -19,8 +19,38 @@ void registerRegionYolo() {
     auto* layer = ngraph::as_type<ngraph::opset4::RegionYolo>(ctx.layer);
     IE_ASSERT(ctx.operands.size() == 1);
     auto I = ctx.operands.at(0);
-    fprintf(stderr, "(DBG) reached this point\n");
-    return edsl::make_tuple(I + 1);
+
+    auto input_shape = I.compute_shape().sizes();
+    const int batches = input_shape[0];
+    const int channels = input_shape[1];
+    const int height = input_shape[2];
+    const int width = input_shape[3];
+
+    auto coords = layer->get_num_coords();
+    auto classes = layer->get_num_classes();
+    auto regions = layer->get_num_regions();
+    auto do_softmax = layer->get_do_softmax();
+    auto mask = layer->get_mask();
+    auto mask_size = mask.size();
+
+    edsl::Tensor O = I;
+
+    int num_regions = 0;
+    int end_index = 0;
+
+    if (do_softmax) {
+      // Region layer (Yolo v2)
+      num_regions = regions;
+      end_index = width * height;
+    } else {
+      // Yolo layer (Yolo v3)
+      num_regions = mask_size;
+      end_index = width * height * (classes + 1);
+    }
+
+    const int inputs_size = width * height * num_regions * (classes + coords + 1);
+
+    return edsl::make_tuple(O);
   });
 }
 
