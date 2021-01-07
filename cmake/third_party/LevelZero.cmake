8888bb572d142d4aad6914397e1eda69585de604
xin1.wang <xin1.wang@intel.com>
2021-01-07 11:52:26 -0500

    CMake fix. Change Level_zero_probe_test.
    Signed-off-by: xin1.wang <xin1.wang@intel.com>

--- cmake/third_party/LevelZero.cmake
+++ cmake/third_party/LevelZero.cmake
@@ -3,9 +3,4 @@ FetchContent_Declare(
   URL      https://github.com/oneapi-src/level-zero/archive/v1.0.22.zip
   URL_HASH SHA256=ad9a757c5e07cd44d8e63019e63ba8a8ea19981ea4b7567d3099d3ef80567908
 )
-FetchContent_GetProperties(LevelZero)
-if(NOT levelzero_POPULATED)
-    FetchContent_Populate(LevelZero)
-    set_property(GLOBAL PROPERTY LEVEL_ZERO_INCLUDE_DIRS ${levelzero_SOURCE_DIR}/include)
-    add_subdirectory(${levelzero_SOURCE_DIR})
-endif()
+FetchContent_MakeAvailable(LevelZero)
--- pmlc/rt/level_zero/level_zero_probe.cc
+++ pmlc/rt/level_zero/level_zero_probe.cc
@@ -5,43 +5,49 @@
 #ifdef PML_ENABLE_LEVEL_ZERO
 #include "pmlc/rt/level_zero/level_zero_utils.h"
 #endif
-#include "pmlc/util/logging.h"
 
 int main(int argc, char **argv) {
 #ifdef PML_ENABLE_LEVEL_ZERO
   ze_result_t result = ZE_RESULT_NOT_READY;
   try {
     result = zeInit(0);
+    if (result != ZE_RESULT_SUCCESS) {
+      std::cout << "Function zeInit failed with result: "
+                << pmlc::rt::level_zero::lzu::to_string(result) << std::endl;
+      return -1;
+    }
   } catch (std::exception &e) {
-    std::cout << "failed to init level zero driver, result:" << result
-              << " info:" << e.what() << std::endl;
-    return -1;
+    std::cout << "Function zeInit crashed with result: "
+              << pmlc::rt::level_zero::lzu::to_string(result)
+              << " info: " << e.what() << std::endl;
+    return -2;
   }
   std::vector<ze_driver_handle_t> drivers;
   try {
     pmlc::rt::level_zero::lzu::get_all_driver_handles();
   } catch (std::exception &e) {
-    std::cout << "inited level zero, but failed to get driver handles:"
-              << e.what() << std::endl;
-    return -2;
+    std::cout << "Get level zero driver handle crashed with info: " << e.what()
+              << std::endl;
+    return -3;
   }
   std::vector<std::pair<ze_driver_handle_t, ze_device_handle_t>>
       supportedDevices = pmlc::rt::level_zero::lzu::getSupportedDevices();
   if (supportedDevices.empty()) {
     std::cout << "No supported level zero devices available" << std::endl;
-    return -3;
+    return -4;
   }
 
-  std::cout << "Available level zero devices: " << supportedDevices.size()
+  std::cout << "Available level zero devices count: " << supportedDevices.size()
             << std::endl;
   for (auto &target : supportedDevices) {
-    IVLOG(1, "  " << pmlc::rt::level_zero::lzu::get_device_properties(
-                         target.second)
-                         .name);
+    std::cout
+        << "Device: "
+        << pmlc::rt::level_zero::lzu::get_device_properties(target.second).name
+        << std::endl;
   }
   return 0;
 #else
   std::cout << "LevelZero is not opened on this platform" << std::endl;
-  return 0;
+  return -1;
 #endif
 }
