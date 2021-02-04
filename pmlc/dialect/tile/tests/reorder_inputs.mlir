0a7d07566a305d77b2f2a96ff0bc7039d75df485
Artur Bialas <artur.bialas@intel.com>
2021-02-04 16:57:48 +0100

    reorder inputs pass

---
+++ pmlc/dialect/tile/tests/reorder_inputs.mlir
@@ -0,0 +1,16 @@
+// RUN: pmlc-opt -tile-reorder-inputs %s | FileCheck %s
+
+// CHECK-LABEL: @main
+// CHECK:   %[[reorder0:.*]] = tile.contract assign, none, %{{.*}}, %{{.*}} {lowerBounds = #map0, sink = #map1, srcs = [#map1], tags = {layout = 1 : i64}
+// CHECK:   %[[reorder1:.*]] = tile.contract assign, none, %{{.*}}, %{{.*}} {lowerBounds = #map0, sink = #map1, srcs = [#map1], tags = {layout = 2 : i64}
+// CHECK:   layer.box "ng.Convolution" (%{{.*}}, %{{.*}}) = (%[[reorder0]], %[[reorder1]])
+// CHECK:   tile.contract add, mul, %{{.*}}, %{{.*}}, %{{.*}} {sink = #map4, srcs = [#map5, #map6], tags = {layout = 1 : i64}}
+// CHECK:   layer.return {tags = {layout = 1 : i64}}
+func @main(%arg0: tensor<1x3x224x224xf32>, %arg1: tensor<64x3x7x7xf32> {tile.const = 0 : index}) -> tensor<1x64x112x112xf32> {
+  %0 = layer.box "ng.Convolution" (%arg111, %arg112) = (%arg0, %arg1) : (tensor<1x3x224x224xf32>, tensor<64x3x7x7xf32>) -> tensor<1x64x112x112xf32> {
+    %cst_109 = tile.constant(0.000000e+00 : f64) : tensor<f32>
+    %conv = tile.contract add, mul, %cst_109, %arg111, %arg112 {sink = affine_map<(d0, d1, d2, d3, d4, d5, d6) -> (d0, d1, d2, d3)>, srcs = [affine_map<(d0, d1, d2, d3, d4, d5, d6) -> (d0, d4, d2 * 2 + d5 - 3, d3 * 2 + d6 - 3)>, affine_map<(d0, d1, d2, d3, d4, d5, d6) -> (d1, d4, d5, d6)>] } : tensor<f32>, tensor<1x3x224x224xf32>, tensor<64x3x7x7xf32> -> tensor<1x64x112x112xf32>
+    layer.return %conv : tensor<1x64x112x112xf32>
+  } {attrs = {dilations = [1, 1], pads_begin = [3, 3], pads_end = [3, 3], strides = [2, 2]}}
+  return %0 : tensor<1x64x112x112xf32>
+}
---
+++ pmlc/dialect/tile/transforms/reorder_inputs.cc
@@ -0,0 +1,128 @@
+// Copyright 2020, Intel Corporation
+
+#include "mlir/IR/Builders.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Support/DebugStringHelper.h"
+#include "llvm/ADT/SetVector.h"
+
+#include "pmlc/dialect/layer/ir/ops.h"
+#include "pmlc/dialect/tile/ir/ops.h"
+#include "pmlc/dialect/tile/transforms/pass_detail.h"
+#include "pmlc/util/layout.h"
+#include "pmlc/util/logging.h"
+
+using namespace mlir; // NOLINT
+
+namespace pmlc::dialect::tile {
+
+namespace {
+
+int64_t getArgPos(Operation &op, BlockArgument &arg) {
+  auto argPos = 0;
+  for (auto i = 0; i < op.getOperands().size(); i++) {
+    if (op.getOperand(i) == arg) {
+      argPos = i;
+    }
+  }
+  return argPos;
+}
+
+void performDataReordering(OpBuilder &builder, Operation *op, BlockArgument arg,
+                           MLFramework framework, bool isConst) {
+  // Check if op is a BoxOp, if so scan which argument is used,
+  // and get its tensor type.
+  // Later on create the new contraciton op with the same sizes and additional
+  // layout tag. These ops would be moved to init as weights reorders in the
+  // later pass.
+  auto layerOp = dyn_cast<layer::BoxOp>(op);
+  if (!layerOp)
+    return;
+
+  auto argPos = getArgPos(*op, arg);
+  auto tensorType =
+      op->getOperand(argPos).getType().dyn_cast<mlir::RankedTensorType>();
+  if (!tensorType)
+    return;
+
+  Type elementType = tensorType.getElementType();
+  auto ident = tile::createIdentity(builder, op->getLoc(), elementType,
+                                    AggregationKind::assign);
+  auto outRank = tensorType.getRank();
+  SmallVector<AffineExpr, 4> dimExprs;
+  dimExprs.reserve(outRank);
+
+  for (unsigned i = 0; i < outRank; i++) {
+    dimExprs.push_back(mlir::getAffineDimExpr(i, op->getContext()));
+  }
+  auto idMap = AffineMap::get(outRank, 0, dimExprs, op->getContext());
+
+  std::stringstream ss;
+  ss << "reordered_" << arg.getArgNumber();
+
+  auto newOp = builder.create<ContractionOp>(
+      op->getLoc(), op->getOperand(argPos).getType(), ident,
+      ArrayRef<Value>{op->getOperand(argPos)}, AggregationKind::assign,
+      CombinationKind::none, idMap, ArrayRef<AffineMap>{idMap},
+      IntegerSet::getEmptySet(outRank, 0, op->getContext()), ss.str());
+  newOp.setLowerBounds(SmallVector<int64_t, 4>(outRank, 0));
+
+  newOp.setUpperBounds(tensorType.getShape());
+
+  // Switch all uses to the new contraction
+  op->getOperand(argPos).replaceAllUsesExcept(
+      newOp.getResult(), SmallPtrSet<Operation *, 1>{newOp.getOperation()});
+
+  // Add layout tag
+  setLayoutTag(newOp, getLayoutType(framework, layerOp.op(), isConst));
+}
+
+struct ReorderInputsPass : public ReorderInputsBase<ReorderInputsPass> {
+  void runOnFunction() final {
+    auto func = getFunction();
+
+    auto layoutSet = false;
+    auto framework = MLFramework::Default;
+
+    // Get Framework and check if at least one BoxOp is present
+    func.walk([&](layer::BoxOp op) {
+      framework = getMLFramework(op.op());
+      layoutSet = true;
+      return;
+    });
+
+    // If no BoxOp present then terminate
+    if (!layoutSet)
+      return;
+
+    // Set proper layout tags for the BoxOps
+    func.walk([&](layer::BoxOp op) {
+      for (auto &innerOp : op.getBody()->getOperations()) {
+        setLayoutTag(&innerOp, getLayoutType(framework, op.op()));
+      }
+    });
+
+    // Go over each function argument to set correct layouts
+    // in the inner tile ops if they are used
+    auto &block = func.getBody().front();
+    Operation *op = &block.front();
+    OpBuilder builder(op);
+
+    for (auto &arg : func.getArguments()) {
+      for (auto *op : arg.getUsers()) {
+        performDataReordering(builder, op, arg, framework,
+                              static_cast<bool>(func.getArgAttr(
+                                  arg.getArgNumber(), "tile.const")));
+      }
+    }
+
+    IVLOG(1, "Func: " << debugString(*func));
+  }
+};
+
+} // namespace
+
+std::unique_ptr<Pass> createReorderInputsPass() {
+  return std::make_unique<ReorderInputsPass>();
+}
+
+} // namespace pmlc::dialect::tile
