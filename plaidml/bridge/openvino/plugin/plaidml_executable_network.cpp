8107679424298c59faad6e56ca8ad25de995749d
Tim Zerrell <tim.zerrell@intel.com>
2021-03-05 18:44:47 -0800

    Handle opset3 broadcast & opset1 batchnorm

--- plaidml/bridge/openvino/plugin/plaidml_executable_network.cpp
+++ plaidml/bridge/openvino/plugin/plaidml_executable_network.cpp
@@ -14,6 +14,8 @@
 #include <ngraph/pass/constant_folding.hpp>
 #include <ngraph/pass/manager.hpp>
 #include <transformations/common_optimizations/common_optimizations.hpp>
+#include <transformations/op_conversions/batch_norm_decomposition.hpp>
+#include <transformations/op_conversions/convert_broadcast3.hpp>
 #include <transformations/op_conversions/convert_interpolate1_to_interpolate4.hpp>
 
 #include "plaidml_builder.hpp"
@@ -34,6 +36,8 @@ static plaidml::Program buildProgram(const ICNNNetwork& network) {
 
   std::shared_ptr<ngraph::Function> func = ngraph::clone_function(*network.getFunction());
   ngraph::pass::Manager manager;
+  manager.register_pass<ngraph::pass::BatchNormDecomposition>();
+  manager.register_pass<ngraph::pass::ConvertBroadcast3>();
   manager.register_pass<ngraph::pass::ConvertInterpolate1ToInterpolate4>();
   manager.run_passes(func);
   ngraph::pass::ConstantFolding().run_on_function(func);
