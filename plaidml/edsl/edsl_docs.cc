d74b4ecc4b2fcb40bf981e2f5e69dbdfc616d1f1
Sana Nazir <snazir@bliviero-MOBL1.amr.corp.intel.com>
2021-03-08 15:56:10 -0800

    minor fix

--- plaidml/edsl/edsl_docs.cc
+++ plaidml/edsl/edsl_docs.cc
@@ -46,8 +46,8 @@ Tensor MatMul(const Tensor& A, const Tensor& B) {
 Tensor GlobalMin(const Tensor& I) {
   TensorIndex i, j, k;
   auto Neg = -I;
-  Tensor O = -Contraction().max(Neg(i, j, k));
-  return O;
+  Tensor O_Neg = Contraction().max(Neg(i, j, k));
+  return -O_Neg;
 }
 // global_min_end
 
