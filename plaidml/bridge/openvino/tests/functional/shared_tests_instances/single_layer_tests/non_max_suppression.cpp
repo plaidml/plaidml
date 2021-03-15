f1b638e7aafe89946e1e186efe0530a3ad930efd
xin1.wang <xin1.wang@intel.com>
2021-03-15 16:10:46 -0400

    Change tests.

--- plaidml/bridge/openvino/tests/functional/shared_tests_instances/single_layer_tests/non_max_suppression.cpp
+++ plaidml/bridge/openvino/tests/functional/shared_tests_instances/single_layer_tests/non_max_suppression.cpp
@@ -12,17 +12,16 @@ using LayerTestsDefinitions::NmsLayerTest;
 
 namespace {
 // Each InputShapeParams consists of Number of batches, Number of boxes, Number of classes
-const std::vector<InputShapeParams> inShapeParams = {InputShapeParams{3, 100, 5}, InputShapeParams{1, 10, 50},
-                                                     InputShapeParams{2, 50, 50}};
+const std::vector<InputShapeParams> inShapeParams = {InputShapeParams{1, 5, 1}};
 
-const std::vector<int32_t> maxOutBoxPerClass = {5, 20};
-const std::vector<float> threshold = {0.3f, 0.7f};
-const std::vector<float> sigmaThreshold = {0.0f, 0.5f};
+const std::vector<int32_t> maxOutBoxPerClass = {5};
+const std::vector<float> threshold = {0.3f};
+const std::vector<float> sigmaThreshold = {0.0f};
 const std::vector<ngraph::op::v5::NonMaxSuppression::BoxEncodingType> encodType = {
     ngraph::op::v5::NonMaxSuppression::BoxEncodingType::CENTER,
     ngraph::op::v5::NonMaxSuppression::BoxEncodingType::CORNER};
-const std::vector<bool> sortResDesc = {true, false};
-const std::vector<ngraph::element::Type> outType = {ngraph::element::i32, ngraph::element::i64};
+const std::vector<bool> sortResDesc = {false};
+const std::vector<ngraph::element::Type> outType = {ngraph::element::i32};
 
 const auto nmsParams = ::testing::Combine(
     ::testing::ValuesIn(inShapeParams),
