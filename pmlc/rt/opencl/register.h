a31cb42a3bf58aec52ac9faa09ceb90625c51b8b
Frank Laub <frank.laub@intel.com>
2021-04-08 23:15:47 +0000

    Drop unmaintained conversions, dialects, passes, and targets

--- pmlc/rt/opencl/register.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2020 Intel Corporation
-
-#pragma once
-
-namespace pmlc::rt::opencl {
-
-void registerRuntime();
-
-} // namespace pmlc::rt::opencl
--- pmlc/target/intel_gen_ocl_spirv/pipeline.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace pmlc::target::intel_gen_ocl_spirv {
-
-void registerTarget();
-
-} // namespace pmlc::target::intel_gen_ocl_spirv
