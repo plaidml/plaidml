cbc98054df3ea238b636ae578b990058621675c4
John Bartlett <john.bartlett@intel.com>
2021-04-14 12:55:27 -0700

    infer result shape & output info

--- plaidml/bridge/openvino/ops/convolution.cpp
+++ plaidml/bridge/openvino/ops/convolution.cpp
@@ -9,6 +9,7 @@
 #include "ngraph/opsets/opset1.hpp"
 
 #include "plaidml/op/op.h"
+#include <iostream>
 
 using namespace plaidml;          // NOLINT[build/namespaces]
 using namespace InferenceEngine;  // NOLINT[build/namespaces]
@@ -17,6 +18,7 @@ namespace PlaidMLPlugin {
 
 void registerConvolution() {
   registerOp("Convolution", [](const Context& ctx) {
+    std::cout << "Making OV convolution" << std::endl;
     auto* layer = ngraph::as_type<ngraph::opset1::Convolution>(ctx.layer);
     IE_ASSERT(ctx.operands.size() == 2);
     auto I = ctx.operands.at(0);
