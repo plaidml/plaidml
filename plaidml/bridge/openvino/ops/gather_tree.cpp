1cf8022c33cf94a47887e5535c9c798e01757723
xin1.wang <xin1.wang@intel.com>
2021-05-10 11:27:55 -0400

    Use gather instead of slice.

--- plaidml/bridge/openvino/ops/gather_tree.cpp
+++ plaidml/bridge/openvino/ops/gather_tree.cpp
@@ -2,8 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include <iostream>
-
 #include "ngraph/opsets/opset.hpp"
 #include "ngraph/opsets/opset4.hpp"
 #include "plaidml/op/op.h"
@@ -44,7 +42,9 @@ Tensor GatherTree(Tensor STEP_IDS, Tensor PARENT_IDX, Tensor MAX_SEQ_LEN, Tensor
   Tensor MAX_TIME = edsl::cast(Tensor(max_time), MAX_SEQ_LEN.dtype());
   Tensor MAX_SEQ_LEN_IN_BEAM = op::minimum(MAX_SEQ_LEN, MAX_TIME);
   Tensor PARENT_IDX_FILTER = INDEX_TIME - edsl::reshape(MAX_SEQ_LEN_IN_BEAM, {1, batch_size, 1});
-  Tensor INDEX_BB = op::slice(INDEX_BEAM).add_dim(0, 1).add_dim(0, batch_size).add_dim(0, beam_width);
+  Tensor ZERO = edsl::cast(edsl::index({edsl::TensorDim(1)}, 0), STEP_IDS.dtype());
+  Tensor ZERO_INT = edsl::cast(ZERO, DType::INT32);
+  Tensor INDEX_BB = edsl::gather(INDEX_BEAM, ZERO_INT).axis(0);
   Tensor PARENT_IDX_NEW = edsl::select(PARENT_IDX_FILTER < 0, PARENT_IDX, edsl::cast(INDEX_BEAM, PARENT_IDX.dtype()));
   Tensor INDEX_BATCH = edsl::index(
       {edsl::TensorDim(max_time), edsl::TensorDim(batch_size), edsl::TensorDim(beam_width), edsl::TensorDim(1)}, 1);
@@ -58,8 +58,7 @@ Tensor GatherTree(Tensor STEP_IDS, Tensor PARENT_IDX, Tensor MAX_SEQ_LEN, Tensor
   Tensor PARENT = INDEX_O;
   for (int i = max_time - 1; i >= 0; i--) {
     parents.push_back(op::unsqueeze(PARENT, {0}));
-    Tensor PARENT_IDX_S = op::squeeze(
-        op::slice(PARENT_IDX_COR).add_dim(i, i + 1).add_dim(0, batch_size).add_dim(0, beam_width).add_dim(0, 2), {0});
+    Tensor PARENT_IDX_S = op::squeeze(edsl::gather(PARENT_IDX_COR, ZERO_INT + i).axis(0), {0});
     Tensor NEW_PARENT =
         op::squeeze(edsl::gather(op::unsqueeze(PARENT_IDX_S, {-1}), PARENT).mode(edsl::GatherMode::ND), {-1});
     PARENT = NEW_PARENT;
@@ -69,16 +68,15 @@ Tensor GatherTree(Tensor STEP_IDS, Tensor PARENT_IDX, Tensor MAX_SEQ_LEN, Tensor
   Tensor INDEX_ND = edsl::index(
       {edsl::TensorDim(max_time), edsl::TensorDim(batch_size), edsl::TensorDim(beam_width), edsl::TensorDim(2)}, 0);
   Tensor PARENTS_ND_FILTER = INDEX_ND - edsl::reshape(MAX_SEQ_LEN_IN_BEAM, {1, batch_size, 1, 1});
-  Tensor PARENTS_ND_T = edsl::select(PARENTS_ND_FILTER < 0, PARENTS_ND,
-                                     op::unsqueeze(edsl::cast(INDEX_O, PARENTS_ND.dtype()), {0}));  // 2*2*2*2  1*2*2*2
-  Tensor PARENTS_ND_NEW = op::concatenate({op::unsqueeze(INDEX_TIME, {-1}), PARENTS_ND_T}, -1);     // 2*2*2*1 2*2*2*2
+  Tensor PARENTS_ND_T =
+      edsl::select(PARENTS_ND_FILTER < 0, PARENTS_ND, op::unsqueeze(edsl::cast(INDEX_O, PARENTS_ND.dtype()), {0}));
+  Tensor PARENTS_ND_NEW = op::concatenate({op::unsqueeze(INDEX_TIME, {-1}), PARENTS_ND_T}, -1);
   Tensor UPDATE = edsl::gather(op::unsqueeze(STEP_IDS, {-1}), PARENTS_ND_NEW).mode(edsl::GatherMode::ND);
   Tensor OUTPUT = edsl::select(PARENT_IDX_FILTER < 0, op::squeeze(UPDATE, {-1}), END_TOKEN);
 
-  Tensor ZERO = cast(Tensor(0), OUTPUT.dtype());
   Tensor FILTER_A;
   if (OUTPUT.dtype() == DType::FLOAT32) {
-    float epsilon = 1e-6f;
+    float epsilon = 1e-7f;
     FILTER_A = op::abs(edsl::select(op::abs(OUTPUT - END_TOKEN) < epsilon, OUTPUT, ZERO));
   } else {
     FILTER_A = edsl::select(OUTPUT == END_TOKEN, OUTPUT, ZERO);
