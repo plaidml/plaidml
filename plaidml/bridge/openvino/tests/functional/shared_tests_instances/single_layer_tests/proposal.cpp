a0deb590ad7aa5077e48bedcc2b8b67bc718df9b
Ling, Liyang <liyang.ling@intel.com>
2021-05-21 16:56:35 +0800

    Update tests

--- plaidml/bridge/openvino/tests/functional/shared_tests_instances/single_layer_tests/proposal.cpp
+++ plaidml/bridge/openvino/tests/functional/shared_tests_instances/single_layer_tests/proposal.cpp
@@ -9,53 +9,78 @@ using namespace LayerTestsDefinitions;
 
 namespace {
 
-const std::vector<size_t> base_sizes = {16, 8};
-const std::vector<size_t> pre_nms_topns = {40, 80};
-const std::vector<size_t> post_nms_topns = {20, 40};
-const std::vector<float> nms_threshes = {1.0f, 0.5f};
-const std::vector<size_t> min_sizes = {0, 8};
+const std::vector<size_t> baseSizes = {16, 8};
+const std::vector<size_t> preNmsTopns = {80};
+const std::vector<size_t> postNmsTopns = {10};
+const std::vector<float> nmsThresholds = {1.0f, 0.5f};
+const std::vector<size_t> minSizes = {0, 8};
 const std::vector<std::vector<float>> ratios = {{1.0f}};
 const std::vector<std::vector<float>> scales = {{1.0f, 2.0f}};
-const std::vector<bool> clip_before_nms = {true, false};
-const std::vector<bool> clip_after_nms = {true, false};
-const std::vector<std::string> frameworks = {"", "tensorflow"};
 // feat_stride, normalize, box_size_scale, box_coordinate_scale, infer_probs
 // are hard coded in openvino op setup
 
-const auto proposalAttributes = ::testing::Combine(  //
-    ::testing::ValuesIn(base_sizes),                 //
-    ::testing::ValuesIn(pre_nms_topns),              //
-    ::testing::ValuesIn(post_nms_topns),             //
-    ::testing::ValuesIn(nms_threshes),               //
-    ::testing::ValuesIn(min_sizes),                  //
+const auto clipNMSAttributes = ::testing::Combine(  //
+    ::testing::ValuesIn(baseSizes),                 //
+    ::testing::ValuesIn(preNmsTopns),               //
+    ::testing::ValuesIn(postNmsTopns),              //
+    ::testing::ValuesIn(nmsThresholds),             //
+    ::testing::ValuesIn(minSizes),                  //
+    ::testing::ValuesIn(ratios),                    //
+    ::testing::ValuesIn(scales),                    //
+    ::testing::Values(true),                        //
+    ::testing::Values(true),                        //
+    ::testing::Values(""));
+const auto clipNMSParams = ::testing::Combine(  //
+    clipNMSAttributes,                          //
+    ::testing::Values(CommonTestUtils::DEVICE_PLAIDML));
+INSTANTIATE_TEST_CASE_P(clipNMS, ProposalLayerTest, clipNMSParams, ProposalLayerTest::getTestCaseName);
+
+const auto noClipAttributes = ::testing::Combine(  //
+    ::testing::ValuesIn(baseSizes),                //
+    ::testing::ValuesIn(preNmsTopns),              //
+    ::testing::ValuesIn(postNmsTopns),             //
+    ::testing::ValuesIn(nmsThresholds),            //
+    ::testing::ValuesIn(minSizes),                 //
+    ::testing::ValuesIn(ratios),                   //
+    ::testing::ValuesIn(scales),                   //
+    ::testing::Values(false),                      //
+    ::testing::Values(false),                      //
+    ::testing::Values(""));
+const auto noClipParams = ::testing::Combine(  //
+    noClipAttributes,                          //
+    ::testing::Values(CommonTestUtils::DEVICE_PLAIDML));
+INSTANTIATE_TEST_CASE_P(noClipNMS, ProposalLayerTest, noClipParams, ProposalLayerTest::getTestCaseName);
+
+const auto tfDecodeAttributes = ::testing::Combine(  //
+    ::testing::ValuesIn(baseSizes),                  //
+    ::testing::ValuesIn(preNmsTopns),                //
+    ::testing::ValuesIn(postNmsTopns),               //
+    ::testing::ValuesIn(nmsThresholds),              //
+    ::testing::ValuesIn(minSizes),                   //
     ::testing::ValuesIn(ratios),                     //
     ::testing::ValuesIn(scales),                     //
-    ::testing::ValuesIn(clip_before_nms),            //
-    ::testing::ValuesIn(clip_after_nms),             //
-    ::testing::ValuesIn(frameworks));
-
-const auto proposalParams = ::testing::Combine(  //
-    proposalAttributes,                          //
+    ::testing::Values(true),                         //
+    ::testing::Values(false),                        //
+    ::testing::Values("tensorflow"));
+const auto tfDecodeParams = ::testing::Combine(  //
+    tfDecodeAttributes,                          //
     ::testing::Values(CommonTestUtils::DEVICE_PLAIDML));
-
-INSTANTIATE_TEST_CASE_P(Proposal, ProposalLayerTest, proposalParams, ProposalLayerTest::getTestCaseName);
+INSTANTIATE_TEST_CASE_P(tfDecode, ProposalLayerTest, tfDecodeParams, ProposalLayerTest::getTestCaseName);
 
 const auto smokeAttributes = ::testing::Combine(        //
     ::testing::Values(8),                               //
-    ::testing::Values(80),                              //
-    ::testing::Values(40),                              //
-    ::testing::Values(1.0f),                            //
+    ::testing::Values(50),                              //
+    ::testing::Values(5),                               //
+    ::testing::Values(0.5f),                            //
     ::testing::Values(8),                               //
     ::testing::Values(std::vector<float>{1.0f}),        //
     ::testing::Values(std::vector<float>{1.0f, 2.0f}),  //
     ::testing::Values(true),                            //
     ::testing::Values(true),                            //
     ::testing::Values(""));
-
 const auto smokeParams = ::testing::Combine(  //
     smokeAttributes,                          //
     ::testing::Values(CommonTestUtils::DEVICE_PLAIDML));
-
 INSTANTIATE_TEST_CASE_P(smoke, ProposalLayerTest, smokeParams, ProposalLayerTest::getTestCaseName);
 
 }  // namespace
