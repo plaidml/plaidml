37c63bbaa735cc213b940811634b5f75ea094bde
Frank Laub <frank.laub@intel.com>
2021-06-24 23:53:59 +0000

    Disable resize tmps pass

--- pmlc/target/x86/pipeline.cc
+++ pmlc/target/x86/pipeline.cc
@@ -296,7 +296,7 @@ void pipelineBuilderStage1(OpPassManager &pm, const Options &options) {
   pm.addPass(createCanonicalizerPass());
 
   pm.addNestedPass<FuncOp>(pxa::createLocalizePass());
-  pm.addNestedPass<FuncOp>(pxa::createResizeTmpsPass());
+  // pm.addNestedPass<FuncOp>(pxa::createResizeTmpsPass());
   pm.addPass(pxa::createDeallocPlacementPass());
   pm.addNestedPass<FuncOp>(pxa::createAffineNormalizePass(/*promote=*/true,
                                                           /*denest=*/true));
