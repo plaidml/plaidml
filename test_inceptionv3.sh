166f60c17008684f3d455f30bf06afaa3d687596
Sanket Tavarageri <sanket.tavarageri@intel.com>
2021-06-29 23:23:10 -0700

    Adding inceptionv3 testing script

---
+++ test_inceptionv3.sh
@@ -0,0 +1,4 @@
+MODEL=inception_v3
+cd build-x86_64/Release 
+PLAIDML_VERBOSE=4 PLAIDML_DUMP=/nfs_home/stavarag/work/PlaidML/plaidml-brgemm_polydl/${MODEL}_files PYTHONPATH=$PWD python plaidbench/plaidbench.py -n3 keras ${MODEL}
+cd ../..
