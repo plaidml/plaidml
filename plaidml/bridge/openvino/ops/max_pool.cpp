222630fcac7284e7a4132700274f17102edcb1a7
Yao Shi <yao.shi@intel.com>
2022-04-06 22:32:33 -0700

    Enable OpenVINO Resnet50-fp32

--- plaidml/bridge/openvino/ops/max_pool.cpp
+++ plaidml/bridge/openvino/ops/max_pool.cpp
@@ -7,6 +7,7 @@
 
 #include "ngraph/opsets/opset.hpp"
 #include "ngraph/opsets/opset1.hpp"
+#include "ngraph/opsets/opset8.hpp"
 
 #include "plaidml/op/op.h"
 
@@ -15,35 +16,46 @@ using namespace InferenceEngine;  // NOLINT[build/namespaces]
 
 namespace PlaidMLPlugin {
 
+template <typename MaxPoolType>
+edsl::Tensor registerMaxPoolOp(MaxPoolType* layer, const Context& ctx) {
+  IE_ASSERT(ctx.operands.size() == 1);
+  auto I = ctx.operands.at(0);
+  std::vector<int> strides;
+  for (auto stride : layer->get_strides()) {
+    strides.push_back(stride);
+  }
+  std::vector<int> kernel;
+  for (auto k : layer->get_kernel()) {
+    kernel.push_back(k);
+  }
+  auto pool_type = plaidml::op::PoolMode::MAX;
+  auto input_layout = plaidml::op::TensorLayout::NCX;
+  auto autopad_mode = to_plaidml(layer->get_auto_pad());
+  bool include_padding_in_avg = false;
+  auto use_ceil_for_output_shape = layer->get_rounding_type() == ngraph::op::RoundingType::CEIL;
+  std::vector<int> padding;
+  if (autopad_mode == plaidml::op::AutoPadMode::EXPLICIT) {
+    for (auto pad : layer->get_pads_begin()) {
+      padding.push_back(pad);
+    }
+    for (auto pad : layer->get_pads_end()) {
+      padding.push_back(pad);
+    }
+  }
+  return op::pool(I, pool_type, kernel, strides, autopad_mode, padding, input_layout, include_padding_in_avg,
+                  use_ceil_for_output_shape);
+}
+
 void registerMaxPool() {
   registerOp("MaxPool", [](const Context& ctx) {
-    auto* layer = ngraph::as_type<ngraph::opset1::MaxPool>(ctx.layer);
-    IE_ASSERT(ctx.operands.size() == 1);
-    auto I = ctx.operands.at(0);
-    std::vector<int> strides;
-    for (auto stride : layer->get_strides()) {
-      strides.push_back(stride);
+    if (auto* layer = ngraph::as_type<ngraph::opset1::MaxPool>(ctx.layer)) {
+      return edsl::make_tuple(registerMaxPoolOp<ngraph::opset1::MaxPool>(layer, ctx));
     }
-    std::vector<int> kernel;
-    for (auto k : layer->get_kernel()) {
-      kernel.push_back(k);
+    if (auto* layer = ngraph::as_type<ngraph::opset8::MaxPool>(ctx.layer)) {
+      // TODO: the second output tensor is useful
+      return edsl::make_tuple(registerMaxPoolOp<ngraph::opset8::MaxPool>(layer, ctx), edsl::Tensor{0});
     }
-    auto pool_type = plaidml::op::PoolMode::MAX;
-    auto input_layout = plaidml::op::TensorLayout::NCX;
-    auto autopad_mode = to_plaidml(layer->get_auto_pad());
-    bool include_padding_in_avg = false;
-    auto use_ceil_for_output_shape = layer->get_rounding_type() == ngraph::op::RoundingType::CEIL;
-    std::vector<int> padding;
-    if (autopad_mode == plaidml::op::AutoPadMode::EXPLICIT) {
-      for (auto pad : layer->get_pads_begin()) {
-        padding.push_back(pad);
-      }
-      for (auto pad : layer->get_pads_end()) {
-        padding.push_back(pad);
-      }
-    }
-    return edsl::make_tuple(op::pool(I, pool_type, kernel, strides, autopad_mode, padding, input_layout,
-                                     include_padding_in_avg, use_ceil_for_output_shape));
+    IE_ASSERT(false);
   });
 }
 
