cdf878774540225e2bf6a4c6804c2a396a156747
lorenzo chelini <l.chelini@icloud.com>
2022-05-09 23:29:02 -0700

    fix compilation after rebase

--- pmlc/dialect/linalgx/transforms/pass_detail.h
+++ pmlc/dialect/linalgx/transforms/pass_detail.h
@@ -3,6 +3,12 @@
 #include "mlir/Dialect/MemRef/IR/MemRef.h"
 #include "mlir/Pass/Pass.h"
 
+namespace mlir {
+namespace func {
+class FuncOp;
+} // end namespace func
+} // end namespace mlir
+
 namespace pmlc::dialect::linalgx {
 
 #define GEN_PASS_CLASSES
