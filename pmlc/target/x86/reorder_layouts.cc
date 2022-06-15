d7dd9407cd37e3989444c65f42f6e9b14e00bb9f
Tim Zerrell <tim.zerrell@intel.com>
2022-06-15 16:22:55 -0700

    Backout weight reorder if loops can't be inferred

--- pmlc/target/x86/reorder_layouts.cc
+++ pmlc/target/x86/reorder_layouts.cc
@@ -726,6 +726,20 @@ struct ReorderWeightLayoutsPass
           builder.create<linalg::YieldOp>(loc, ValueRange{add});
         });
 
+    if (!newConv.getShapesToLoopsMap()) {
+      IVLOG(1, "Cannot reorder: LinAlg unable to infer ShapesToLoopsMap. op: "
+                   << debugString(op));
+      IVLOG(1, "    Attempted reordered op: " << debugString(newConv));
+
+      // Back out the reordered op
+      newConv.erase();
+      reorderFilter.erase();
+      return;
+    } else {
+      IVLOG(1, "TEMP ShapesToLoopsMap OK for op " << debugString(op));
+      IVLOG(1, "    TEMP Resulting reordered op: " << debugString(newConv));
+    }
+
     op.getResult(0).replaceAllUsesWith(newConv.getResult(0));
     op.erase();
   }
