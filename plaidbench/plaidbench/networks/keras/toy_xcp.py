7675365a74d8b0c83c4f732229223149e4d0e438
Tim Zerrell <tim.zerrell@intel.com>
2022-06-15 12:21:11 -0700

    Simplify and adjust example

--- plaidbench/plaidbench/networks/keras/toy_xcp.py
+++ plaidbench/plaidbench/networks/keras/toy_xcp.py
@@ -13,15 +13,17 @@
 # limitations under the License.
 
 
-def scale_dataset(x_train):
+def scale_dataset(x_train, as_internal=True):
     import numpy as np
-    # Upscale image size by a factor of 10
-    x_train = np.repeat(np.repeat(x_train, 10, axis=1), 10, axis=2)
-    # Crop the images to 199 x 199 and normalize
-    return (x_train[:, 10:10 + 299, 10:10 + 299]) / 255.
+    if as_internal:
+        x_train = np.repeat(np.repeat(np.repeat(x_train, 10, axis=1), 10, axis=2), 22, axis=3)
+        return (x_train[:, 10:10 + 149, 10:10 + 149, :64]) / 255.
+    else:
+        x_train = np.repeat(np.repeat(x_train, 10, axis=1), 10, axis=2)
+        return (x_train[:, 10:10 + 299, 10:10 + 299]) / 255.
 
 
-def build_model(use_batchnorm=False):
+def build_model(as_internal=True, use_batchnorm=False):
     from keras.models import Model
     from keras.layers import Activation, BatchNormalization, Conv2D, Input, MaxPooling2D, SeparableConv2D
     import keras.backend as K
@@ -30,12 +32,13 @@ def build_model(use_batchnorm=False):
     old_backend = K.backend
     K.backend = lambda: "tensorflow"
 
-    in_shape = (299, 299, 3)
+    if as_internal:
+        in_shape = (149, 149, 64)
+    else:
+        in_shape = (299, 299, 3)
     inp = Input(in_shape)
-    b1c1 = Conv2D(64, (3, 3), strides=(2, 2), use_bias=False)(inp)
-    b1c1relu = Activation('relu')(b1c1)
 
-    b2sc1 = SeparableConv2D(128, (3, 3), padding='same', use_bias=False)(b1c1relu)
+    b2sc1 = Conv2D(128, (3, 3), padding='same', use_bias=False)(Activation('relu')(inp))
     if use_batchnorm:
         b2sc1 = BatchNormalization()(b2sc1)
 
