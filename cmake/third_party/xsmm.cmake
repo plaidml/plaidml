2d42af1462a23ce4e869ec38603383989a130453
Hans Pabst <hans.pabst@intel.com>
2022-09-15 12:02:29 +0200

    Updated LIBXSMM and introduced environment variables LIBXSMMROOT=/path/to/libxsmm-make-install and LIBXSMMFETCH=0|1.

--- cmake/third_party/xsmm.cmake
+++ cmake/third_party/xsmm.cmake
@@ -1,21 +1,40 @@
-message("Fetching xsmm")
-FetchContent_Declare(
-  xsmm
-  URL      https://github.com/libxsmm/libxsmm/archive/394294d7f62efdd25033ee46e73abd4ccb3ded59.zip
-  URL_HASH SHA256=b2307f0374fac07f74414dc4365e078aa97d7abc59204c7955a658ad4e8a676e
-)
+# Use LIBXSMM (make PREFIX=/path/to/libxsmm) given by LIBXSMMROOT
+set(LIBXSMMROOT $ENV{LIBXSMMROOT})
+# Fetch LIBXSMM (even if LIBXSMMROOT is present)
+set(LIBXSMMFETCH $ENV{LIBXSMMFETCH})
 
-FetchContent_GetProperties(xsmm)
-if(NOT xsmm_POPULATED)
-  FetchContent_Populate(xsmm)
+if(LIBXSMMROOT AND NOT LIBXSMMFETCH)
+  message(STATUS "Found LIBXSMM (${LIBXSMMROOT})")
+  file(GLOB _GLOB_XSMM_SRCS LIST_DIRECTORIES false CONFIGURE_DEPENDS ${LIBXSMMROOT}/include/libxsmm/*.c)
+  list(REMOVE_ITEM _GLOB_XSMM_SRCS ${LIBXSMMROOT}/include/libxsmm/libxsmm_generator_gemm_driver.c)
+else()
+  message(STATUS "Fetching LIBXSMM")
+  include(FetchContent)
+
+  FetchContent_Declare(
+    xsmm
+    URL https://github.com/libxsmm/libxsmm/archive/96806053cbc9a19c25318965498553f3a935ede2.tar.gz
+    URL_HASH SHA256=2afc21069f2b329d52b554f52ffab83866b06e040a1113f39a11caeb7523b420
+  )
+
+  FetchContent_GetProperties(xsmm)
+  if(NOT xsmm_POPULATED)
+    FetchContent_Populate(xsmm)
+  endif()
+
+  set(LIBXSMMROOT ${xsmm_SOURCE_DIR})
+  file(GLOB _GLOB_XSMM_SRCS LIST_DIRECTORIES false CONFIGURE_DEPENDS ${LIBXSMMROOT}/src/*.c)
+  list(REMOVE_ITEM _GLOB_XSMM_SRCS ${LIBXSMMROOT}/src/libxsmm_generator_gemm_driver.c)
 endif()
-
-file(GLOB _GLOB_XSMM_SRCS LIST_DIRECTORIES false CONFIGURE_DEPENDS ${xsmm_SOURCE_DIR}/src/*.c)
-list(REMOVE_ITEM _GLOB_XSMM_SRCS ${xsmm_SOURCE_DIR}/src/libxsmm_generator_gemm_driver.c)
+set(XSMM_INCLUDE_DIRS ${LIBXSMMROOT}/include)
 
 add_library(xsmm STATIC ${_GLOB_XSMM_SRCS})
-target_include_directories(xsmm PUBLIC ${xsmm_SOURCE_DIR}/include)
-target_compile_definitions(xsmm PRIVATE
-  __BLAS=0
+target_include_directories(xsmm PUBLIC ${XSMM_INCLUDE_DIRS})
+target_compile_definitions(xsmm PUBLIC
   LIBXSMM_DEFAULT_CONFIG
 )
+target_compile_definitions(xsmm PRIVATE
+  __BLAS=0
+)
+
+#target_link_libraries(xsmm PUBLIC m)
