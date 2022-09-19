9adda959f5d99d8726c95fae117d42445c718068
Hans Pabst <hans.pabst@intel.com>
2022-09-19 23:13:17 +0200

    Updated build definitions (LIBXSMM).

--- cmake/third_party/xsmm.cmake
+++ cmake/third_party/xsmm.cmake
@@ -5,16 +5,16 @@ set(LIBXSMMFETCH $ENV{LIBXSMMFETCH})
 
 if(LIBXSMMROOT AND NOT LIBXSMMFETCH)
   message(STATUS "Found LIBXSMM (${LIBXSMMROOT})")
-  file(GLOB _GLOB_XSMM_SRCS LIST_DIRECTORIES false CONFIGURE_DEPENDS ${LIBXSMMROOT}/include/libxsmm/*.c)
-  list(REMOVE_ITEM _GLOB_XSMM_SRCS ${LIBXSMMROOT}/include/libxsmm/libxsmm_generator_gemm_driver.c)
+  file(GLOB XSMM_SRCS LIST_DIRECTORIES false CONFIGURE_DEPENDS ${LIBXSMMROOT}/include/libxsmm/*.c)
+  list(REMOVE_ITEM XSMM_SRCS ${LIBXSMMROOT}/include/libxsmm/libxsmm_generator_gemm_driver.c)
 else()
   message(STATUS "Fetching LIBXSMM")
   include(FetchContent)
 
   FetchContent_Declare(
     xsmm
-    URL https://github.com/libxsmm/libxsmm/archive/96806053cbc9a19c25318965498553f3a935ede2.tar.gz
-    URL_HASH SHA256=2afc21069f2b329d52b554f52ffab83866b06e040a1113f39a11caeb7523b420
+    URL https://github.com/libxsmm/libxsmm/archive/9a0c707384f76bf4a304a4998be9c6c5e7c41400.tar.gz
+    URL_HASH SHA256=bc17bc0d07e4210ec4cdb7790a67cf1226c93dc82862e5d89d93e83d81233532
   )
 
   FetchContent_GetProperties(xsmm)
@@ -23,12 +23,12 @@ else()
   endif()
 
   set(LIBXSMMROOT ${xsmm_SOURCE_DIR})
-  file(GLOB _GLOB_XSMM_SRCS LIST_DIRECTORIES false CONFIGURE_DEPENDS ${LIBXSMMROOT}/src/*.c)
-  list(REMOVE_ITEM _GLOB_XSMM_SRCS ${LIBXSMMROOT}/src/libxsmm_generator_gemm_driver.c)
+  file(GLOB XSMM_SRCS LIST_DIRECTORIES false CONFIGURE_DEPENDS ${LIBXSMMROOT}/src/*.c)
+  list(REMOVE_ITEM XSMM_SRCS ${LIBXSMMROOT}/src/libxsmm_generator_gemm_driver.c)
 endif()
 set(XSMM_INCLUDE_DIRS ${LIBXSMMROOT}/include)
 
-add_library(xsmm STATIC ${_GLOB_XSMM_SRCS})
+add_library(xsmm STATIC ${XSMM_SRCS})
 target_include_directories(xsmm PUBLIC ${XSMM_INCLUDE_DIRS})
 target_compile_definitions(xsmm PUBLIC
   LIBXSMM_DEFAULT_CONFIG
@@ -36,5 +36,10 @@ target_compile_definitions(xsmm PUBLIC
 target_compile_definitions(xsmm PRIVATE
   __BLAS=0
 )
+add_definitions(-U_DEBUG)
 
-#target_link_libraries(xsmm PUBLIC m)
+target_link_libraries(xsmm PUBLIC pthread)
+target_link_libraries(xsmm PUBLIC rt)
+target_link_libraries(xsmm PUBLIC dl)
+target_link_libraries(xsmm PUBLIC m)
+target_link_libraries(xsmm PUBLIC c)
