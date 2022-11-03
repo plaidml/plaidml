1d07273ee5ce5a2bcea10f1fb517a04c8d914e74
Hans Pabst <hans.pabst@intel.com>
2022-11-03 10:55:21 +0100

    Updated Boost (latest compatible version).

--- cmake/third_party/boost.cmake
+++ cmake/third_party/boost.cmake
@@ -1,5 +1,5 @@
-set(BOOST_URL "https://boostorg.jfrog.io/artifactory/main/release/1.71.0/source/boost_1_71_0.tar.bz2" CACHE STRING "Boost download URL")
-set(BOOST_URL_SHA256 "d73a8da01e8bf8c7eda40b4c84915071a8c8a0df4a6734537ddde4a8580524ee" CACHE STRING "Boost download URL SHA256 checksum")
+set(BOOST_URL "https://boostorg.jfrog.io/artifactory/main/release/1.77.0/source/boost_1_77_0.tar.bz2" CACHE STRING "Boost download URL")
+set(BOOST_URL_SHA256 "fc9f85fc030e233142908241af7a846e60630aa7388de9a5fafb1f3a26840854" CACHE STRING "Boost download URL SHA256 checksum")
 
 include(FetchContent)
 FetchContent_Declare(
