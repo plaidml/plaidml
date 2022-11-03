e2228af3709f124940bba1f7bc59ad0960b6e69a
Hans Pabst <hans.pabst@intel.com>
2022-11-03 09:37:53 +0100

    Fixed path (mlperf wheel).

--- mlperf/setup.in.py
+++ mlperf/setup.in.py
@@ -12,15 +12,16 @@ class BinaryDistribution(Distribution):
 
 
 def main():
+    setup_package_name = 'mlperf'
     setup_package_dir = '@PROJECT_BINARY_DIR@'
     setup_wheel_name = '@_WHEEL_FILE@'
     result = setup(
-        name='mlperf',
+        name=setup_package_name,
         version='1.1',
         description="MLPerf Inference benchmark",
         url="https://mlperf.org",
-        packages=['mlperf'],
-        package_data={'mlperf': ['@_MLPERF_LOADGEN_MODULE@']},
+        packages=[setup_package_name],
+        package_data={setup_package_name: ['@_MLPERF_LOADGEN_MODULE@']},
         package_dir={'': setup_package_dir},
         distclass=BinaryDistribution,
         zip_safe=False,
@@ -30,7 +31,7 @@ def main():
         wheel_tags = '-'.join(bdist_wheel.get_tag())
         wheel_name = f'{bdist_wheel.wheel_dist_name}-{wheel_tags}.whl'
         if setup_wheel_name != wheel_name:
-            wheel_path = Path(setup_package_dir)
+            wheel_path = Path(setup_package_dir) / setup_package_name
             wheel_orig = wheel_path / setup_wheel_name
             wheel_link = wheel_path / wheel_name
             wheel_orig.symlink_to(wheel_link)
