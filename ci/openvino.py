e260555f0cf85a21e375a02aede7c83c92c594a1
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-28 14:17:58 +0000

    Adding tarfile member sanitization to extractall()

--- ci/openvino.py
+++ ci/openvino.py
@@ -88,7 +88,26 @@ def prepare_virtual_env(venv_path=Path('.venv')):
     print(f'Downloading: {OPEN_ZOO_URL}')
     zoo_tarball, _ = urllib.request.urlretrieve(OPEN_ZOO_URL)
     with tarfile.open(zoo_tarball) as zoo:
-        zoo.extractall()
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(zoo)
 
     # install AC into venv
     cmd = [venv_bin / 'python', AC_DIR / 'setup.py', 'install']
